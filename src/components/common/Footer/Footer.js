61eff63c010874661261fcbc12ae9abbefbf8b9a
Ashutosh <ashutosh.sahu@wipro.com>
2023-01-31 17:25:30 +0530

    Footer design has been implemented

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -7,13 +7,13 @@ const { Footer: FooterLayout } = Layout;
 
 export const Footer = () => {
     return (
-        <FooterLayout id="footerSection" className="main-footer">
-            <div className="link">
+        <FooterLayout id="footerSection">
+            <div className={styles.footerLink}>
                 <a href="/">Terms of use</a>
                 <a href="/">About us</a>
                 <a href="/">Disclaimer</a> <a href="/">Contact Us</a>
             </div>
-            <div className="textAlignCenter">
+            <div className={styles.footerBottomText}>
                 Copyright &copy; 2022-2023 <a href="/">ROBIN</a>. All rights reserved.
             </div>
         </FooterLayout>
