98eb8025168a01f110487917626c5726fd9b6c71
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-02-02 18:11:51 +0530

    WIP #2267 #2268
    working on footer menu

--- src/pages/routing/UnAuthenticatedUserPage.js
+++ src/pages/routing/UnAuthenticatedUserPage.js
@@ -9,14 +9,8 @@ import { ProfilePage } from 'pages/user/ProfilePage';
 import { SettingPage } from 'pages/user/SettingPage';
 import { FaqPage } from 'pages/user/FaqPage';
 import { TrainingPage } from 'pages/user/TrainingPage';
-import { MahindraPage } from 'pages/user/MahindraPage';
-import { MgPage } from 'pages/user/MgPage';
-import { TermPage } from 'pages/user/TermPage';
-import { AboutPage } from 'pages/user/AboutPage';
-import { DisclaimerPage } from 'pages/user/DisclaimerPage';
-import { ContactPage } from 'pages/user/ContactPage';
-import { BranchPage } from 'pages/user/BranchPage';
-import { FinancialPage } from 'pages/user/FinancialPage';
+
+import { CMSPage } from 'pages/cms';
 
 export const UnAuthenticatedUserPage = () => {
     return (
@@ -29,14 +23,12 @@ export const UnAuthenticatedUserPage = () => {
             <Route path={routing.ROUTING_USER_SETTING} element={<SettingPage />} exact />
             <Route path={routing.ROUTING_USER_FAQ} element={<FaqPage />} exact />
             <Route path={routing.ROUTING_USER_TRAINING} element={<TrainingPage />} exact />
-            <Route path={routing.ROUTING_USER_MAHINDRA} element={<MahindraPage />} exact />
-            <Route path={routing.ROUTING_USER_MG} element={<MgPage />} exact />
-            <Route path={routing.ROUTING_USER_TERM} element={<TermPage />} exact />
-            <Route path={routing.ROUTING_USER_ABOUT} element={<AboutPage />} exact />
-            <Route path={routing.ROUTING_USER_DISCLAIMER} element={<DisclaimerPage />} exact />
-            <Route path={routing.ROUTING_USER_CONTACT} element={<ContactPage />} exact />
-            <Route path={routing.ROUTING_USER_BRANCH} element={<BranchPage />} exact />
-            <Route path={routing.ROUTING_USER_FINANACIAL} element={<FinancialPage />} exact />
+           
+           
+            <Route path={routing.ROUTING_USER_TERM} element={<CMSPage />} exact />
+            <Route path={routing.ROUTING_USER_ABOUT} element={<CMSPage />} exact />
+            <Route path={routing.ROUTING_USER_DISCLAIMER} element={<CMSPage />} exact />
+            <Route path={routing.ROUTING_USER_CONTACT} element={<CMSPage />} exact />
 
             <Route path="*" exact />
         </Routes>
