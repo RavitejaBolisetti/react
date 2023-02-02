bbff3e8e20e38cac435622cd30a71c7919a1714d
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-02-02 18:20:01 +0530

    WIP #2267 #2268
    working on footer menu

--- src/constants/routing.js
+++ src/constants/routing.js
@@ -1,13 +1,12 @@
 export const ROUTING_HOME = '/';
 export const ROUTING_LOGIN = '/login';
-export const ROUTING_FORGOT_PASSWORD = '/forgotpassword';
+export const ROUTING_FORGOT_PASSWORD = '/forgot-password';
 
 export const ROUTING_DASHBOARD = '/dashboard';
-export const ROUTING_DASHBOARD1 = '/dashboard1';
 
 export const ROUTING_COMMON = '/common/';
 export const ROUTING_COMMON_GEO = ROUTING_COMMON.concat('geo');
-export const ROUTING_COMMON_PRODUCT_HIERARCHY = ROUTING_COMMON.concat('productHierarchy');
+export const ROUTING_COMMON_PRODUCT_HIERARCHY = ROUTING_COMMON.concat('product-hierarchy');
 
 export const ROUTING_USER = '/user/';
 export const ROUTING_USER_PROFILE = ROUTING_USER.concat('profile');
@@ -15,7 +14,7 @@ export const ROUTING_USER_SETTING = ROUTING_USER.concat('setting');
 export const ROUTING_USER_FAQ = ROUTING_USER.concat('faq');
 export const ROUTING_USER_TRAINING = ROUTING_USER.concat('training');
 
-export const ROUTING_USER_TERM = ROUTING_HOME.concat('term');
-export const ROUTING_USER_ABOUT = ROUTING_HOME.concat('about');
+export const ROUTING_USER_TERM = ROUTING_HOME.concat('term-of-Use');
+export const ROUTING_USER_ABOUT = ROUTING_HOME.concat('about-us');
 export const ROUTING_USER_DISCLAIMER = ROUTING_HOME.concat('disclaimer');
-export const ROUTING_USER_CONTACT = ROUTING_HOME.concat('contact');
+export const ROUTING_USER_CONTACT = ROUTING_HOME.concat('contact-us');
