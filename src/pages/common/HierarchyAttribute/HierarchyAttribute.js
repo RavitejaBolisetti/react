03c58bc20808154523c5470d6a1f69d0701c94ec
Kartik Gupta <KA20371579@wipro.com>
2023-02-04 16:46:25 +0530

    Working on Hierarchy Attribute Master
    WIP #1805 #1458

--- src/pages/common/HierarchyAttribute/HierarchyAttribute.js
+++ src/pages/common/HierarchyAttribute/HierarchyAttribute.js
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { FaSearch, FaEdit, FaUserPlus, FaUserFriends } from 'react-icons/fa';
 
-import TreeView from 'components/common/TreeView';
+import { DeleteOutlined, EditOutlined, ExclamationCircleFilled } from '@ant-design/icons';
+
+import { FaUserPlus, FaUserFriends, FaSave, FaUndo } from 'react-icons/fa';
+
+// import TreeView from 'components/common/TreeView';
 // import { GeoTree as TreeView } from './Sample/GeoTree';
 
 import { withLayoutMaster } from 'components/withLayoutMaster';
 import { Button, Col, Input, Modal, Form, Row, Select, Space, Switch } from 'antd';
-import { Table, Tooltip } from 'antd';
+import { Table } from 'antd';
 import { validateRequiredSelectField } from 'utils/validation';
 
 import MetaTag from 'utils/MetaTag';
-import styles from './HierarchyAttribute.module.css';
+
+import styles from '../Common.module.css';
 
 const { Option } = Select;
+const { confirm } = Modal;
+
+const showConfirm = () => {
+    confirm({
+        title: 'Do you Want to delete these items?',
+        icon: <ExclamationCircleFilled />,
+        content: 'Some descriptions',
+        onOk() {
+            console.log('OK');
+        },
+        onCancel() {
+            console.log('Cancel');
+        },
+    });
+};
 
 const columns = [
     {
@@ -21,39 +40,53 @@ const columns = [
         dataIndex: 'code',
         key: 'code',
         render: () => <Input placeholder="MT0001" />,
-        width: 120,
+        width: 150,
     },
     {
         title: 'Name',
         dataIndex: 'name',
         key: 'name',
         render: (text) => <Input placeholder="Name" />,
-        width: 300,
+        width: 400,
     },
     {
         title: 'Duplicate Allowed?',
         dataIndex: 'DuplicateAllowed?',
+        width: 100,
         key: 'address 1',
-        render: () => <Switch checkedChildren="Yes" unCheckedChildren="NO" defaultChecked />,
+        render: () => <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked />,
     },
     {
         title: 'Duplicate Allowed under different Parent?',
         dataIndex: 'Duplicate Allowed under different Parent?',
         key: 'address 2',
+        width: 200,
 
-        render: () => <Switch checkedChildren="Yes" unCheckedChildren="NO" defaultChecked />,
+        render: () => <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked />,
     },
     {
         title: 'Child Allowed?',
         dataIndex: 'Child Allowed?',
         key: 'address 3',
-        render: () => <Switch checkedChildren="Yes" unCheckedChildren="NO" defaultChecked />,
+        render: () => <Switch checkedChildren="Yes" unCheckedChildren="N0" defaultChecked />,
     },
     {
         title: 'Status',
         dataIndex: 'Status',
         key: 'address 4',
-        render: () => <Switch checkedChildren="Yes" unCheckedChildren="NO" defaultChecked />,
+        render: () => <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked />,
+    },
+    {
+        title: '',
+        dataIndex: '',
+        width: 100,
+        render: () => [
+            <Space wrap>
+                <EditOutlined />
+
+                <DeleteOutlined onClick={showConfirm} />
+            </Space>,
+        ],
     },
 ];
 const data = [
@@ -80,7 +113,6 @@ const mapStateToProps = (state) => {
 
     return returnValue;
 };
-
 export const HierarchyAttributeBase = () => {
     return (
         <>
@@ -94,16 +126,21 @@ export const HierarchyAttributeBase = () => {
                                 <span className={styles.headingGradient}>Hierarchy Attribute Master</span>
                             </div>
                         </Col>
-                        <Col xs={24} sm={24} md={12} lg={6} xl={6} xxl={6}>
-                            <button type="button" className="btn btn-outline rightbtn boxShdwNon mrl15">
+                        <Col xs={24} sm={24} md={12} lg={6} xl={6} xxl={6} className={styles.buttonContainer}>
+                            {/* <button type="button" className="btn btn-outline rightbtn boxShdwNon mrl15">
                                 <FaUserFriends className="fa-solid fa-user-group mrr5" />
                                 Exit
-                            </button>
+                            </button> */}
+                            <Button danger className={styles.exitButton}>
+                                <FaUserPlus className={styles.buttonIcon} />
+                                Exit
+                            </Button>
                         </Col>
                     </Row>
                     <div className={styles.pageHeaderNameSection}></div>
                 </Col>
             </Row>
+
             <Row gutter={20}>
                 <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
                     <div>
@@ -111,7 +148,6 @@ export const HierarchyAttributeBase = () => {
                     </div>
                     <Form.Item name=" Hierarchy Attribute " rules={[validateRequiredSelectField('Hierarchy Attribute ')]}>
                         <Select>
-                            <Option value="Select Hierarchy Type">Select Hierarchy Type</Option>
                             <Option value="Manufacturer Organisation">Manufacturer Organisation</Option>
                             <Option value="Manufacturer Administration">Manufacturer Administration</Option>
                             <Option value="Product">Product</Option>
@@ -121,7 +157,34 @@ export const HierarchyAttributeBase = () => {
                         </Select>
                     </Form.Item>
                 </Col>
-                <Table columns={columns} dataSource={data} pagination={false} />
+                {/* <Table columns={columns} dataSource={data} pagination={false} /> */}
+            </Row>
+            <Row gutter={20}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+                    <Table columns={columns} dataSource={data} pagination={false} />
+                </Col>
+            </Row>
+
+            <Row gutter={20}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} className={styles.buttonContainer}>
+                    <Button danger>
+                        <FaUserPlus className={styles.buttonIcon} />
+                        Add Row
+                    </Button>
+                </Col>
+            </Row>
+            <Row gutter={20}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24} className={styles.buttonContainer}>
+                    <Button danger>
+                        <FaSave className={styles.buttonIcon} />
+                        Save
+                    </Button>
+
+                    <Button danger>
+                        <FaUndo className={styles.buttonIcon} />
+                        Reset
+                    </Button>
+                </Col>
             </Row>
         </>
     );
