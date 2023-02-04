2875df18a328a69f58ce047f7b8af779c546afe3
Shakambhar pujari <shakambhar.pujari.ext@wipro.com>
2023-02-04 11:14:17 +0530

    REFRACTORING WIP #1797

--- src/pages/common/ChangeHistory/ChangeHistoryNeedToFinal.js
+++ src/pages/common/ChangeHistory/ChangeHistoryNeedToFinal.js
@@ -80,7 +80,22 @@ const generalsorter= (a,b) =>
         }
    }
 }
-const onFilterFn = (value, record) => record.ChangeDate.startsWith(value);
+const onFilterFn = (value, record) => 
+    {
+        if(record.ChangeDate!==undefined)
+        {
+           return (record.ChangeDate.startsWith(value));
+        }
+        else if(record.EmployeeCode!==undefined)
+        {
+            return (record.EmployeeCode.startsWith(value));
+        }
+        else if(record.EmployeeName!==undefined)
+        {
+            return (record.EmployeeName.startsWith(value));
+        }
+
+    }
 
 const tblPrepareColumns = ({ title, dataIndex, ellipsis = false, filters = undefined, filterMode = 'tree', filterSearch = true, sortFn = undefined }) => {
     return {
