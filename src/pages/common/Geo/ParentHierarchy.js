b191a0eefc46e5cc22e2947e27a7dee268217a63
Reena <reena.harikishan@wipro.com>
2023-02-04 15:14:54 +0530

    WIP #1779 #1519

--- src/pages/common/Geo/ParentHierarchy.js
+++ src/pages/common/Geo/ParentHierarchy.js
@@ -5,11 +5,7 @@ import { Modal } from 'antd';
 export default function ParentHierarchy({ isModalOpen, setIsModalOpen, title }) {
     return (
         <div>
-<<<<<<< HEAD
-            <Modal style={{ top: 100 }} title={title} open={open} onOk={() => setOpen(false)} onCancel={() => setOpen(false)}>
-=======
             <Modal style={{ top: 100 }} title={title} open={isModalOpen} onOk={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)}>
->>>>>>> feature
                 <TreeView isOpenInModal={true} />
             </Modal>
         </div>
