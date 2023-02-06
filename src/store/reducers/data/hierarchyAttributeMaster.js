5fae7804230148017a8e24c082e9e537b03e8771
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-02-06 17:38:43 +0530

    Working on GEO curd
    WIP #1779

---
+++ src/store/reducers/data/hierarchyAttributeMaster.js
@@ -0,0 +1,18 @@
+import { HIERARCHY_ATTRIBUTE_MASTER_DATA_LOADED, HIERARCHY_ATTRIBUTE_MASTER_DATA_SHOW_LOADING } from 'store/actions/data/hierarchyAttributeMaster';
+
+const initialState = {
+    isLoaded: false,
+    data: [],
+    isLoading: false,
+};
+
+export const HierarchyAttributeMaster = (state = initialState, action) => {
+    switch (action.type) {
+        case HIERARCHY_ATTRIBUTE_MASTER_DATA_LOADED:
+            return { ...state, isLoaded: action.isLoaded, data: action.data };
+        case HIERARCHY_ATTRIBUTE_MASTER_DATA_SHOW_LOADING:
+            return { ...state, isLoading: action.isLoading };
+        default:
+            return { ...state };
+    }
+};
