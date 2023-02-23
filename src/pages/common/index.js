fc78247ed7885c370689b9356b3923befb6e1ede
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-02-23 17:10:05 +0530

    Rolemanagement #wip #578

--- src/pages/common/index.js
+++ src/pages/common/index.js
@@ -1,6 +1,7 @@
 import { GeoPage } from './Geo';
 import { ProductMasterPage } from './ProductMaster/ProductMasterPage';
+import { RoleManagementPage } from './RoleManagement/RoleManagementPage';
 import { ProductHierarchyPage } from './ProductHierarchy';
 import { HierarchyAttributeMaster as HierarchyAttributeMasterPage } from './HierarchyAttributeMaster/HierarchyAttributeMaster';
 
-export { GeoPage, ProductMasterPage, ProductHierarchyPage, HierarchyAttributeMasterPage };
+export { GeoPage, ProductMasterPage, ProductHierarchyPage, HierarchyAttributeMasterPage, RoleManagementPage };
