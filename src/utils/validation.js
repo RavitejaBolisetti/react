f882f5259b3729f489dc0bc43a2af7a7556c4691
Vivek <vivek.vimal@wipro.com>
2023-02-27 19:43:25 +0530

    working on form

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -20,15 +20,15 @@ export const validationFieldLetterAndNumber = (fieldName) => ({
 
 export const validatePanField = (fieldName) => ({
     pattern: /^([A-Z]){5}([0-9]){4}([A-Z]){1}?$/,
-    message: 'Please Enter Valid' + fieldName,
+    message: 'Please Enter Valid ' + fieldName,
 });
 
 export const validateVehicleRegistrationField = (fieldName) => ({
     pattern: /^[0-9]{2}BH[0-9]{4}[A-HJ-NP-Z]{2}$/,
-    message: 'Please Enter Valid' + fieldName,
+    message: 'Please Enter Valid ' + fieldName,
 });
 
 export const validateMobileNoField = (fieldName) => ({
     pattern: /^([5-9]){5}([0-9])5$/,
-    message: 'Please Enter Valid' + fieldName,
+    message: 'Please Enter Valid ' + fieldName,
 });
