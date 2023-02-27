4066733e8191f16b1c2e9e8352fd10a1d549f35e
Ashutosh <ashutosh.sahu@wipro.com>
2023-02-27 16:40:37 +0530

    Working on Menu UI Changes
    WIP #518

--- src/constants/MenuConstant.js
+++ src/constants/MenuConstant.js
@@ -16,7 +16,7 @@ export const MenuConstant = {
     [getMenyKey('FAV')]: {
         key: 'FAV',
         link: undefined,
-        icon: <MdStars fontSize={20} />,
+        icon: <MdStars fontSize={22} />,
         parentMenuId: 'FAV',
     },
 
@@ -57,13 +57,13 @@ export const MenuConstant = {
 
     [getMenyKey('COMN')]: {
         link: undefined,
-        icon: <FaCreativeCommonsShare fontSize={20} />,
+        icon: <FaCreativeCommonsShare fontSize={22} />,
         parentMenuId: 'WEB',
     },
 
     [getMenyKey('DBP')]: {
         link: undefined,
-        icon: <FaAddressBook fontSize={20} />,
+        icon: <FaAddressBook fontSize={22} />,
         parentMenuId: 'WEB',
     },
 
@@ -75,13 +75,13 @@ export const MenuConstant = {
 
     [getMenyKey('HR')]: {
         link: undefined,
-        icon: <IoPeopleCircle fontSize={20} />,
+        icon: <IoPeopleCircle fontSize={22} />,
         parentMenuId: 'WEB',
     },
 
     [getMenyKey('Sales')]: {
         link: undefined,
-        icon: <AiFillCar fontSize={20} />,
+        icon: <AiFillCar fontSize={22} />,
         parentMenuId: 'WEB',
     },
 
@@ -105,7 +105,7 @@ export const MenuConstant = {
 
     [getMenyKey('CRM')]: {
         link: undefined,
-        icon: <MdWeb fontSize={18} />,
+        icon: <MdWeb fontSize={22} />,
         parentMenuId: 'WEB',
     },
 };
