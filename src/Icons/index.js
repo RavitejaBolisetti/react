139ce590c2ce58cec75306c07f2cd53f9455ec9a
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-02-28 14:00:12 +0000

    Squashed commit of the following
    commit 704869ca 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Tue Feb 28 2023 19:24:03 GMT+0530 (India Standard Time) 
    
        Merge branch 'US-518-UI-Changes-For-Menu' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/dms-ui-application into US-518-UI-Changes-For-Menu
    
    
    commit 19318471 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Tue Feb 28 2023 19:23:35 GMT+0530 (India Standard Time) 
    
        Working on Menu UI Changes
    
    WIP #518
    
    
    commit 9d38e5ea 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Tue Feb 28 2023 19:08:36 GMT+0530 (India Standard Time) 
    
        Added temp logout code
    
    wip #US-518
    
    
    commit 3140a141 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Tue Feb 28 2023 18:58:50 GMT+0530 (India Standard Time) 
    
        Working on Menu UI Changes
    
    WIP #518
    
    
    commit 3ba442b5 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Tue Feb 28 2023 16:36:19 GMT+0530 (India Standard Time) 
    
        Working on Menu UI Changes
    
    WIP #518
    
    
    commit 2c82268f 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Tue Feb 28 2023 14:51:25 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-518-UI-Changes-For-Menu
    
    
    commit aa2e97bc 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Tue Feb 28 2023 13:19:14 GMT+0530 (India Standard Time) 
    
        Merge branch 'US-518-UI-Changes-For-Menu' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/dms-ui-application into US-518-UI-Changes-For-Menu
    
    
    commit ed9e176c 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Tue Feb 28 2023 13:18:45 GMT+0530 (India Standard Time) 
    
        Working on Menu UI Changes
    
    WIP #518
    
    
    commit 3c992614 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Tue Feb 28 2023 13:12:46 GMT+0530 (India Standard Time) 
    
        Working on menu and change password icon update
    
    WIP #US-518
    
    
    commit 53f196f9 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Mon Feb 27 2023 22:17:42 GMT+0530 (India Standard Time) 
    
        working on  css
    
    
    commit eba1022e 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Mon Feb 27 2023 21:38:27 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-518-UI-Changes-For-Menu
    
    
    commit 76ac2e14 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Mon Feb 27 2023 21:37:03 GMT+0530 (India Standard Time) 
    
        add parent child class
    
    WIP #US-518
    
    
    commit d0a62167 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Mon Feb 27 2023 19:26:52 GMT+0530 (India Standard Time) 
    
        Working on header icon
    
    WIP #518
    
    
    commit b49abd02 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Mon Feb 27 2023 17:43:44 GMT+0530 (India Standard Time) 
    
        Working on Menu UI Changes
    
    WIP #518
    
    
    commit 4066733e 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Mon Feb 27 2023 16:40:37 GMT+0530 (India Standard Time) 
    
        Working on Menu UI Changes
    
    WIP #518
    
    
    commit 44c9d343 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Mon Feb 27 2023 16:14:38 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-518-UI-Changes-For-Menu
    
    
    commit 7f5f6253 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Mon Feb 27 2023 16:12:34 GMT+0530 (India Standard Time) 
    
        Working on Menu UI Changes
    
    WIP #1779

--- src/Icons/index.js
+++ src/Icons/index.js
@@ -1,4 +1,9 @@
 import { SVGComponent as HeadPhoneIcon } from './HeadPhone';
+import { SVGComponent as AdminIcon } from './AdminIcon';
+import { SVGComponent as CrmIcon } from './CrmIcon';
+import { SVGComponent as ServiceIcon } from './ServiceIcon';
+import { SVGComponent as SparesIcon } from './SparesIcon';
 import { SVGComponent as MenuArrow } from './MenuArrow';
+import { SVGComponent as HrIcon } from './HrIcon';
 
-export { HeadPhoneIcon, MenuArrow };
+export { HeadPhoneIcon, MenuArrow, AdminIcon, CrmIcon, ServiceIcon, SparesIcon, HrIcon };
