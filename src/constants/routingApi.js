b5395e7f9365df53b91a987818892c61e034684c
K Aman Rao <aman.rao1@wipro.com-at-803584912514>
2023-03-03 15:27:38 +0530

    Worked in Manufacturer Organization Hierarchy
    WIP #US-2851

--- src/constants/routingApi.js
+++ src/constants/routingApi.js
@@ -20,8 +20,8 @@ export const BASE_URL_PRODUCT_HIERARCHY_CHANGE_HISTORY = BASE_URL + 'product/hie
 export const BASE_URL_ATTRIBUTE_MASTER = BASE_URL + 'hierarchyattributes';
 
 export const BASE_URL_CONFIG_PARAM_EDIT = BASE_URL + 'configurableparameters';
-export const BASE_URL_MANUFACTURER_ORGANIZATION_HIERARCHY = BASE_URL + 'manufacturer/organization/hierarchy';
-export const BASE_URL_MANUFACTURER_ORG_HIERARCHY_SAVE = BASE_URL + 'manufacturehierarchy';
+export const BASE_URL_MANUFACTURER_ORGANIZATION_HIERARCHY = BASE_URL + 'manufacture/organization/hierarchy';
+export const BASE_URL_MANUFACTURER_ORG_HIERARCHY_SAVE = BASE_URL + 'manufacture/organization/hierarchy';
 export const BASE_URL_MANUFACTURER_ORG_HIERARCHY_CHANGE_HISTORY = BASE_URL + 'manufacturer/organization/changehistory';
 
 export const BASE_URL_MANUFACTURER_ADMINISTRATION_HIERARCHY = BASE_URL + 'manufactureradministrationhierarchy';
