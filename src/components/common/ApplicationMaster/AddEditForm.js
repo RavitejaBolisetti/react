86dd59a7b0376239a6243522cc48384c1e9d8175
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-03-14 16:30:14 +0530

    refactor code and fix attribute data loading issue
    wip #US-518

--- src/components/common/ApplicationMaster/AddEditForm.js
+++ src/components/common/ApplicationMaster/AddEditForm.js
@@ -4,7 +4,7 @@ import { Col, Row } from 'antd';
 import { Collapse, Space } from 'antd';
 
 import styles from 'pages/common/Common.module.css';
-import {ApplicationDetails} from './ApplicationDetails';
+import { ApplicationDetails } from './ApplicationDetails';
 import DocumentTypes from './DocumentTypes';
 import ApplicationActions from './ApplicationActions';
 import AccessibleDealerLocations from './AccessibleDealerLocations';
@@ -13,9 +13,8 @@ const { Panel } = Collapse;
 
 const AddEditForm = ({ setOpenAccordian, openAccordian, applicationform, applicationActionsform, documentTypesform, accessibleDealerLocationsform, setSelectedTreeKey, isChecked, setIsChecked, flatternData, formActionType, selectedTreeKey, selectedTreeSelectKey, isReadOnly, formData, geoData, handleSelectTreeClick, isDataAttributeLoaded, attributeData, setIsModalOpen }) => {
     const [selectedLocaationAccessiblity, setSelectedLocaationAccessiblity] = useState('');
-    
+
     const handleCollapse = (key) => {
-        console.log('key', key);
         setOpenAccordian(key);
     };
 
@@ -40,14 +39,13 @@ const AddEditForm = ({ setOpenAccordian, openAccordian, applicationform, applica
                                 <ApplicationActions form={applicationActionsform} formActionType={formActionType} isReadOnly={isReadOnly} />
                             </Panel>
                         </Collapse>
-                        {
-                            selectedLocaationAccessiblity === "restrictedAccessible"  &&
+                        {selectedLocaationAccessiblity === 'restrictedAccessible' && (
                             <Collapse onChange={() => handleCollapse('4')} activeKey={openAccordian} defaultActiveKey={['1']} expandIconPosition="end">
                                 <Panel header="Accessible Dealer Locations" key="4">
                                     <AccessibleDealerLocations form={accessibleDealerLocationsform} formActionType={formActionType} isReadOnly={isReadOnly} />
                                 </Panel>
                             </Collapse>
-                        }
+                        )}
                         <Collapse onChange={() => handleCollapse(3)} activeKey={openAccordian} defaultActiveKey={['1']} expandIconPosition="end">
                             <Panel header="Document Types" key="3">
                                 <DocumentTypes form={documentTypesform} formActionType={formActionType} isReadOnly={isReadOnly} />
