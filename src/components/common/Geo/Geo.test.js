4cb6ff3d2f11fd7c2246a88dc39f159ebd386fb9
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-03-16 12:54:47 +0530

    Coverage
    wip #GeoUnitTest

--- src/components/common/Geo/Geo.test.js
+++ src/components/common/Geo/Geo.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { async } from 'sonarqube-scanner';
 import { Geo } from './Geo';
 
 jest.mock('react-redux', () => ({
@@ -45,7 +46,7 @@ describe('geo component', () => {
         expect(codeInputField).toBeNull();
     });
 
-    test('render Code Field in  form element', async () => {
+    test('render form element', async () => {
         render(<Geo fetchList={fetchList} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
         const RootChildButton = screen.getByText('Add Child');
         fireEvent.click(RootChildButton);
@@ -53,17 +54,134 @@ describe('geo component', () => {
         expect(codeInputField).toBeTruthy();
     });
 
+    test('render form element', async () => {
+        render(<Geo fetchList={fetchList} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
+        const RootChildButton = screen.getByText('Add Child');
+        fireEvent.click(RootChildButton);
+        const CancelBtn = screen.getByText('Cancel');
+        fireEvent.click(CancelBtn);
+        expect(RootChildButton).toBeTruthy();
+    });
 
+    test('render form element', async () => {
+        render(<Geo fetchList={fetchList} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
+        const RootChildButton = screen.getByText('Add Child');
+        fireEvent.click(RootChildButton);
+        const ResetBtn = screen.getByText('Reset');
+        fireEvent.click(ResetBtn);
+        const codeInputField = screen.getByPlaceholderText('Please Enter Code');
+        const nameInputField = screen.getByPlaceholderText('Please Enter Name');
+        expect(codeInputField.value).toMatch('');
+        expect(nameInputField.value).toMatch('');
+    });
 
+    test('input field on enetering data form element', async () => {
+        render(<Geo fetchList={fetchList} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
+        const RootChildButton = screen.getByText('Add Child');
+        fireEvent.click(RootChildButton);
+        const codeInputField = screen.getByPlaceholderText('Please Enter Code');
+        const nameInputField = screen.getByPlaceholderText('Please Enter Name');
+        fireEvent.change(codeInputField, {
+            target: {
+                value: 'New Checkbox Item!',
+            },
+        });
+        fireEvent.change(nameInputField, {
+            target: {
+                value: 'New Checkbox Item!',
+            },
+        });
+        expect(codeInputField.value).toMatch('New Checkbox Item!');
+        expect(nameInputField.value).toMatch('New Checkbox Item!');
+    });
 
-    // test('render form element', async () => {
-    //     render(<Geo fetchList={fetchList} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
+    test('Save form element', async () => {
+        const onFinish = jest.fn();
+        const { getByLabelText, getByText } = render(<Geo fetchList={fetchList} onFinish={onFinish} saveData={saveData} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
+        const RootChildButton = screen.getByText('Add Child');
+        fireEvent.click(RootChildButton);
+        // console.log("test",SaveBtn)
+        const codeInputField = getByLabelText('Code');
+        const nameInputField = getByLabelText('Name');
+        const SaveBtn = getByText('Save');
+        onFinish.mockResolvedValue({
+            geoCode: 'ABCDE',
+            geoName: 'Asia',
+            isActive: true,
+            attributeKey: 'Continent',
+            geoParentCode: 'DMS',
+        });
+        const result = await onFinish();
+        // fireEvent.change(codeInputField, {
+        //     target: {
+        //         value: 'ABCDE',
+        //     },
+        // });
+
+        // fireEvent.change(nameInputField, {
+        //     target: {
+        //         value: 'ABCDEFG',
+        //     },
+        // });
+
+        fireEvent.click(SaveBtn);
+        expect(result).toBeTruthy();
+        expect(onFinish).toHaveBeenCalled();
+    });
+
+    test('reset form button', async () => {
+        const handleResetBtn = jest.fn();
+        const { getByLabelText, getByText } = render(<Geo handleResetBtn={handleResetBtn} fetchList={fetchList} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
+        const RootChildButton = getByText('Add Child');
+        fireEvent.click(RootChildButton);
+        const ResetBtn = getByText('Reset');
+        const codeInputField = getByLabelText('Code');
+        const nameInputField = getByLabelText('Name');
+        fireEvent.change(codeInputField, {
+            target: {
+                value: 'ABCDE',
+            },
+        });
+
+        fireEvent.change(nameInputField, {
+            target: {
+                value: 'ABCDEFG',
+            },
+        });
+        fireEvent.click(ResetBtn);
+        handleResetBtn.mockResolvedValue(1);
+        const result = await handleResetBtn();
+        expect(codeInputField.value).toMatch('');
+        expect(nameInputField.value).toMatch('');
+        expect(result).toBeTruthy();
+        expect(handleResetBtn).toHaveBeenCalled();
+    });
+
+    test('tree exists in geo page ', async () => {
         
-    //     const formText = screen.getByPlaceholderText("Please Enter Code");
-    //     expect(formText).toBeUndefined();
+    });
+
+    // test('tree visiblity', async () => {
+    //     render(<Geo fetchList={fetchList} hierarchyAttributeFetchList={hierarchyAttributeFetchList} />);
+    //     const RootChildButton = getByText('Add Child');
+    //     fireEvent.click(RootChildButton);
+    //     const ResetBtn = getByText('Reset');
+    //     const codeInputField = getByLabelText('Code');
+    //     const nameInputField = getByLabelText('Name');
+    //     fireEvent.change(codeInputField, {
+    //         target: {
+    //             value: 'ABCDE',
+    //         },
+    //     });
+
+    //     fireEvent.change(nameInputField, {
+    //         target: {
+    //             value: 'ABCDEFG',
+    //         },
+    //     });
+    //     fireEvent.click(ResetBtn);
+    //     expect(codeInputField.value).toMatch('');
+    //     expect(nameInputField.value).toMatch('');
+    //     await waitFor(() => expect(handleResetBtn).toHaveBeenCalled());
     // });
-
-
-
-
 });
