8d2443e46d5cacd1fc25fd7b7aa83ac7a64fcdf9
Simran <Simran.sehgal@wipro.com>
2023-03-24 13:06:18 +0530

    Screen chnanges
    #WIP

--- src/components/Auth/ForgotPassword/ForgotPassword.js
+++ src/components/Auth/ForgotPassword/ForgotPassword.js
@@ -113,7 +113,7 @@ const ForgotPassword = (props) => {
         setValue(event);
     };
 
-    const currentStep = 3;
+    const currentStep = 4;
     return (
         <>
             {contextAlertNotification}
@@ -220,10 +220,97 @@ const ForgotPassword = (props) => {
                                                 </div>
                                             </div>
                                         ) : currentStep === 3 ? (
-                                            ''
-                                        ) : (
-                                            ''
-                                        )}
+                                            <div className={styles.centerInner}>
+                                                <div className={styles.loginForm}>
+                                                    <div className={styles.loginHeading}>
+                                                    <h1>OTP Verification</h1>
+                                                                    <div className={styles.loginSubHeading}>
+                                                                        Please enter 6 digit OTP sent to your <br></br> registered mobile number +91-96 XXXX XXXX
+                                                                    </div></div>
+                                                                    <Row gutter={20}>
+                                                                    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                                                        <div className={styles.checkColor}> Enter Otp </div>
+                                                                    </Col>
+                                                                </Row>
+                                                                <Row gutter={20}>
+                                                                    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                                                        <OTPInput className={styles.changer} value={value} onChange={handleChange} autoFocus OTPLength={6} disabled={false} />
+                                                                    </Col>
+                                                                </Row>
+                                                                <Row gutter={20}>
+                                                                    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                                                        {counter != 0 ? (
+                                                                            <>
+                                                                                <Row gutter={20}>
+                                                                                    <Col xs={16} sm={16} md={16} lg={16} xl={16}>
+                                                                                        <div className={styles.checkColor}>
+                                                                                            00:{counter > 9 ? null : '0'}
+                                                                                            {counter}s
+                                                                                        </div>
+                                                                                    </Col>
+                                                                                    <Col xs={8} sm={8} md={8} lg={8} xl={8}>
+                                                                                        <div onClick={() => setCounter(30)} className={styles.resendDisabled} type="radio">
+                                                                                            <UndoOutlined /> Resend OTP
+                                                                                        </div>
+                                                                                    </Col>{' '}
+                                                                                </Row>
+                                                                            </>
+                                                                        ) : (
+                                                                            <>
+                                                                                <Row gutter={20}>
+                                                                                    <Col xs={16} sm={16} md={16} lg={16} xl={16}>
+                                                                                        <div className={styles.checkColor}>Didn't receive OTP?</div>
+                                                                                    </Col>
+                                                                                    <Col xs={8} sm={8} md={8} lg={8} xl={8}>
+                                                                                        <div onClick={() => Alert()} className={styles.resendEnabled} type="radio">
+                                                                                            <UndoOutlined /> Resend OTP
+                                                                                        </div>
+                                                                                    </Col>{' '}
+                                                                                </Row>{' '}
+                                                                            </>
+                                                                        )}
+                                                                    </Col>
+                                                                </Row>
+
+                                                                <Button onClick={handleNewPassword} className={styles.button} type="primary" htmlType="submit">
+                                                                        Verify OTP
+                                                                    </Button>
+
+                                                    <Row gutter={20}>
+                                                        <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                                            <div className={styles.loginFooter} type="radio">
+                                                                <Link to={ROUTING_LOGIN}>Back To Login</Link>
+                                                            </div>
+                                                        </Col>
+                                                    </Row>
+                                                </div>
+                                            </div>
+                                        ) :  currentStep === 4 ? (
+                                            <div className={styles.centerInner}>
+                                                <div className={styles.loginForm}>
+                                                    <div className={styles.loginHeading}>
+                                                    <h1 className={styles.inputBox}>Create New Password</h1>
+                                                <Form form={form}>
+                                                    <Row gutter={20}>
+                                                                    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                                                        <Form.Item name="newPassword" rules={[validateRequiredInputField('New password')]} className={`${styles.changer} ${styles.inputBox}`}>
+                                                                            <Input.Password prefix={<FaKey size={18} />} type="text" placeholder="Enter new password" visibilityToggle={true} />
+                                                                        </Form.Item>
+                                                                    </Col>
+                                                                </Row>
+                                                                <Row gutter={20}>
+                                                                    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                                                        <Form.Item name="confirmPassword" rules={[validateRequiredInputField('New password again')]} className={styles.inputBox}>
+                                                                            <Input.Password prefix={<FaKey size={18} />} type="text" placeholder="Re-enter new password" visibilityToggle={false} />
+                                                                        </Form.Item>
+                                                                    </Col>
+                                                                </Row>
+                                                                <Button onClick={handleChangedPassword} className={styles.button} type="primary" htmlType="submit">
+                                                                        Submit
+                                                                    </Button>
+                                                                    </Form>
+                                                        </div></div></div>
+                                        ): null}
                                     </div>
                                 </Col>
                             </Row>
