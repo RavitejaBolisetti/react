d93a5c44c0bc67da877f4ec2060ba07395c8ac87
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-03-27 10:08:37 +0000

    Squashed commit of the following
    commit 5a6f2275 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Mon Mar 27 2023 15:13:03 GMT+0530 (India Standard Time) 
    
        WIP #US-Menu-UI-Changes-Final
    
    
    commit 860185d1 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Mon Mar 27 2023 13:37:33 GMT+0530 (India Standard Time) 
    
        Working on Menu Theme Buttons UI alignment
    
    WIP #US-Menu-UI-Changes-Final
    
    
    commit 09211ce8 
    Author: Ashutosh <ashutosh.sahu@wipro.com> 
    Date: Sat Mar 25 2023 17:12:24 GMT+0530 (India Standard Time) 
    
        Working on Menu Theme Buttons UI Changes
    
    WIP #US-Menu-UI-Changes-Final

--- src/components/common/LeftSideBar/LeftSideBar.js
+++ src/components/common/LeftSideBar/LeftSideBar.js
@@ -80,7 +80,7 @@ const LeftSideBarMain = ({ isMobile, setIsMobile, isDataLoaded, isLoading, menuD
         if (!isDataLoaded) {
             fetchList({ setIsLoading: listShowLoading, userId });
         }
-        return () => {};
+        return () => { };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isDataLoaded]);
 
@@ -189,7 +189,7 @@ const LeftSideBarMain = ({ isMobile, setIsMobile, isDataLoaded, isLoading, menuD
                     className={styles.changeTheme}
                     onClick={handleThemeChange}
                     style={{
-                        paddingLeft: isMobile ? (collapsed ? '0px' : '14px') : '16px',
+                        paddingLeft: collapsed ? '14px' : '10px',
                         position: isMobile ? (collapsed ? 'relative' : 'absolute') : 'absolute',
                     }}
                 >
