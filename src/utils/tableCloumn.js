9f721bdfb3733c8ca8c00ba5cacb6bcf3f0db7dc
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-03-27 10:55:47 +0530

    TableColums Tostring
    wip #1784

--- src/utils/tableCloumn.js
+++ src/utils/tableCloumn.js
@@ -20,7 +20,7 @@ export const tblPrepareColumns = ({ title, dataIndex, render = undefined, ellips
         onFilter: onFilterFn,
         editable,
         width,
-        sorter: sorter && ((a, b) => (a && b ? a[dataIndex].localeCompare(b[dataIndex], undefined, { sensitivity: 'base' }) : a)),
+        sorter: sorter && ((a, b) => (a && b ? String(a[dataIndex]).localeCompare(String(b[dataIndex]), undefined, { sensitivity: 'base' }) : a)),
         sortDirections: ['descend', 'ascend'],
     };
 };
