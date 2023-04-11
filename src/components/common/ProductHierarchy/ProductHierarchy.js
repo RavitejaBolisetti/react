07402aa7faec1e179b09d43d812199c01fd4022c
Asitava <asitava.chowdhury@wipro.com>
2023-04-11 11:22:17 +0530

    Changes in UI

--- src/components/common/ProductHierarchy/ProductHierarchy.js
+++ src/components/common/ProductHierarchy/ProductHierarchy.js
@@ -47,8 +47,8 @@ const mapStateToProps = (state) => {
         userId,
         isChangeHistoryVisible: changeHistoryVisible,
         isDataLoaded,
-        //productHierarchyData,
-        productHierarchyData: [],
+        productHierarchyData,
+        //productHierarchyData: [],
         moduleTitle,
         viewTitle,
         isDataAttributeLoaded,
@@ -324,9 +324,9 @@ export const ProductHierarchyMain = ({ moduleTitle, viewTitle, userId, isDataLoa
                                 />
                             </Col>
                             {productHierarchyData.length > 0 && (
-                                <Col className={styles.buttonHeadingContainer} xs={5} sm={5} md={5} lg={5} xl={5}>
-                                    <Button type="primary" onClick={changeHistoryModelOpen}>
-                                        <FaHistory className={styles.buttonIcon} />
+                                <Col className={styles.buttonHeadingContainer}  xs={5} sm={5} md={5} lg={5} xl={5}>
+                                    <Button type="primary" className={`${styles.changeHistoryModelOpen} ${styles.floatRight}`}>
+                                        <FaHistory className={styles.buttonIcon}  />
                                         Change History
                                     </Button>
                                 </Col>
