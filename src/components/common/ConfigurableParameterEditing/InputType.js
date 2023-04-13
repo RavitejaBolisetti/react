c4814569f122e2be0a43f735538f259cb2f5faac
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-04-13 23:46:45 +0530

    Remove extra file

--- src/components/common/ConfigurableParameterEditing/InputType.js
+++ src/components/common/ConfigurableParameterEditing/InputType.js
@@ -1,23 +1,22 @@
 export const CONFIGURABLE_PARAMETARS_INPUT_TYPE = {
     TEXT: {
         KEY: 'T',
-        TYPE: 'text',        
+        TYPE: 'text',
         TITLE: 'Text',
     },
     NUMBER: {
         KEY: 'N',
-        TYPE: 'number',        
+        TYPE: 'number',
         TITLE: 'Number',
     },
     BOOLEAN: {
         KEY: 'B',
-        TYPE: 'boolean',    
+        TYPE: 'boolean',
         TITLE: 'Boolean',
     },
     DATE_RANGE: {
         KEY: 'D',
-        TYPE: 'date',   
+        TYPE: 'date',
         TITLE: 'Date',
     },
-   
 };
