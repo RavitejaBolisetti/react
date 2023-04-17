af4c0e300354f5cf928798f530d040f8be16c944
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-04-17 16:37:59 +0000

    Squashed commit of the following
    commit 2451d82b 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Mon Apr 17 2023 22:05:18 GMT+0530 (India Standard Time) 
    
        working on issue
    wip #change-password
    
    
    commit b2989ef5 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Mon Apr 17 2023 22:03:23 GMT+0530 (India Standard Time) 
    
        implemented password show on mouse hover
    
    wip #US-Change-Password
    
    
    commit d30de02f 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Mon Apr 17 2023 21:39:26 GMT+0530 (India Standard Time) 
    
        Worked on change password form
    
    WIP

--- src/components/common/ChangePassword/ChangePasswordForm.js
+++ src/components/common/ChangePassword/ChangePasswordForm.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Button, Col, Form, Input, Row } from 'antd';
 import { bindActionCreators } from 'redux';
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
 import { changePasswordActions } from 'store/actions/data/changePassword';
 import { showGlobalNotification } from 'store/actions/notification';
 import { validateFieldsPassword, validateRequiredInputField } from 'utils/validation';
@@ -42,15 +43,14 @@ const mapDispatchToProps = (dispatch) => ({
     ),
 });
 
-const ChangePasswordBase = ({ showGlobalNotification, isOpen = false, onOk = () => {}, onCancel = () => {}, title = '', discreption = '', doLogout, saveData, isDataLoaded, listShowLoading, userId }) => {
-    const [form] = Form.useForm();
+const ChangePasswordBase = ({ form, showGlobalNotification, isOpen = false, onOk = () => {}, title = '', discreption = '', doLogout, saveData, isDataLoaded, listShowLoading, userId }) => {
     const navigate = useNavigate();
-
-    const [confirmDirty, setConfirmDirty] = useState(false);
 
     const onFinish = (errorInfo) => {
         // form.validateFields().then((values) => {});
     };
+
+    const [showPassword, setShowPassword] = useState({ oldPassword: false, newPassword: false, confirmNewPassword: false });
 
     const onFinishFailed = (values) => {
         if (values.errorFields.length === 0) {
@@ -87,7 +87,7 @@ const ChangePasswordBase = ({ showGlobalNotification, isOpen = false, onOk = () 
     };
 
     const validateToNextPassword = (rule, value, callback) => {
-        if (value && confirmDirty) {
+        if (value) {
             form.validateFields(['confirmNewPassword'], { force: true });
         }
         callback();
@@ -101,16 +101,17 @@ const ChangePasswordBase = ({ showGlobalNotification, isOpen = false, onOk = () 
         }
     };
 
-    const handleConfirmBlur = (e) => {
-        const value = e.target.value;
-        setConfirmDirty(confirmDirty || !!value);
-    };
+    const passwordSuffix = (type) => (
+        <span onMouseDown={() => setShowPassword({ [type]: true })} onMouseUp={() => setShowPassword({ [type]: false })} onMouseLeave={() => setShowPassword({ [type]: false })}>
+            {!showPassword?.[type] ? <AiOutlineEyeInvisible size={18} /> : <AiOutlineEye size={18} />}
+        </span>
+    );
     return (
         <Form className={styles.changePasswordForm} form={form} name="change_password" layout="vertical" autoComplete="false" onFinish={onFinish} onFinishFailed={onFinishFailed}>
             <Row gutter={20}>
                 <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                     <Form.Item label="Old Password" name="oldPassword" rules={[validateRequiredInputField('Old Password')]}>
-                        <Input.Password type="text" placeholder="Enter old password" visibilityToggle={true} />
+                        <Input type={showPassword?.oldPassword ? 'text' : 'password'} placeholder="Enter old password" suffix={passwordSuffix('oldPassword')} />
                     </Form.Item>
                 </Col>
             </Row>
@@ -127,7 +128,7 @@ const ChangePasswordBase = ({ showGlobalNotification, isOpen = false, onOk = () 
                             },
                         ]}
                     >
-                        <Input.Password type="text" placeholder="Enter new password" visibilityToggle={true} />
+                        <Input type={showPassword?.newPassword ? 'text' : 'password'} placeholder="Enter new password" suffix={passwordSuffix('newPassword')} />
                     </Form.Item>
                 </Col>
             </Row>
@@ -138,13 +139,12 @@ const ChangePasswordBase = ({ showGlobalNotification, isOpen = false, onOk = () 
                         name="confirmNewPassword"
                         rules={[
                             validateRequiredInputField('Confirm Password'),
-                            validateFieldsPassword('Confirm Password'),
                             {
                                 validator: compareToFirstPassword,
                             },
                         ]}
                     >
-                        <Input.Password type="text" placeholder="Enter confirm password" onBlur={handleConfirmBlur} visibilityToggle={true} />
+                        <Input type={showPassword?.confirmNewPassword ? 'text' : 'password'} placeholder="Enter confirm password" suffix={passwordSuffix('confirmNewPassword')} />
                     </Form.Item>
                 </Col>
             </Row>
