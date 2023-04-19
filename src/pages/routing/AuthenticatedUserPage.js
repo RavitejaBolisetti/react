7651cae99374b9bc1a38cd9f50a37f9149eb2118
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-04-19 13:19:40 +0530

    dealer and manufacturer routing
    wip #581

--- src/pages/routing/AuthenticatedUserPage.js
+++ src/pages/routing/AuthenticatedUserPage.js
@@ -43,6 +43,7 @@ export const AuthenticatedUserPage = () => {
             <Route path={routing.ROUTING_REPORT_PAGINATED_REPORT} element={<PaginatedReportPage />} exact />
             <Route path={routing.ROUTING_USER_TRAINING} element={<TrainingPage />} exact />
             <Route path={routing.ROUTING_USER_MANAGEMENT_DEALER} element={<UserManagementPage />} exact />
+            <Route path={routing.ROUTING_USER_MANAGEMENT_MANUFACTURER} element={<UserManagementPage />} exact />
 
             <Route path={routing.ROUTING_USER_TERM} element={<CMSPage />} exact />
             <Route path={routing.ROUTING_USER_ABOUT} element={<CMSPage />} exact />
