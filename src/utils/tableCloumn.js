694e57712e8085b06f637738951e95f13f217eca
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-04-21 22:23:32 +0530

    Fix desing fixes
    wip #UI-Design-Fixes

--- src/utils/tableCloumn.js
+++ src/utils/tableCloumn.js
@@ -8,7 +8,7 @@ const onFilterFn = (value, record) => {
     }
 };
 
-export const tblPrepareColumns = ({ title, dataIndex, render = undefined, ellipsis = false, filters = undefined, filterMode = 'tree', filterSearch = true, sortFn = undefined, width, sorter=true }) => {
+export const tblPrepareColumns = ({ title, dataIndex, render = undefined, ellipsis = false, filters = undefined, filterMode = 'tree', filterSearch = true, sortFn = undefined, width, sorter = true }) => {
     return {
         title,
         dataIndex,
@@ -19,7 +19,7 @@ export const tblPrepareColumns = ({ title, dataIndex, render = undefined, ellips
         filterMode,
         filterSearch,
         onFilter: onFilterFn,
-        sorter: (sorter && ((a, b) => a[dataIndex].localeCompare(b[dataIndex], undefined, { sensitivity: 'base' }))),
+        sorter: sorter && ((a, b) => (a && b ? String(a[dataIndex]).localeCompare(String(b[dataIndex]), undefined, { sensitivity: 'base' }) : a)),
         sortDirections: ['descend', 'ascend'],
     };
 };
