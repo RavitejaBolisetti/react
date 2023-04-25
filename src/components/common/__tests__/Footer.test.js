d47f60725eba47b13bc2a505ae3e43338975e6f1
Vivek Verma <Vivek.verma.ext@wipro.com>
2023-04-25 20:19:20 +0530

    Footer Test
    wip #usUnitTesting

--- src/components/common/__tests__/Footer.test.js
+++ src/components/common/__tests__/Footer.test.js
@@ -1,12 +1,11 @@
 import { render, screen } from '@testing-library/react';
 
 import { Footer } from '../Footer/Footer';
-import Copyright from './Copyright';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 jest.mock('react-redux', () => ({
-    connect: () => (Dashboard) => Dashboard,
+    connect: () => (Footer) => Footer,
 }));
 
 window.matchMedia =
@@ -20,54 +19,19 @@ describe('Testing DashBoard', () => {
         render(
             <BrowserRouter>
                 <Routes>
-                    <Route path="*" element={<Dashboard />} />
+                    <Route path="*" element={<Footer />} />
                 </Routes>
             </BrowserRouter>
         );
-        const HomeText = await screen.findByText('Home');
-        const DashboardCardTitle = await screen.findByText('Dashboard');
-        const ViewDashboardButton = await screen.findByText('View Dashboard');
-        const ActionItems = await screen.findByText('Action Items');
-        const BirthdayCalendar = await screen.findByText('Birthday Calendar');
-        const Enquiriestobefollowedup = await screen.findByText('Enquiries to be followed up');
-        const PendingPOstobereleased = await screen.findByText('Pending POs to be released');
-        const Vehiclestobedelivered = await screen.findByText('Vehicles to be delivered');
-        const News = await screen.findByText('News');
-        const anandMahindra = await screen.findByText('Anand Mahindra Highlights Car Price Hikes Over 50 Years Ago');
-        const AnandMahindra = await screen.findByText('Anand Mahindra continues to amuse the netizens with his posts. This time he has highlighted car price hikes from 50 years ago.');
-        const ViewDashboard = await screen.findByText('View Dashboard');
-        const ElectriSuv = await screen.findByText("What to expect from Mahindra's' Born Electric concept SUVs");
-        const Sales66 = await screen.findByText('Mahindra SUV sales up 66%');
-        const UpcomingTrainings = await screen.findByText('Upcoming Trainings');
-        const FirstNameLastName = await screen.findByText('First Name, Last Name');
-        const Today = await screen.findByText('Today');
-        const KnowledgeCenter = await screen.findByText('Knowledge Center');
-
-        expect(HomeText).toBeTruthy();
-        expect(DashboardCardTitle).toBeTruthy();
-        expect(ViewDashboardButton).toBeTruthy();
-        expect(ActionItems).toBeTruthy();
-        expect(BirthdayCalendar).toBeTruthy();
-        expect(Enquiriestobefollowedup).toBeTruthy();
-        expect(PendingPOstobereleased).toBeTruthy();
-        expect(Vehiclestobedelivered).toBeTruthy();
-        expect(News).toBeTruthy();
-        expect(anandMahindra).toBeTruthy();
-        expect(AnandMahindra).toBeTruthy();
-        expect(ViewDashboard).toBeTruthy();
-        expect(ElectriSuv).toBeTruthy();
-        expect(Sales66).toBeTruthy();
-        expect(UpcomingTrainings).toBeTruthy();
-        expect(FirstNameLastName).toBeTruthy();
-        expect(Today).toBeTruthy();
-        expect(KnowledgeCenter).toBeTruthy();
+        const FooterText = await screen.findByText('Copyright © 2023 ROBIN.');
+        expect(FooterText).toBeTruthy();
     });
 
     test('Testing Routing', async () => {
         render(
             <BrowserRouter>
                 <Routes>
-                    <Route path="*" element={<Dashboard />} />
+                    <Route path="*" element={<Footer />} />
                 </Routes>
             </BrowserRouter>
         );
