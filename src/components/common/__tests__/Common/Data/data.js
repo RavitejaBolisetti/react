c917248e82abff7f44c04dc46892ad2ab5b00f09
Kartik Gupta <KA20371579@wipro.com>
2023-04-25 17:56:31 +0530

    Worked on product hierarchy test cases
    WIP # US-UNIT-TESTING

--- src/components/common/__tests__/Common/Data/data.js
+++ src/components/common/__tests__/Common/Data/data.js
@@ -84,3 +84,90 @@ export const ManufacturerTreeData = [
         ],
     },
 ];
+
+export const ProductDatas = [
+    {
+        geoName: 'parent 1',
+        id: 'parent 1',
+        subGeo: [
+            {
+                title: 'node1',
+                key: 'node1',
+
+                children: [
+                    {
+                        title: 'randomNode_1',
+                        key: 'randomNode_1',
+                    },
+                    {
+                        title: 'node2',
+                        key: 'node2',
+
+                        children: [
+                            {
+                                title: 'randomNode_2',
+                                key: 'randomNode_2',
+
+                                children: [
+                                    {
+                                        title: 'node2',
+                                        key: 'node2',
+
+                                        children: [
+                                            {
+                                                title: 'randomNode_3',
+                                                key: 'randomNode_3',
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: 'topNode',
+        key: 'topNode',
+        children: [
+            {
+                title: 'node1',
+                key: 'node1',
+
+                children: [
+                    {
+                        title: 'randomNode_1',
+                        key: 'randomNode_1',
+                    },
+                    {
+                        title: 'node2',
+                        key: 'node2',
+
+                        children: [
+                            {
+                                title: 'randomNode_2',
+                                key: 'randomNode_2',
+
+                                children: [
+                                    {
+                                        title: 'node2',
+                                        key: 'node2',
+
+                                        children: [
+                                            {
+                                                title: 'randomNode_3',
+                                                key: 'randomNode_3',
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
