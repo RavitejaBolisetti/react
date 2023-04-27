9060173f6e15f66f83586f3afe452c9d248c1755
Simran <Simran.sehgal@wipro.com>
2023-04-27 19:25:55 +0530

    Geo State page  implementation

--- src/components/common/Geo/State/AddEditForm.js
+++ src/components/common/Geo/State/AddEditForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Col, Input, Form, Row, Select, Button, InputNumber, DatePicker } from 'antd';
+import { Col, Input, Form, Row, Select, Button, InputNumber,Switch, DatePicker } from 'antd';
 import { validateRequiredInputField, validateRequiredSelectField } from 'utils/validation';
 import { withDrawer } from 'components/withDrawer';
 import { convertCalenderDate, convertDate } from 'utils/formatDateTime';
+import { preparePlaceholderSelect, preparePlaceholderText } from 'utils/preparePlaceholder';
+
 
 import { PARAM_MASTER } from 'constants/paramMaster';
-import { CONFIGURABLE_PARAMETARS_INPUT_TYPE } from '../Geo/State/InputType';
-import { ViewConfigDetails } from './ViewConfigDetails'
+// import { CONFIGURABLE_PARAMETARS_INPUT_TYPE } from '../Geo/State/InputType';
+import { ViewStateDetails } from './ViewStateDetails'
 
 import styles from 'components/common/Common.module.css';
 
@@ -17,6 +19,8 @@ const AddEditFormMain = (props) => {
     const { typeData, configData, parameterType, setParameterType, hanndleEditData, setSaveAndAddNewBtnClicked } = props;
     const { footerEdit, form,setClosePanels, isReadOnly, showSaveBtn, formData, onCloseAction, isViewModeVisible, setisViewModeVisible } = props;
     const { isFormBtnActive, setFormBtnActive, onFinish, onFinishFailed } = props;
+    const disabledProps = { disabled: isReadOnly };
+
 
     const handleFormValueChange = () => {
         setFormBtnActive(true);
@@ -42,7 +46,6 @@ const AddEditFormMain = (props) => {
         setClosePanels,
         formData,
         styles,
-        parameterType
     };
 
 
@@ -52,87 +55,38 @@ const AddEditFormMain = (props) => {
                 <>
                     <Row gutter={16}>
                         <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                            <Form.Item initialValue={formData?.controlId} label="Control ID" name="controlId" rules={[validateRequiredInputField('ControlID')]}>
-                                <Select placeholder="Select" showSearch allowClear onChange={handleControlChange} disabled={isReadOnly}>
-                                    {typeData && typeData[PARAM_MASTER.CFG_PARAM.id] && typeData[PARAM_MASTER.CFG_PARAM.id]?.map((item) => <Option value={item?.key}>{item?.value}</Option>)}
-                                </Select>
+                            <Form.Item initialValue={formData?.stateCd} label="State Code" name="stateCd" rules={[validateRequiredInputField('State Code')]}>
+                            <Input className={styles.inputBox} placeholder={preparePlaceholderText('State Code')} maxLength={50} {...disabledProps} />
                             </Form.Item>
                         </Col>
+                        </Row>
+                        <Row>
                         <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                            <Form.Item label="Control Description" initialValue={formData?.controlDescription} rules={[validateRequiredInputField('Control Description')]} name="controlDescription">
-                                <TextArea rows={2} value={formData?.controlDescription} placeholder="Enter Data" disabled={isReadOnly} />
+                            <Form.Item label="State Name" initialValue={formData?.stateName} rules={[validateRequiredInputField('State Name')]} name="stateName">
+                            <Input className={styles.inputBox} placeholder={preparePlaceholderText('State Name')} maxLength={50} {...disabledProps} />
                             </Form.Item>
                         </Col>
                     </Row>
                     <Row gutter={16}>
-                        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                            <Form.Item label="Control Group" initialValue={formData?.controlGroup} name="controlGroup" rules={[validateRequiredSelectField('controlGroup')]}>
-                                <Select disabled={isReadOnly} placeholder="Select">
-                                    {typeData && typeData[PARAM_MASTER.CTRL_GRP.id] && typeData[PARAM_MASTER.CTRL_GRP.id]?.map((item) => <Option value={item?.key}>{item?.value}</Option>)}
-                                </Select>
+                        <Col  xs={24} sm={24} md={24} lg={24} xl={24}>
+                            <Form.Item label="GST State Code" initialValue={formData?.gstCode} name="gstCode" rules={[validateRequiredSelectField('GST State Code')]}>
+                            <Input className={styles.inputBox} placeholder={preparePlaceholderText('GST State Code')} maxLength={50} {...disabledProps} />
                             </Form.Item>
                         </Col>
-                        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                            <Form.Item initialValue={parameterType} label="Configurable Parameter Type" name="configurableParameterType" rules={[validateRequiredSelectField('ConfigParamType')]}>
-                                <Select placeholder="Select Parameter Type" onChange={changeSelectOptionHandler} disabled={isReadOnly}>
-                                    {typeData && typeData[PARAM_MASTER.CFG_PARAM_TYPE.id] && typeData[PARAM_MASTER.CFG_PARAM_TYPE.id]?.map((item) => <Option value={item?.key}>{item?.value}</Option>)}
-                                </Select>
-                            </Form.Item>
+                        </Row>
+                        <Row gutter ={16}>
+                        <Col  xs={24} sm={24} md={24} lg={24} xl={24}>
+                        <Form.Item initialValue={true} labelAlign="left" wrapperCol={{ span: 24 }} valuePropName="checked" name="status" label="Status">
+                            <Switch checkedChildren="Active" unCheckedChildren="Inactive" onChange={(checked) => (checked ? 1 : 0)} {...disabledProps} />
+                        </Form.Item>
                         </Col>
                     </Row>
 
-                    <Row gutter={20}>
-                        <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                            {parameterType && parameterType === CONFIGURABLE_PARAMETARS_INPUT_TYPE.TEXT.KEY ? (
-                                <Form.Item initialValue={formData?.textValue} label="Configurable Parameter Values" name="textValue" rules={[validateRequiredInputField('ConfigParamValues')]}>
-                                    <Input value={configData?.textValue} placeholder="Enter Data" disabled={isReadOnly} />
-                                </Form.Item>
-                            ) : parameterType && parameterType === CONFIGURABLE_PARAMETARS_INPUT_TYPE.NUMBER.KEY ? (
-                                <Row gutter={20}>
-                                    <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                                        <Form.Item label="From Number" className={styles.numberRange} initialValue={formData?.fromNumber} name="fromNumber" rules={[validateRequiredInputField('Number')]}>
-                                            <InputNumber min={0} max={100} placeholder="From Number" style={{ display: 'auto', width: '100%' }} disabled={isReadOnly} />
-                                        </Form.Item>
-                                    </Col>
-
-                                    <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                                        <Form.Item label="To Number" className={styles.numberRange} initialValue={formData?.toNumber} name="toNumber" rules={[validateRequiredInputField('Number')]}>
-                                            <InputNumber min={0} max={100} placeholder="To Number" style={{ display: 'auto', width: '100%' }} disabled={isReadOnly} />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
-                            ) : parameterType && parameterType === CONFIGURABLE_PARAMETARS_INPUT_TYPE.DATE_RANGE.KEY ? (
-                                <Row gutter={20}>
-                                    <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                                        <Form.Item label="From Date" name="fromDate" initialValue={convertCalenderDate(formData?.fromDate)} rules={[validateRequiredInputField('Number')]}>
-                                            <DatePicker format="DD-MM-YYYY" style={{ display: 'auto', width: '100%' }} disabled={isReadOnly} />
-                                        </Form.Item>
-                                    </Col>
-
-                                    <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                                        <Form.Item label="To Date" name="toDate" initialValue={convertCalenderDate(formData?.toDate)} rules={[validateRequiredInputField('Number')]}>
-                                            <DatePicker format="DD-MM-YYYY" style={{ display: 'auto', width: '100%' }} disabled={isReadOnly} />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
-                            ) : parameterType && parameterType === CONFIGURABLE_PARAMETARS_INPUT_TYPE.BOOLEAN.KEY ? (
-                                <Form.Item initialValue={formData?.booleanValue} name="booleanValue" label="Configurable Parameter Values" rules={[validateRequiredInputField('ConfigParamValues')]}>
-                                    <Select
-                                        placeholder="Select"
-                                        options={[
-                                            { value: true, label: 'Yes' },
-                                            { value: false, label: 'No' },
-                                        ]}
-                                        disabled={isReadOnly}
-                                    />
-                                </Form.Item>
-                            ) : null}
-                        </Col>
-                    </Row>
+                    
                 </>
             ) : (
-                // <ViewConfigDetails {...viewProps} />
-                null
+                <ViewStateDetails {...viewProps} />
+                
             )}
 
             <Row gutter={20} className={styles.formFooter}>
--- src/components/common/Geo/State/StateGeo.js
+++ src/components/common/Geo/State/StateGeo.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Button, Col, Input, Form, Row, Space, Empty, ConfigProvider } from 'antd';
+import { Button, Col, Input, Form, Row, Space, Empty, ConfigProvider ,Select} from 'antd';
 import { bindActionCreators } from 'redux';
 import { configParamEditActions } from 'store/actions/data/configurableParamterEditing';
 import { CONFIGURABLE_PARAMETARS_INPUT_TYPE } from './InputType';
+import style from 'components/common/ProductHierarchy/producthierarchy.module.css'
 import { tblPrepareColumns } from 'utils/tableCloumn';
+
 import { DataTable } from 'utils/dataTable';
 import { filterFunction } from 'utils/filterFunction';
 import { PARAM_MASTER } from 'constants/paramMaster';
@@ -20,6 +22,7 @@ import { FaRegEye } from 'react-icons/fa';
 import styles from 'components/common/Common.module.css';
 
 const { Search } = Input;
+const { Option } = Select;
 
 // const mapStateToProps = (state) => {
 //     const {
@@ -114,13 +117,13 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
         setFormActionType('update');
         setFooterEdit(false);
         setIsReadOnly(false);
-        const data = configData.find((i) => i.id === record.id);
+        const data = tableData.find((i) => i.id === record.id);
         console.log('data', data);
         if (data) {
             data && setFormData(data);
             console.log('formData', formData);
 
-            setParameterType((data?.configurableParameterType).toString() || defaultParametarType);
+           
             setIsFormVisible(true);
         }
     };
@@ -131,41 +134,18 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
 
         setShowSaveAndAddNewBtn(false);
         setFooterEdit(true);
-        const data = configData.find((i) => i.id === record.id);
+        const data = tableData.find((i) => i.id === record.id);
         if (data) {
             data && setFormData(data);
-            setParameterType((data?.configurableParameterType).toString() || defaultParametarType);
             setIsFormVisible(true);
         }
 
         setIsReadOnly(true);
     };
 
-    const renderTableColumnName = (record, key, type) => {
-        return typeData && typeData[type]?.find((item) => item?.key === record?.[key])?.value;
-    };
+   
 
-    const renderConfigurableParemetarValue = (record) => {
-        let fieldType = '';
-        switch (record?.configurableParameterType) {
-            case CONFIGURABLE_PARAMETARS_INPUT_TYPE.TEXT.KEY:
-                fieldType = record?.textValue;
-                break;
-            case CONFIGURABLE_PARAMETARS_INPUT_TYPE.NUMBER.KEY:
-                fieldType = fieldType.concat(record?.fromNumber).concat(' - ').concat(record?.toNumber);
-                break;
-            case CONFIGURABLE_PARAMETARS_INPUT_TYPE.DATE_RANGE.KEY:
-                fieldType = fieldType.concat(record?.fromDate).concat('  ').concat(record?.toDate);
-                break;
-            case CONFIGURABLE_PARAMETARS_INPUT_TYPE.BOOLEAN.KEY:
-                fieldType = record?.booleanValue ? 'Yes' : 'No';
-                break;
-            default:
-                fieldType = undefined;
-                break;
-        }
-        return fieldType;
-    };
+   
 
     const tableColumn = [];
     tableColumn.push(
@@ -178,32 +158,29 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
 
         tblPrepareColumns({
             title: 'State Code',
-            dataIndex: 'controlId',
-            render: (text, record, value) => renderTableColumnName(record, 'controlId', PARAM_MASTER.CFG_PARAM.id),
+            dataIndex: 'stateCd',
             width: '15%',
         }),
 
         tblPrepareColumns({
             title: 'State Name',
-            dataIndex: 'controlDescription',
+            dataIndex: 'stateName',
             width: '20%',
         }),
 
         tblPrepareColumns({
             title: 'GST State Code',
-            dataIndex: 'configurableParameterType',
-            render: (text, record, value) => renderTableColumnName(record, 'configurableParameterType', PARAM_MASTER.CFG_PARAM_TYPE.id),
+            dataIndex: 'gstCode',
             width: '20%',
         }),
 
         tblPrepareColumns({
             title: 'Status',
-            dataIndex: 'activeIndicator',
+            dataIndex: 'status',
             render: (text, record) => <>{text === 1 ? <div className={styles.activeText}>Active</div> : <div className={styles.inactiveText}>Inactive</div>}</>,
             width: '15%',
         }),
 
-    
         {
             title: 'Action',
             dataIndex: '',
@@ -211,12 +188,12 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
             render: (record) => [
                 <Space wrap>
                     {
-                        <Button data-testid='edit' className={styles.tableIcons} aria-label='fa-edit' onClick={() => handleEditBtn(record, 'edit')}>
+                        <Button data-testid="edit" className={styles.tableIcons} aria-label="fa-edit" onClick={() => handleEditBtn(record, 'edit')}>
                             <FiEdit2 />
                         </Button>
                     }
                     {
-                        <Button className={styles.tableIcons} aria-label='ai-view' onClick={() => handleView(record)}>
+                        <Button className={styles.tableIcons} aria-label="ai-view" onClick={() => handleView(record)}>
                             <FaRegEye />
                         </Button>
                     }
@@ -224,6 +201,21 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
             ],
         }
     );
+
+    const tableData = [
+        {
+            id : '1',
+
+            stateCd: 'Test50',
+
+            stateName: 'Test50',
+
+            gstCode: 'Test50',
+
+            status: 1,
+
+        },
+    ];
 
     const handleReferesh = () => {
         setRefershData(!refershData);
@@ -292,11 +284,11 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
     };
 
     const onFinishFailed = (errorInfo) => {
-        form.validateFields().then((values) => { });
+        form.validateFields().then((values) => {});
     };
     const tableProps = {
         tableColumn: tableColumn,
-        tableData: searchData,
+        tableData: tableData,
     };
 
     const formProps = {
@@ -311,7 +303,7 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
         typeData,
         isVisible: isFormVisible,
         onCloseAction: () => (setIsFormVisible(false), setFormBtnActive(false)),
-        titleOverride: (isViewModeVisible ? 'View ' : formData?.id ? 'Edit ' : 'Add ').concat(moduleTitle),
+        titleOverride: (isViewModeVisible ? 'View ' : formData?.id ? 'Edit ' : 'Add ').concat('State Details'),
         onFinish,
         onFinishFailed,
         isFormBtnActive,
@@ -330,32 +322,22 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
             <Row gutter={20}>
                 <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                     <div className={styles.contentHeaderBackground}>
-                        <Row gutter={20}>
-                            <Col xs={24} sm={24} md={16} lg={16} xl={16}>
+                    <Row gutter={20} className={styles.searchAndLabelAlign}>
+                            <Col xs={24} sm={24} md={19} lg={19} xl={19} className={style.subheading}>
                                 <Row gutter={20}>
-                                    <div className={styles.searchBox}>
-                                        <Col xs={24} sm={24} md={24} lg={24} xl={24} className={styles.subheading}>
-                                            Configurable Parameter Editing
-                                            <Search
-                                                placeholder="Search"
-                                                style={{
-                                                    width: 300,
-                                                }}
-                                                allowClear
-                                                className={styles.headerSelectField}
-                                                onSearch={onSearchHandle}
-                                                onChange={onChangeHandle}
-                                            />
-                                        </Col>
-                                        {/* <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                            
-                                        </Col> */}
-                                    </div>
+                                    <Col xs={24} sm={24} md={6} lg={6} xl={6} className={styles.lineHeight33}>
+                                        State List
+                                    </Col>
+                                    <Col xs={24} sm={24} md={18} lg={18} xl={18}>
+                                        <Select placeholder="Country" allowClear className={styles.headerSelectField}>
+                                            <Option value="India">India</Option>
+                                        </Select>
+                                        <Search placeholder="Search" allowClear onChangeHandle={onChangeHandle} className={styles.headerSearchField} />
+                                    </Col>
                                 </Row>
                             </Col>
-
-                            {configData?.length ? (
-                                <Col className={styles.addGroup} xs={24} sm={24} md={8} lg={8} xl={8}>
+                            {tableData?.length ? (
+                                <Col className={styles.addGroup} xs={24} sm={24} md={24} lg={24} xl={24}>
                                     <Button icon={<TfiReload />} className={styles.refreshBtn} onClick={handleReferesh} danger />
 
                                     <Button icon={<PlusOutlined />} className={`${styles.actionbtn} ${styles.lastheaderbutton}`} type="primary" danger onClick={handleAdd}>
@@ -365,7 +347,8 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
                             ) : (
                                 ''
                             )}
-                        </Row>
+                    </Row>
+                        
                     </div>
                 </Col>
             </Row>
@@ -379,7 +362,7 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
                                     height: 60,
                                 }}
                                 description={
-                                    !configData?.length ? (
+                                    !tableData?.length ? (
                                         <span>
                                             No records found. Please add new parameter <br />
                                             using below button
@@ -389,7 +372,7 @@ export const StateGeoBase = ({ moduleTitle, fetchDataList, isLoading, saveData, 
                                     )
                                 }
                             >
-                                {!configData?.length ? (
+                                {!tableData?.length ? (
                                     <Row>
                                         <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                                             <Button icon={<PlusOutlined />} className={styles.actionbtn} type="primary" danger onClick={handleAdd}>
