6386c77307645c1b1729b7c8155682211eb937ca
Simran <Simran.sehgal@wipro.com>
2023-05-06 18:20:00 +0530

    Update data table

--- src/utils/dataTable/DataTable.js
+++ src/utils/dataTable/DataTable.js
@@ -5,7 +5,7 @@ import { InputSkeleton } from 'components/common/Skeleton';
 export default function DataTable({ isLoading, tableColumn, tableData }) {
     const showTotal = (total) => `Total ${!isLoading ? tableData?.length : null} items`;
 
-    const [tablePagination, setPagination] = useState({ pageSize: 10, current: 1, position: ['bottomRight'], showSizeChanger: true, hideOnSinglePage: true, showTotal });
+    const [tablePagination, setPagination] = useState({ pageSize: 10, current: 1, position: ['bottomRight'], showSizeChanger: true, hideOnSinglePage: false, showTotal });
 
     const handleTableChange = (pagination, filters, sorter) => {
         setPagination({ ...pagination, showTotal });
@@ -21,52 +21,14 @@ export default function DataTable({ isLoading, tableColumn, tableData }) {
         setPagination({ ...tablePagination, current: page });
     };
     return (
-        <>
-            <Table
-                columns={isLoading ? tableSkeletonColumn : tableColumn}
-                dataSource={isLoading ? skeletonData : tableData}
-                onChange={handleTableChange}
-                pagination={tablePagination}
-                scroll={{
-                    x: 'auto',
-                }}
-            />
-            {/* <Row>
-                <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                    <Select
-                        defaultValue="10"
-                        style={{
-                            width: 120,
-                        }}
-                        options={[
-                            {
-                                value: '10',
-                                label: '10 / Page',
-                            },
-                            {
-                                value: '20',
-                                label: '20 / Page',
-                            },
-                            {
-                                value: '30',
-                                label: '30 / Page',
-                            },
-                            {
-                                value: '50',
-                                label: '50 / Page',
-                            },
-
-                            {
-                                value: '100',
-                                label: '100 / Page',
-                            },
-                        ]}
-                    />
-                </Col>
-                <Col xs={24} sm={12} md={12} lg={12} xl={12} style={{ float: 'right' }}>
-                    <Pagination current={tablePagination?.current} total={tableData?.length} hideOnSinglePage={true} pagination={tableData?.length > 10 ? tablePagination : false} showSizeChanger={false} onChange={onChange} showTotal={showTotal} />
-                </Col> 
-            </Row>*/}
-        </>
+        <Table
+            columns={isLoading ? tableSkeletonColumn : tableColumn}
+            dataSource={isLoading ? skeletonData : tableData}
+            onChange={handleTableChange}
+            pagination={tablePagination}
+            scroll={{
+                x: 'auto',
+            }}
+        />
     );
 }
