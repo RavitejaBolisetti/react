8a8841fe76f0d296840dfc4886056b0b6a8e8fa9
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-05-09 16:05:24 +0530

    Added space in file

--- src/pages/common/Geo/index.js
+++ src/pages/common/Geo/index.js
@@ -2,6 +2,6 @@ import StatePage from './StatePage';
 import DistrictPage from './DistrictPage';
 import TehsilPage from './TehsilPage';
 import CityPage from './CityPage';
-import PinCodePage from './PinCodePage';
+import PinCodePage from './PinCodePage'; 
 
 export {  StatePage, DistrictPage, TehsilPage, CityPage, PinCodePage };
