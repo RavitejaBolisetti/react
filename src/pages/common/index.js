e5a4beb61ffcf61da1ba02c147e286c6d1f0cbd0
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-05-09 15:57:25 +0530

    Fixed issue on data refresh
    wip #3121

--- src/pages/common/index.js
+++ src/pages/common/index.js
@@ -1,11 +1,6 @@
 import { ProductMasterPage } from './ProductMaster/ProductMasterPage';
 import { RoleManagementPage } from './RoleManagement/RoleManagementPage';
 import { ProductHierarchyPage } from './ProductHierarchy';
-// import { StatePage } from './Geo/StatePage';
-// import { DistrictPage } from './Geo/DistrictPage';
-// import { CityPage } from './Geo/CityPage';
-// import { TehsilPage } from './Geo/TehsilPage';
-// import { PinCodePage } from './Geo/PinCodePage';
 import { StatePage, DistrictPage, TehsilPage, CityPage, PinCodePage } from './Geo';
 
 import { CriticalityGroupPage } from './CriticalityGroup';
@@ -17,10 +12,10 @@ import { ManufacturerOrgHierarchyPage } from './ManufacturerOrganizationHierarch
 import { DealerHierarchyPage } from './DealerHierarchy';
 
 import { DealerLocationTypePage } from './DealerManpower';
-import {BayTypeMasterPage } from './DealerManpower'
+import { BayTypeMasterPage } from './DealerManpower';
 import { ConfigurableParameterEditingPage } from './ConfigurableParameterEditing';
 import { ApplicationMasterPage } from './ApplicationMasterPage';
 import { UserManagementPage } from './UserManagementPage';
 import { UserManagementManufacturerPage } from './UserManagementManufacturer';
 
-export { ProductMasterPage, ProductHierarchyPage, ManufacturerAdministrativeHierarchyPage, ManufacturerOrgHierarchyPage, DealerHierarchyPage, ConfigurableParameterEditingPage, ApplicationMasterPage, CriticalityGroupPage, RoleManagementPage, QualificationMasterPage, HierarchyAttributeMasterPage, UserManagementPage, UserManagementManufacturerPage, StatePage, DistrictPage, CityPage, TehsilPage, PinCodePage, DealerLocationTypePage,BayTypeMasterPage };
+export { ProductMasterPage, ProductHierarchyPage, ManufacturerAdministrativeHierarchyPage, ManufacturerOrgHierarchyPage, DealerHierarchyPage, ConfigurableParameterEditingPage, ApplicationMasterPage, CriticalityGroupPage, RoleManagementPage, QualificationMasterPage, HierarchyAttributeMasterPage, UserManagementPage, UserManagementManufacturerPage, StatePage, DistrictPage, CityPage, TehsilPage, PinCodePage, DealerLocationTypePage, BayTypeMasterPage };
