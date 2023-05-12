1fb3626c2b28ddc3c75482a4db9a5adb5cc38d0d
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-05-12 12:40:43 +0530

    Added search functionality

--- src/components/common/Geo/Pincode/tableColumn.js
+++ src/components/common/Geo/Pincode/tableColumn.js
@@ -1,4 +1,4 @@
-import { tblPrepareColumns, tblSerialNumberColumn, tblStatusColumn,tblApprovalStatusColumn, tblActionColumn } from 'utils/tableCloumn';
+import { tblPrepareColumns, tblSerialNumberColumn, tblStatusColumn, tblApprovalStatusColumn, tblActionColumn } from 'utils/tableCloumn';
 import { Checkbox } from 'antd';
 
 import styles from 'components/common/Common.module.css';
@@ -6,7 +6,7 @@ import styles from 'components/common/Common.module.css';
 export const tableColumn = (handleButtonClick, page, pageSize) => {
     const tableColumn = [];
     tableColumn.push(
-        tblSerialNumberColumn({ page, pageSize }),
+        tblSerialNumberColumn({ page, pageSize, width: '10%', fixed: 'left' }),
 
         tblPrepareColumns({
             title: 'PIN Category',
@@ -17,19 +17,19 @@ export const tableColumn = (handleButtonClick, page, pageSize) => {
         tblPrepareColumns({
             title: 'PIN Code',
             dataIndex: 'pinCode',
-            width: '15%',
+            width: '35%',
         }),
 
         tblPrepareColumns({
             title: 'Locality',
             dataIndex: 'localityName',
-            width: '20%',
+            width: '35%',
         }),
 
         tblPrepareColumns({
             title: 'Within 50Km from the GPO',
             dataIndex: 'withIn50KmFromGpo',
-            width: '20%',
+            width: '35%',
             render: (text, record) => {
                 return <Checkbox disabled defaultChecked={text ? true : false} className={styles.registered}></Checkbox>;
             },
@@ -44,9 +44,9 @@ export const tableColumn = (handleButtonClick, page, pageSize) => {
 
         tblApprovalStatusColumn({ styles }),
 
-        tblStatusColumn({ styles }),
+        tblStatusColumn({ styles, fixed: 'right' }),
 
-        tblActionColumn({ handleButtonClick, styles })
+        tblActionColumn({ handleButtonClick, styles, fixed: 'right', width: '15%' })
     );
 
     return tableColumn;
