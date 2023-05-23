0dcec4e82fd9021858ec52aa8bca60948936e326
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-05-23 16:58:52 +0530

    footer removed from withdrawer and common module css
    wip

--- src/components/common/CustomerMaster/IndividualCustomer/CustomerDetails/IndivisualCustomerDetailsMaster.js
+++ src/components/common/CustomerMaster/IndividualCustomer/CustomerDetails/IndivisualCustomerDetailsMaster.js
@@ -10,10 +10,10 @@ const { Option } = Select;
 
 const IndivisualCustomerDetailsMaster = (props) => {
     return (
-        <>
+        <div className={styles.drawerCustomerMaster}>
             <h2>Customer Details</h2>
             <AddEditForm {...props} />
-        </>
+        </div>
     );
 };
 
