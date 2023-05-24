853216a8996220688fe88c83f503fb84b78c4ea9
Simran <Simran.sehgal@wipro.com>
2023-05-24 19:41:57 +0530

    changes

--- src/components/common/Geo/District/AddEditForm.js
+++ src/components/common/Geo/District/AddEditForm.js
@@ -83,7 +83,7 @@ const AddEditFormMain = (props) => {
 
                     <Row gutter={16}>
                         <Col xs={24} sm={12} md={12} lg={12} xl={12}>
-                            <Form.Item initialValue={formData?.code} label="District Code" name="code" rules={[validateRequiredInputField('district Code'), validationFieldLetterAndNumber('District Code')]}>
+                            <Form.Item initialValue={formData?.code} label="District Code" name="code" rules={[validateRequiredInputField('district Code'), validateLettersWithWhitespaces('District Code')]}>
                                 <Input placeholder={preparePlaceholderText('District Code')} className={styles.inputBox} maxLength={6} disabled={editMode ? true : false} />
                             </Form.Item>
                         </Col>
