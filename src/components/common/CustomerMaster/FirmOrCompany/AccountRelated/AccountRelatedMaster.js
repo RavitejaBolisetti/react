a9323df1192b394c26cb8265b243b4c849184e64
Kartik Gupta <KA20371579@wipro.com>
2023-05-24 10:47:34 +0530

    Done Changes In Account Related
    WIP  #US-Customer-Master-development-Structure

--- src/components/common/CustomerMaster/FirmOrCompany/AccountRelated/AccountRelatedMaster.js
+++ src/components/common/CustomerMaster/FirmOrCompany/AccountRelated/AccountRelatedMaster.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 
-import { Col, Collapse, Form, Select, Space, Typography } from 'antd';
+import { Col, Collapse, Form, Row, Button, Select, Space, Typography } from 'antd';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import { FaRegUserCircle } from 'react-icons/fa';
 import { AddEditForm } from './AddEditForm';
+import { ViewDetail } from './ViewAccountDetails';
+
 import style from '../../../Common.module.css';
 import styles from 'components/common/Common.module.css';
 
@@ -13,16 +15,52 @@ const { Text } = Typography;
 
 const AccountRelatedBase = (props) => {
     const [form] = Form.useForm();
+    const { onCloseAction, isViewModeVisible } = props;
+
     const [openAccordian, setOpenAccordian] = useState([1]);
 
     const handleCollapse = (key) => {
         setOpenAccordian((prev) => (prev === key ? '' : key));
     };
+    const viewProps = {
+        styles,
+    };
 
     return (
         <>
             <h2>Account Related</h2>
-            <Space direction="vertical" size="small" style={{ display: 'flex' }}>
+            {!isViewModeVisible ? (
+                <Space direction="vertical" size="small" style={{ display: 'flex' }}>
+                    <Collapse onChange={() => handleCollapse(1)} expandIcon={({ isActive }) => (isActive ? <AiOutlineMinus /> : <AiOutlinePlus />)} activeKey={openAccordian} expandIconPosition="end">
+                        <Panel
+                            header={
+                                <div className={styles.alignUser}>
+                                    <FaRegUserCircle className={styles.userCircle} />
+                                    <Text strong style={{ marginTop: '4px', marginLeft: '8px' }}>
+                                        Company Account
+                                    </Text>
+                                </div>
+                            }
+                            key="1"
+                        >
+                            <AddEditForm {...props} />
+                        </Panel>
+                    </Collapse>
+                    <Row gutter={20}>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                            <Button danger onClick={onCloseAction}>
+                                Cancel
+                            </Button>
+                        </Col>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                            <Button type="primary" className={styles.floatRight}>
+                                Save & Proceed
+                                {/* onClick={onFinish} */}
+                            </Button>
+                        </Col>
+                    </Row>
+                </Space>
+            ) : (
                 <Collapse onChange={() => handleCollapse(1)} expandIcon={({ isActive }) => (isActive ? <AiOutlineMinus /> : <AiOutlinePlus />)} activeKey={openAccordian} expandIconPosition="end">
                     <Panel
                         header={
@@ -35,10 +73,10 @@ const AccountRelatedBase = (props) => {
                         }
                         key="1"
                     >
-                        <AddEditForm {...props} />
+                        <ViewDetail {...viewProps} />
                     </Panel>
                 </Collapse>
-            </Space>
+            )}
         </>
     );
 };
--- src/components/common/CustomerMaster/FirmOrCompany/AccountRelated/AddEditForm.js
+++ src/components/common/CustomerMaster/FirmOrCompany/AccountRelated/AddEditForm.js
@@ -34,72 +34,54 @@ const AddEditFormMain = (props) => {
 
     return (
         <>
-            {!isViewModeVisible ? (
-                <Form form={form} id="myForm" autoComplete="off" layout="vertical">
-                    <Row gutter={20}>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item label="Credit Limit" name="limitAmt">
-                                <Input placeholder={preparePlaceholderText('limit')} />
-                            </Form.Item>
-                        </Col>
+            <Form form={form} id="myForm" autoComplete="off" layout="vertical">
+                <Row gutter={20}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item label="Credit Limit" name="limitAmt">
+                            <Input placeholder={preparePlaceholderText('limit')} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item label="Credit Limit Days" name="limitDays">
-                                <Input placeholder={preparePlaceholderText('limit')} />
-                            </Form.Item>
-                        </Col>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item label="Credit Limit Days" name="limitDays">
+                            <Input placeholder={preparePlaceholderText('limit')} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item label="Outstanding Amount" name="outstandingAmt">
-                                <Input placeholder={preparePlaceholderText('amount')} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={20}>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item label="Parts Discount %" name="partsDiscount">
-                                <Input placeholder={preparePlaceholderText('discount')} />
-                            </Form.Item>
-                        </Col>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item label="Labour Discount %" name="laborDiscount">
-                                <Input placeholder={preparePlaceholderText('discount')} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item label="Outstanding Amount" name="outstandingAmt">
+                            <Input placeholder={preparePlaceholderText('amount')} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={20}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item label="Parts Discount %" name="partsDiscount">
+                            <Input placeholder={preparePlaceholderText('discount')} />
+                        </Form.Item>
+                    </Col>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item label="Labour Discount %" name="laborDiscount">
+                            <Input placeholder={preparePlaceholderText('discount')} />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-                    <Row gutter={20}>
-                        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-                            <Form.Item label="Remarks" name="remarks">
-                                <TextArea rows={3} maxLength={250} placeholder={preparePlaceholderText('remark')} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={20}>
-                        <Col xs={24} sm={12} md={8} lg={8} xl={8}>
-                            <Form.Item valuePropName="checked" name="vipDealerInd">
-                                <Checkbox>VIP Dealer</Checkbox>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <br></br>
-                    <Row gutter={20}>
-                        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                            <Button danger onClick={onCloseAction}>
-                                Cancel
-                            </Button>
-                        </Col>
-                        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                            <Button type="primary" className={styles.floatRight}>
-                                Save & Proceed
-                                {/* onClick={onFinish} */}
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
-            ) : (
-                <ViewDetail {...viewProps} />
-            )}
+                <Row gutter={20}>
+                    <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+                        <Form.Item label="Remarks" name="remarks">
+                            <TextArea rows={3} maxLength={250} placeholder={preparePlaceholderText('remark')} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={20}>
+                    <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+                        <Form.Item valuePropName="checked" name="vipDealerInd">
+                            <Checkbox>VIP Dealer</Checkbox>
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </Form>
         </>
     );
 };
