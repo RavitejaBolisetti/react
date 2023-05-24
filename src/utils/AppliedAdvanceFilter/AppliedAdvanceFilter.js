20af585e40ac1811a6d0010b0d88f6f195e538ff
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-05-24 12:34:36 +0000

    Squashed commit of the following
    commit d6e7d269 
    Author: Kartik Gupta <KA20371579@wipro.com> 
    Date: Wed May 24 2023 17:33:10 GMT+0530 (India Standard Time) 
    
        working on the advenaced Seacrch filter
    
    
    commit 75773769 
    Author: Vivek <vivek.vimal@wipro.com> 
    Date: Wed May 24 2023 17:12:49 GMT+0530 (India Standard Time) 
    
        Working on advanced search

--- src/utils/AppliedAdvanceFilter/AppliedAdvanceFilter.js
+++ src/utils/AppliedAdvanceFilter/AppliedAdvanceFilter.js
@@ -17,10 +17,6 @@ export default function AppliedAdvanceFilter(props) {
         e.key === 'Enter' && e.preventDefault();
     };
 
-    const clearData = () => {
-        
-    }
-
     return (
         <>
             <div className={styles.contentHeaderBackground}>
@@ -45,7 +41,7 @@ export default function AppliedAdvanceFilter(props) {
                             </Col>
                             {advanceFilter && (
                                 <Col xs={24} sm={24} md={4} lg={4} xl={4}>
-                                    <Button icon={<FilterIcon />} type="link" className={styles.filterBtn} onClick={() => { setAdvanceSearchVisible(true); clearData();}}>
+                                    <Button icon={<FilterIcon />} type="link" className={styles.filterBtn} onClick={() => { setAdvanceSearchVisible(true)}}>
                                         Advanced Filters
                                     </Button>
                                 </Col>
