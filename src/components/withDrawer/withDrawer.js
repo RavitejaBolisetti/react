73f4d73ffeea771023809cf66ec9ad0ab6d65f8f
Ashutosh <ashutosh.sahu@wipro.com>
2023-05-26 12:27:22 +0530

    Stash merge

--- src/components/withDrawer/withDrawer.js
+++ src/components/withDrawer/withDrawer.js
@@ -18,7 +18,7 @@ export const withDrawer =
             // open={onOpenAction}
             return (
                 <Drawer title={titleOverride || title} open={onOpenAction} onOk={onOk} onClose={onCloseAction} width={width} visible={isVisible} className={styles.myDrawerStyle} keyboard={false} maskClosable={false}>
-                    {isVisible && <InputComponent {...props} />}
+                    {width === 520 ? <div className={styles.withDrawerBody}>{isVisible && <InputComponent {...props} />}</div> : isVisible && <InputComponent {...props} />}
                 </Drawer>
             );
         };
