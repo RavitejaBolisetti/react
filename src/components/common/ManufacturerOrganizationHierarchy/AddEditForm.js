b270d18fde73839806283f815c251e3a9dc310f5
Ashutosh <ashutosh.sahu@wipro.com>
2023-05-27 14:39:51 +0530

    Fixed hierarchy drawer textarea height and expansion issue
    #feature

--- src/components/common/ManufacturerOrganizationHierarchy/AddEditForm.js
+++ src/components/common/ManufacturerOrganizationHierarchy/AddEditForm.js
@@ -73,7 +73,7 @@ const AddEditFormMain = (props) => {
     };
     return (
         <>
-            <Form autoComplete="off" form={form}  layout="vertical" onValuesChange={handleFormValueChange} onFieldsChange={handleFormFieldChange} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+            <Form autoComplete="off" form={form} layout="vertical" onValuesChange={handleFormValueChange} onFieldsChange={handleFormFieldChange} onFinish={onFinish} onFinishFailed={onFinishFailed}>
                 <Row gutter={20}>
                     <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                         <Form.Item initialValue={formData?.attributeKey} name="attributeKey" label="Attribute Type" rules={[validateRequiredSelectField('Attribute Type Code')]}>
@@ -108,8 +108,8 @@ const AddEditFormMain = (props) => {
 
                 <Row gutter={20}>
                     <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                        <Form.Item name="manufactureOrgLongName" label="Long Description" placeholder={preparePlaceholderSelect('Long Description')} initialValue={formData?.manufactureOrgLongName} rules={[validateRequiredInputField('Long Description'),validateAlphanumericWithSpaceHyphenPeriod('Long Description')]}>
-                            <TextArea rows={2} placeholder={preparePlaceholderText('Long Description')} showCount maxLength={100} disabled={formData?.id || isReadOnly} />
+                        <Form.Item name="manufactureOrgLongName" label="Long Description" placeholder={preparePlaceholderSelect('Long Description')} initialValue={formData?.manufactureOrgLongName} rules={[validateRequiredInputField('Long Description'), validateAlphanumericWithSpaceHyphenPeriod('Long Description')]}>
+                            <TextArea rows={1} placeholder={preparePlaceholderText('Long Description')} showCount maxLength={100} disabled={formData?.id || isReadOnly} />
                         </Form.Item>
                     </Col>
                     <Col xs={24} sm={24} md={24} lg={24} xl={24} className={styles.padLeft10}>
