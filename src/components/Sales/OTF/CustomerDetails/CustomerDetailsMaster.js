dcf616fecb9b40b6765a0075c16b5783d544d534
Nishant Malik <nishant.malik.ext@wipro.com>
2023-06-08 16:05:00 +0530

    worked on fix
    WIP #1399

--- src/components/Sales/OTF/CustomerDetails/CustomerDetailsMaster.js
+++ src/components/Sales/OTF/CustomerDetails/CustomerDetailsMaster.js
@@ -4,16 +4,14 @@ import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 import { validateRequiredInputField, validateRequiredSelectField } from 'utils/validation';
 import styles from 'components/common/Common.module.css';
 import { FaRegUserCircle } from 'react-icons/fa';
-import { AddEditForm } from './AddEditForm';
+import AddEditForm from './AddEditForm';
 const { Panel } = Collapse;
 const { Option } = Select;
 
 export const CustomerDetailsMaster = (props) => {
     return (
         <div className={styles.drawerCustomerMaster}>
-            <h2>Booking Customer</h2>
             <AddEditForm {...props} />
         </div>
     );
 };
-
