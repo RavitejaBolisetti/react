4b558fca22fc5ec3a695b41bb798cbef0b2000d3
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-06-09 16:15:31 +0530

    Changes in customer master
    wip #1399

--- src/components/Sales/OTF/CustomerDetails/AddEditForm.js
+++ src/components/Sales/OTF/CustomerDetails/AddEditForm.js
@@ -5,19 +5,25 @@ import { preparePlaceholderSelect, preparePlaceholderText } from 'utils/prepareP
 import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
 
 import styles from 'components/common/Common.module.css';
+import { ViewDetail } from './ViewDetail';
 
-const { Text, Link } = Typography;
+const { Text } = Typography;
 
 const { Panel } = Collapse;
 
-function AddEditForm() {
+const AddEditForm = (props) => {
+    const { formActionType } = props;
     const [activeKey, setactiveKey] = useState([1]);
+    console.log('formActionType?.viewMode', formActionType?.viewMode);
 
     const viewProps = {
         bordered: false,
         colon: false,
         layout: 'vertical',
         column: { xxl: 3, xl: 3, lg: 3, md: 3, sm: 3, xs: 3 },
+        styles,
+        activeKey,
+        setactiveKey,
     };
 
     const onChange = (values) => {
@@ -39,103 +45,107 @@ function AddEditForm() {
 
     return (
         <>
-            <Row gutter={20}>
-                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                    <Space style={{ display: 'flex' }} size="middle" direction="vertical">
-                        <Collapse
-                            expandIcon={() => {
-                                if (activeKey.includes(1)) {
-                                    return <MinusOutlined className={styles.iconsColor} />;
-                                } else {
-                                    return <PlusOutlined className={styles.iconsColor} />;
-                                }
-                            }}
-                            activeKey={activeKey}
-                            onChange={() => onChange(1)}
-                            expandIconPosition="end"
-                        >
-                            <Panel
-                                header={
-                                    <div className={styles.alignUser}>
-                                        <Text strong style={{ marginTop: '4px', marginLeft: '8px' }}>
-                                            Booking Customer
-                                        </Text>
-                                    </div>
-                                }
-                                key="1"
+            {!formActionType?.viewMode ? (
+                <Row gutter={20}>
+                    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                        <Space style={{ display: 'flex' }} size="middle" direction="vertical">
+                            <Collapse
+                                expandIcon={() => {
+                                    if (activeKey.includes(1)) {
+                                        return <MinusOutlined className={styles.iconsColor} />;
+                                    } else {
+                                        return <PlusOutlined className={styles.iconsColor} />;
+                                    }
+                                }}
+                                activeKey={activeKey}
+                                onChange={() => onChange(1)}
+                                expandIconPosition="end"
                             >
-                                <Descriptions {...viewProps}>
-                                    <Descriptions.Item label="Customer ID">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Customer Type">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Mobile Number">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Salutation">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Customer Name">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Address">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="City/District">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="State">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Pin Code">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Alternate Number">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Email">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="PAN">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Aadhar">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="GSTIN">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Driving License">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Trade Licence">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Birth Date">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Do You Want to Add Corporate Details">{'hello'}</Descriptions.Item>
-                                </Descriptions>
-                            </Panel>
-                        </Collapse>
-                        <Collapse
-                            expandIcon={() => {
-                                if (activeKey.includes(2)) {
-                                    return <MinusOutlined className={styles.iconsColor} />;
-                                } else {
-                                    return <PlusOutlined className={styles.iconsColor} />;
-                                }
-                            }}
-                            activeKey={activeKey}
-                            onChange={() => onChange(2)}
-                            expandIconPosition="end"
-                        >
-                            <Panel
-                                header={
-                                    <div className={styles.alignUser}>
-                                        <Text strong style={{ marginTop: '4px', marginLeft: '8px' }}>
-                                            Billing Customer
-                                        </Text>
-                                    </div>
-                                }
-                                key="2"
+                                <Panel
+                                    header={
+                                        <div className={styles.alignUser}>
+                                            <Text strong style={{ marginTop: '4px', marginLeft: '8px' }}>
+                                                Booking Customer
+                                            </Text>
+                                        </div>
+                                    }
+                                    key="1"
+                                >
+                                    <Descriptions {...viewProps}>
+                                        <Descriptions.Item label="Customer ID">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Customer Type">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Mobile Number">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Salutation">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Customer Name">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Address">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="City/District">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="State">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Pin Code">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Alternate Number">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Email">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="PAN">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Aadhar">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="GSTIN">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Driving License">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Trade Licence">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Birth Date">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Do You Want to Add Corporate Details">{'hello'}</Descriptions.Item>
+                                    </Descriptions>
+                                </Panel>
+                            </Collapse>
+                            <Collapse
+                                expandIcon={() => {
+                                    if (activeKey.includes(2)) {
+                                        return <MinusOutlined className={styles.iconsColor} />;
+                                    } else {
+                                        return <PlusOutlined className={styles.iconsColor} />;
+                                    }
+                                }}
+                                activeKey={activeKey}
+                                onChange={() => onChange(2)}
+                                expandIconPosition="end"
                             >
-                                <Checkbox>Same as Booking Customer</Checkbox>
-                                <Descriptions {...viewProps}>
-                                    <Descriptions.Item label="Customer ID">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Customer Type">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Mobile Number">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Salutation">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Customer Name">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Address">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="City/District">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="State">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Pin Code">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Alternate Number">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Email">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="PAN">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Aadhar">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="GSTIN">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Driving License">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Trade Licence">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Birth Date">{'hello'}</Descriptions.Item>
-                                    <Descriptions.Item label="Do You Want to Add Corporate Details">{'hello'}</Descriptions.Item>
-                                </Descriptions>
-                            </Panel>
-                        </Collapse>
-                    </Space>
-                </Col>
-            </Row>
+                                <Panel
+                                    header={
+                                        <div className={styles.alignUser}>
+                                            <Text strong style={{ marginTop: '4px', marginLeft: '8px' }}>
+                                                Billing Customer
+                                            </Text>
+                                        </div>
+                                    }
+                                    key="2"
+                                >
+                                    <Checkbox>Same as Booking Customer</Checkbox>
+                                    <Descriptions {...viewProps}>
+                                        <Descriptions.Item label="Customer ID">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Customer Type">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Mobile Number">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Salutation">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Customer Name">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Address">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="City/District">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="State">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Pin Code">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Alternate Number">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Email">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="PAN">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Aadhar">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="GSTIN">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Driving License">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Trade Licence">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Birth Date">{'hello'}</Descriptions.Item>
+                                        <Descriptions.Item label="Do You Want to Add Corporate Details">{'hello'}</Descriptions.Item>
+                                    </Descriptions>
+                                </Panel>
+                            </Collapse>
+                        </Space>
+                    </Col>
+                </Row>
+            ) : (
+                <ViewDetail {...viewProps} />
+            )}
         </>
     );
-}
+};
 
 export default AddEditForm;
