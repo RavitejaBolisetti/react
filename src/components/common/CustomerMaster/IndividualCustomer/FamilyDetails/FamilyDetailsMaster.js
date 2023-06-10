c8a65bca3a97250390cf7b055d67e49486ee5a8e
Vivek <vivek.vimal@wipro.com>
2023-06-10 11:36:32 +0530

    working

--- src/components/common/CustomerMaster/IndividualCustomer/FamilyDetails/FamilyDetailsMaster.js
+++ src/components/common/CustomerMaster/IndividualCustomer/FamilyDetails/FamilyDetailsMaster.js
@@ -1,35 +1,22 @@
 import React, { useState, useRef, useCallback } from 'react';
-import { Collapse, Space, Typography, Form } from 'antd';
+import { Form } from 'antd';
 import { AddEditForm } from './AddEditForm';
 
-const { Panel } = Collapse;
-const { Text } = Typography;
-
 const FamilyDetailsBase = () => {
-    const [openAccordian, setOpenAccordian] = useState('');
-
-    const handleCollapse = (key) => {
-        setOpenAccordian((prev) => (prev === key ? '' : key));
-    };
-
     const [form] = Form.useForm();
-
     const type = [
         { name: 'YES', value: 1 },
         { name: 'NO', value: 0 },
     ];
-
     const [value, setValue] = useState(true);
-
     const selectRef = useRef();
-
     const onChange = useCallback((item) => {
-        selectRef.current.blur(); //whenever a user triggers value change, we call `blur()` on `Select`
+        selectRef.current.blur();
         setValue(item);
     }, []);
 
     const onFamilyFinish = (values) => {
-        console.log(values, 'valuesvaluesvalues');
+        
     };
 
     const onFinishFailed = (errorInfo) => {
