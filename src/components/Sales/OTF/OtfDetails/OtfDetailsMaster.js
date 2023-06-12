4d0a37e38200c4ed7eb7d4381b3834a570b02783
Nishant Malik <nishant.malik.ext@wipro.com>
2023-06-12 20:26:00 +0530

    worked on code clean
    WIP #1400

--- src/components/Sales/OTF/OtfDetails/OtfDetailsMaster.js
+++ src/components/Sales/OTF/OtfDetails/OtfDetailsMaster.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Card, Typography } from 'antd';
+import { Card } from 'antd';
 
 import AddEditForm from './AddEditForm';
 
-import styles from 'components/common/Common.module.css';
 
-const { Text } = Typography;
 
 const OtfDetailsMaster = () => {
     return (
