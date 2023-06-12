c8bf92ac916815aee78f9289eddea60bd44ca118
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-06-12 15:26:46 +0530

    Making form of Exhange vehicles
    #wip #1441

--- src/components/Sales/OTF/ExchangeVehicles/ExchangeVehiclesMaster.js
+++ src/components/Sales/OTF/ExchangeVehicles/ExchangeVehiclesMaster.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import styles from 'components/common/Common.module.css';
 import { AddEditForm } from './AddEditForm';
+import CommonTimeline from '../Timeline';
+import { Row, Col, Form } from 'antd';
 
 export const ExchangeVehiclesMaster = (props) => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {};
+    const onFinishFailed = (values) => {
+        form.validateFields()
+            .then(() => {})
+            .catch((err) => {
+                console.log('err');
+            });
+    };
+    const ExchangeVehiclesMasterProps = {
+        ...props,
+        form,
+        onFinishFailed,
+        onFinish,
+    };
     return (
         <div className={styles.drawerCustomerMaster}>
-            <h2>Exchange Vehicle</h2>
-            <AddEditForm {...props} />
+            <Row gutter={20}>
+                <Col span={12}>
+                    <h2>Exchange Vehicle</h2>
+                </Col>
+                <Col span={12}>{/* <CommonTimeline /> */}</Col>
+            </Row>
+            <AddEditForm {...ExchangeVehiclesMasterProps} />
         </div>
     );
 };
