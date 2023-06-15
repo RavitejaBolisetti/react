94cf8e35df6e0e05109a5d253f2aab44d5956464
Kartik Gupta <KA20371579@wipro.com>
2023-06-15 16:15:52 +0530

    Working on Accounts UI
    WIP  #US-Customer-Master-Main

--- src/components/common/CustomerMaster/FirmOrCompany/AccountRelated/AddEditForm.js
+++ src/components/common/CustomerMaster/FirmOrCompany/AccountRelated/AddEditForm.js
@@ -2,6 +2,8 @@ import { React } from 'react';
 import { Row, Col, Form, Input, Checkbox } from 'antd';
 
 import { preparePlaceholderText } from 'utils/preparePlaceholder';
+import { validateNumberWithTwoDecimalPlaces, validationNumber, valueBetween0to100 } from 'utils/validation';
+
 const { TextArea } = Input;
 
 const AddEditFormMain = (props) => {
@@ -12,31 +14,31 @@ const AddEditFormMain = (props) => {
             <Form form={form} id="myForm" autoComplete="off" layout="vertical">
                 <Row gutter={20}>
                     <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                        <Form.Item label="Credit Limit" name="limitAmt">
+                        <Form.Item label="Credit Limit" name="limitAmt" rules={[validateNumberWithTwoDecimalPlaces('credit limit amount')]}>
                             <Input placeholder={preparePlaceholderText('limit')} />
                         </Form.Item>
                     </Col>
 
                     <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                        <Form.Item label="Credit Limit Days" name="limitDays">
+                        <Form.Item label="Credit Limit Days" name="limitDays" rules={[validationNumber('credit limit days')]}>
                             <Input placeholder={preparePlaceholderText('limit')} />
                         </Form.Item>
                     </Col>
 
                     <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                        <Form.Item label="Outstanding Amount" name="outstandingAmt">
+                        <Form.Item label="Outstanding Amount" name="outstandingAmt" rules={[validateNumberWithTwoDecimalPlaces('outstanding amount')]}>
                             <Input placeholder={preparePlaceholderText('amount')} />
                         </Form.Item>
                     </Col>
                 </Row>
                 <Row gutter={20}>
                     <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                        <Form.Item label="Parts Discount %" name="partsDiscount">
+                        <Form.Item label="Parts Discount %" name="partsDiscount" rules={[{ validator: (fieldData, value) => valueBetween0to100(value, 'parts discount') }]}>
                             <Input placeholder={preparePlaceholderText('discount')} />
                         </Form.Item>
                     </Col>
                     <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                        <Form.Item label="Labour Discount %" name="laborDiscount">
+                        <Form.Item label="Labour Discount %" name="laborDiscount" rules={[{ validator: (fieldData, value) => valueBetween0to100(value, 'labour discount') }]}>
                             <Input placeholder={preparePlaceholderText('discount')} />
                         </Form.Item>
                     </Col>
@@ -45,7 +47,7 @@ const AddEditFormMain = (props) => {
                 <Row gutter={20}>
                     <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
                         <Form.Item label="Remarks" name="remarks">
-                            <TextArea rows={3} maxLength={250} placeholder={preparePlaceholderText('remark')} />
+                            <TextArea rows={2} maxLength={250} placeholder={preparePlaceholderText('remark')} />
                         </Form.Item>
                     </Col>
                 </Row>
