492b5001026afd82393a3f4d2272544bf9ef5fd3
Nishant Malik <nishant.malik.ext@wipro.com>
2023-06-17 10:58:26 +0530

    worked on upload
    WIP #2144

--- src/components/common/CustomerMaster/IndividualCustomer/SupportingDocument/AddEditForm.js
+++ src/components/common/CustomerMaster/IndividualCustomer/SupportingDocument/AddEditForm.js
@@ -3,62 +3,88 @@ import { Row, Col, Form, Select, Input, message, Upload, Button, Empty } from 'a
 
 import { FiEye, FiTrash } from 'react-icons/fi';
 
+import { withAuthToken } from 'utils/withAuthToken';
+
 import { preparePlaceholderText, preparePlaceholderSelect } from 'utils/preparePlaceholder';
+import { validateRequiredInputField, validateRequiredSelectField } from 'utils/validation';
+
+// import { getAuthToken, getAuthAccessToken, getUserId } from '../store/state/auth';
 
 import styles from 'components/common/Common.module.css';
 
 const { Option } = Select;
 const { Dragger } = Upload;
 
-// const uploadProps = {
-//     name: 'file',
-//     multiple: true,
-//     action: '',
-//     uploadTitle: 'Upload Your Profile Picture',
-//     uploadDescription: 'File type should be .png and .jpg and max file size to be 5MB',
-//     uploadBtnName: 'Upload File',
-//     onChange(info) {
-//         const { status } = info.file;
-//         if (status === 'done') {
-//             message.success(`${info.file.name} file uploaded successfully.`);
-//         } else if (status === 'error') {
-//             message.error(`${info.file.name} file upload failed.`);
-//         }
-//     },
+const AddEditForm = (props) => {
+    const { typeData, userId, accessToken, token } = props;
 
-// };
-
-const AddEditForm = () => {
     const [form] = Form.useForm();
+    var accessTokenObj = JSON.parse(localStorage.getItem('Token:'));
 
-    const showUploadList = {
-        showRemoveIcon: false,
-        showPreviewIcon: true,
-        showDownloadIcon:true,
-        previewIcon: <FiTrash onClick={(e) => console.log(e, 'custom removeIcon event')} />,
-        // removeIcon: <FiTrash onClick={(e) => console.log(e, 'custom removeIcon event')} />,
+    // const token = getAuthToken(getState());
+    // const userId = getUserId(getState());
+    // const accessToken = getAuthAccessToken(getState());
+
+    const uploadProps = {
+        name: 'file',
+        multiple: true,
+        action: 'https://apidev.mahindradealerrise.com/common/document/upload',
+        headers: {
+            userId,
+            accessToken,
+            token,
+        },
+        uploadTitle: 'Upload Your Profile Picture',
+        uploadDescription: 'File type should be .png and .jpg and max file size to be 5MB',
+        uploadBtnName: 'Upload File',
+        onChange(info) {
+            const { status } = info.file;
+            if (status === 'done') {
+                message.success(`${info.file.name} file uploaded successfully.`);
+            } else if (status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+        showUploadList: {
+            showRemoveIcon: true,
+            showPreviewIcon: true,
+            showDownloadIcon: false,
+            previewIcon: <FiEye onClick={(e) => console.log(e, 'custom removeIcon event')} />,
+            removeIcon: <FiTrash onClick={(e) => console.log(e, 'custom removeIcon event')} />,
+        },
+    };
+
+    const selectProps = {
+        optionFilterProp: 'children',
+        showSearch: true,
+        allowClear: true,
+        className: styles.headerSelectField,
     };
 
     return (
         <Form form={form} layout="vertical">
             <Row gutter={16}>
                 <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                    <Form.Item label="Document Type" name="documentType">
-                        <Select placeholder={preparePlaceholderSelect('Document Type')} allowClear>
-                            <Option value="documentType">documentType</Option>
+                    <Form.Item label="Document Type" name="documentType" placeholder={preparePlaceholderSelect('document type')} rules={[validateRequiredSelectField('document type')]}>
+                        <Select className={styles.headerSelectField} loading={!(typeData?.CUST_FILES?.length !== 0)} placeholder="Select" {...selectProps}>
+                            {typeData?.CUST_FILES?.map((item) => (
+                                <Option key={'cust' + item?.value} value={item?.value}>
+                                    {item?.value}
+                                </Option>
+                            ))}
                         </Select>
                     </Form.Item>
                 </Col>
                 <Col xs={24} sm={24} md={12} lg={12} xl={12}>
                     <Form.Item label="File Name" name="fileName">
-                        <Input placeholder={preparePlaceholderText('File Name')} allowClear />
+                        <Input placeholder={preparePlaceholderText('File Name')} rules={[validateRequiredInputField('fileName')]} allowClear />
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                     <div className={styles.uploadDragger}>
-                        <Dragger showUploadList={showUploadList}>
+                        <Dragger {...uploadProps}>
                             <Empty
                                 image={Empty.PRESENTED_IMAGE_SIMPLE}
                                 imageStyle={{
--- src/components/common/CustomerMaster/IndividualCustomer/SupportingDocument/SupportingDocument.js
+++ src/components/common/CustomerMaster/IndividualCustomer/SupportingDocument/SupportingDocument.js
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { bindActionCreators } from 'redux';
+
 import { Card } from 'antd';
+
+import { showGlobalNotification } from 'store/actions/notification';
+
+import { configParamEditActions } from 'store/actions/data/configurableParamterEditing';
 
 import AddEditForm from './AddEditForm';
 
-const SupportingDocument = () => {
+const mapStateToProps = (state) => {
+    const {
+        auth: { userId, accessToken, token },
+        data: {
+            ConfigurableParameterEditing: { isLoaded: isDocumentDataLoaded = false, isDocumentDataLoading, data: configData = [], paramdata: typeData = [] },
+        },
+    } = state;
+
+    console.log('state', state);
+
+    let returnValue = {
+        userId,
+        accessToken, token,
+        configData,
+        typeData,
+    };
+    return returnValue;
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    dispatch,
+    ...bindActionCreators(
+        {
+            configFetchList: configParamEditActions.fetchList,
+            configListShowLoading: configParamEditActions.listShowLoading,
+
+            showGlobalNotification,
+        },
+        dispatch
+    ),
+});
+
+const SupportingDocumentBase = (props) => {
+    const { userId,        accessToken, token,
+        configData, typeData, configFetchList, configListShowLoading } = props;
+
+    useEffect(() => {
+        if (userId) {
+            configFetchList({ setIsLoading: configListShowLoading, userId, parameterType: 'CUST_FILES' });
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [userId]);
+
+    const formProps = {
+        typeData,
+        userId,        accessToken, token,
+    };
+
     return (
         <Card style={{ backgroundColor: '#f2f2f2' }}>
-            <AddEditForm />
+            <AddEditForm {...formProps} />
         </Card>
     );
 };
 
-export default SupportingDocument;
+export const SupportingDocument = connect(mapStateToProps, mapDispatchToProps)(SupportingDocumentBase);
