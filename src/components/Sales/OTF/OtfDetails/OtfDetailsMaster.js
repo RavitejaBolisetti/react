3a840b0381e2fb96ddf6c211a43979abc9d0169f
Nishant Malik <nishant.malik.ext@wipro.com>
2023-06-18 21:29:20 +0530

    worked on api integration
    WIP #1399

--- src/components/Sales/OTF/OtfDetails/OtfDetailsMaster.js
+++ src/components/Sales/OTF/OtfDetails/OtfDetailsMaster.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form } from 'antd';
 
 import AddEditForm from './AddEditForm';
 
-const OtfDetailsMaster = (props) => {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { otfDetailsDataActions } from 'store/actions/data/otf/otfDetails';
+import { showGlobalNotification } from 'store/actions/notification';
+
+const mapStateToProps = (state) => {
+    const {
+        auth: { userId },
+        data: {
+            OTF: {
+                OtfDetails: { isLoaded: isDataLoaded = false, isLoading, data: otfData = [] },
+            },
+        },
+    } = state;
+    console.log('state', state);
+    const moduleTitle = 'OTF Details';
+
+    let returnValue = {
+        userId,
+        isDataLoaded,
+        otfData,
+        isLoading,
+        moduleTitle,
+    };
+    return returnValue;
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    dispatch,
+    ...bindActionCreators(
+        {
+            fetchList: otfDetailsDataActions.fetchList,
+            saveData: otfDetailsDataActions.saveData,
+            resetData: otfDetailsDataActions.reset,
+            listShowLoading: otfDetailsDataActions.listShowLoading,
+            showGlobalNotification,
+        },
+        dispatch
+    ),
+});
+
+const OtfDetailsMasterBase = (props) => {
+    const { fetchList, saveData, listShowLoading, userId, isDataLoaded, otfData, isLoading } = props;
+
     const [form] = Form.useForm();
+
+    const extraParams = [
+        {
+            key: 'otfNumber',
+            title: 'otfNumber',
+            value: 'OTF001',
+            name: 'OTF Number',
+        },
+    ];
+
+    const errorAction = (message) => {
+        showGlobalNotification(message);
+    };
+
+    const onSuccessAction = (res) => {
+        showGlobalNotification({ notificationType: 'success', title: 'Success', message: res?.responseMessage });
+    };
+
+    useEffect(() => {
+        if (!isDataLoaded && userId) {
+            fetchList({ setIsLoading: listShowLoading, extraParams, onSuccessAction, errorAction, userId });
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isDataLoaded, userId]);
 
     const onFinish = () => {};
 
@@ -15,9 +83,15 @@ const OtfDetailsMaster = (props) => {
         form,
         onFinish,
         onFinishFailed,
+        fetchList,
+
+        userId,
+        isDataLoaded,
+        otfData,
+        isLoading,
     };
 
     return <AddEditForm {...formProps} />;
 };
 
-export default OtfDetailsMaster;
+export const OtfDetailsMaster = connect(mapStateToProps, mapDispatchToProps)(OtfDetailsMasterBase);
--- src/store/reducers/data/otf/index.js
+++ src/store/reducers/data/otf/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 
 import { InvoiceDetail } from './invoiceDetails';
+import { OtfDetails } from './otfDetails';
+
 export const OTF = combineReducers({
     InvoiceDetail,
+    OtfDetails,
 });
