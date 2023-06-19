118cb7ca3579f79c4dc0e22337f0d31cb45d7a8d
Kartik Gupta <KA20371579@wipro.com>
2023-06-19 10:05:01 +0530

    Working on API Integration
    WIP  #US-1408-OTF

--- src/components/Sales/OTF/FinananceDetails/AddEditForm.js
+++ src/components/Sales/OTF/FinananceDetails/AddEditForm.js
@@ -41,6 +41,7 @@ const AddEditFormMain = (props) => {
         styles,
         onCloseAction,
         handleEdit,
+        formData,
     };
 
     const selectProps = {
@@ -70,75 +71,75 @@ const AddEditFormMain = (props) => {
                         <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                             <Space style={{ display: 'flex' }} size="middle" direction="vertical">
                                 <Card style={{ backgroundColor: '#F2F2F2' }}>
-                                    <Form id="myform" autoComplete="off" layout="vertical" form={financeForm}>
-                                        <Row gutter={20}>
-                                            <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.financier} label="Financier" name="financier" placeholder={preparePlaceholderSelect('Select')}>
-                                                    <Select placeholder="Select" {...selectProps}>
-                                                        <Option value="financier1">HDFC</Option>
-                                                        <Option value="financier2">SBI</Option>
-                                                        <Option value="financier3">ICICI</Option>
-                                                        <Option value="financier4">PNB</Option>
-                                                    </Select>
-                                                </Form.Item>
-                                            </Col>
-                                            <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.branch} label="Branch" name="branch">
-                                                    <Input placeholder={preparePlaceholderText('branch')} maxLength={55} />
-                                                </Form.Item>
-                                            </Col>
-                                            <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.fileNumber} label="File Number" name="fileNumber">
-                                                    <Input placeholder={preparePlaceholderText('file number')} maxLength={55} />
-                                                </Form.Item>
-                                            </Col>
-                                        </Row>
+                                    {/* <Form id="myform" autoComplete="off" layout="vertical" form={financeForm}> */}
+                                    <Row gutter={20}>
+                                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                                            <Form.Item initialValue={formData?.financier} label="Financier" name="financier" placeholder={preparePlaceholderSelect('Select')}>
+                                                <Select placeholder="Select" {...selectProps}>
+                                                    <Option value="financier1">HDFC</Option>
+                                                    <Option value="financier2">SBI</Option>
+                                                    <Option value="financier3">ICICI</Option>
+                                                    <Option value="financier4">PNB</Option>
+                                                </Select>
+                                            </Form.Item>
+                                        </Col>
+                                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                                            <Form.Item initialValue={formData?.branch} label="Branch" name="branch">
+                                                <Input placeholder={preparePlaceholderText('branch')} maxLength={55} />
+                                            </Form.Item>
+                                        </Col>
+                                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                                            <Form.Item initialValue={formData?.fileNumber} label="File Number" name="fileNumber">
+                                                <Input placeholder={preparePlaceholderText('file number')} maxLength={55} />
+                                            </Form.Item>
+                                        </Col>
+                                    </Row>
 
-                                        <Row gutter={20}>
+                                    <Row gutter={20}>
+                                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                                            <Form.Item initialValue={formData?.loanAmount} label="Loan Amount" name="loanAmount">
+                                                <Input placeholder={preparePlaceholderText('loan amount')} maxLength={55} />
+                                            </Form.Item>
+                                        </Col>
+                                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                                            <Form.Item initialValue={formData?.emi} label="EMI" name="emi">
+                                                <Input placeholder={preparePlaceholderText('emi')} maxLength={55} />
+                                            </Form.Item>
+                                        </Col>
+                                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                                            <Form.Item initialValue={formData?.financeDone} label="Finance Done" name="financeDone">
+                                                <Select placeholder="Select" {...selectProps}>
+                                                    <Option value="yes">Yes</Option>
+                                                    <Option value="no">No</Option>
+                                                </Select>
+                                            </Form.Item>
+                                        </Col>
+                                    </Row>
+                                    <Row gutter={20}>
+                                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                                            <Form.Item initialValue={formData?.doReceived} label="D.O. Recived" name="doReceived">
+                                                <Select placeholder="Select" {...selectProps}>
+                                                    <Option value="yes">Yes</Option>
+                                                    <Option value="no">No</Option>
+                                                </Select>
+                                            </Form.Item>
+                                        </Col>
+                                        {selected === 'yes' && (
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.loanAmount} label="Loan Amount" name="loanAmount">
-                                                    <Input placeholder={preparePlaceholderText('loan amount')} maxLength={55} />
+                                                <Form.Item initialValue={formData?.doNumber} label="D.O. Number" name="doNumber">
+                                                    <Input placeholder={preparePlaceholderText('d.o. number')}></Input>
                                                 </Form.Item>
                                             </Col>
+                                        )}
+                                        {selected === 'yes' && (
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.emi} label="EMI" name="emi">
-                                                    <Input placeholder={preparePlaceholderText('emi')} maxLength={55} />
+                                                <Form.Item initialValue={formData?.doDate} label="D.O. Date" name="doDate" data-testid="CustomerCategory">
+                                                    <DatePicker placeholder={preparePlaceholderSelect('date')} style={datePickerStyle} disabledDate={(date) => date > dayjs()} />
                                                 </Form.Item>
                                             </Col>
-                                            <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.financeDone} label="Finance Done" name="financeDone">
-                                                    <Select placeholder="Select" {...selectProps}>
-                                                        <Option value="yes">Yes</Option>
-                                                        <Option value="no">No</Option>
-                                                    </Select>
-                                                </Form.Item>
-                                            </Col>
-                                        </Row>
-                                        <Row gutter={20}>
-                                            <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.doReceived} label="D.O. Recived" name="doReceived">
-                                                    <Select placeholder="Select" {...selectProps}>
-                                                        <Option value="yes">Yes</Option>
-                                                        <Option value="no">No</Option>
-                                                    </Select>
-                                                </Form.Item>
-                                            </Col>
-                                            {selected === 'yes' && (
-                                                <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                    <Form.Item label="D.O. Number" name="doNumber" data-testid="customerType">
-                                                        <Input placeholder={preparePlaceholderText('d.o. number')}></Input>
-                                                    </Form.Item>
-                                                </Col>
-                                            )}
-                                            {selected === 'yes' && (
-                                                <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                    <Form.Item label="D.O. Date" name="doDate" data-testid="CustomerCategory">
-                                                        <DatePicker placeholder={preparePlaceholderSelect('date')} style={datePickerStyle} disabledDate={(date) => date > dayjs()} />
-                                                    </Form.Item>
-                                                </Col>
-                                            )}
-                                        </Row>
-                                    </Form>
+                                        )}
+                                    </Row>
+                                    {/* </Form> */}
                                 </Card>
                             </Space>
                         </Col>
--- src/components/Sales/OTF/FinananceDetails/FinananceDetailsMaster.js
+++ src/components/Sales/OTF/FinananceDetails/FinananceDetailsMaster.js
@@ -38,6 +38,7 @@ const mapDispatchToProps = (dispatch) => ({
         {
             fetchList: otfFinanceDetailDataActions.fetchList,
             saveData: otfFinanceDetailDataActions.saveData,
+            resetData: otfFinanceDetailDataActions.reset,
             listShowLoading: otfFinanceDetailDataActions.listShowLoading,
             showGlobalNotification,
         },
