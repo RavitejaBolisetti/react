b7c27c4cfc34a4f4876e4505bccad3d6e3c932a7
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-06-19 20:09:31 +0530

    Leftside bar issue fixed in customer module

--- src/components/common/CustomerMaster/FirmOrCompany/CompanyProfile/CompanyProfile.js
+++ src/components/common/CustomerMaster/FirmOrCompany/CompanyProfile/CompanyProfile.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import  AddEditForm  from './AddEditForm';
+import AddEditForm from './AddEditForm';
 
 const CompanyProfileBase = (props) => {
     return (
         <>
-            <h2>Company Profile</h2>
             <AddEditForm {...props} />
         </>
     );
