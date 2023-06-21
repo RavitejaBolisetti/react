226612b2043c0cb341eec5bcba6aa826097d6439
K Aman Rao <aman.rao1@wipro.com-at-803584912514>
2023-06-21 17:00:45 +0530

    handle edit mode in individual wip

--- src/components/common/CustomerMaster/IndividualCustomer/CustomerDetail/ViewDetail.js
+++ src/components/common/CustomerMaster/IndividualCustomer/CustomerDetail/ViewDetail.js
@@ -1,5 +1,10 @@
 /*
- *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd. 
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd. 
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
@@ -22,62 +27,51 @@ const ViewDetailMain = (props) => {
     };
     console.log(formData, 'FORMDATA');
     return (
-        <div className={styles.viewDrawerContainer}>
-            <Space style={{ display: 'flex' }} direction="vertical" size="middle">
-                <Card
-                    header={
-                        <div className={styles.alignUser}>
-                            <FaRegUserCircle className={styles.userCircle} />
-                            <Text strong style={{ marginTop: '4px', marginLeft: '8px' }}>
-                                Customer Information
-                            </Text>
-                        </div>
-                    }
-                >
-                    <Descriptions {...viewProps}>
-                        <Descriptions.Item label="Mobile Number">{formData?.mobileNumber}</Descriptions.Item>
-                        <Descriptions.Item label="Customer Type">{formData?.customerType}</Descriptions.Item>
-                    </Descriptions>
-                    <div className={styles.cardInsideBox}>
-                        <Row>
-                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                <Text>Customer Name</Text>
-                            </Col>
-                            <Col xs={24} sm={24} md={12} lg={12} xl={12} style={{ textAlign: 'right' }}>
-                                <Button type="link" icon={<BiTimeFive />}>
-                                    View History
-                                </Button>
-                            </Col>
-                            <Divider />
-                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                {formData?.title}
-                                <span className={styles.nameSpacing}></span>
-                                {formData?.firstName}
-                                <span className={styles.nameSpacing}></span>
-                                {formData?.middleName}
-                                <span className={styles.nameSpacing}></span>
-                                {formData?.lastName}
-                            </Col>
-                            <Col xs={24} sm={24} md={12} lg={12} xl={12} style={{ textAlign: 'right' }}>
-                                <Button type="link" icon={<FiEdit />}>
-                                    Edit
-                                </Button>
-                            </Col>
-                        </Row>
-                    </div>
-                    <Descriptions>
-                        <Descriptions.Item label="Email Id">{formData?.emailId}</Descriptions.Item>
-                        <Descriptions.Item label="Do you want to contact over whatsapp?">{formData?.corporateName}</Descriptions.Item>
-                        <Descriptions.Item label="Whatsapp Number">{formData?.whatsappNumber}</Descriptions.Item>
+        <Card style={{ backgroundColor: '#F2F2F2' }}>
+            <div className={styles.viewDrawerContainer}>
+                <Descriptions {...viewProps}>
+                    <Descriptions.Item label="Mobile Number">{formData?.mobileNumber}</Descriptions.Item>
+                    <Descriptions.Item label="Customer Type">{formData?.customerType}</Descriptions.Item>
+                </Descriptions>
+                <div className={styles.cardInsideBox}>
+                    <Row>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                            <Text>Customer Name</Text>
+                        </Col>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={12} style={{ textAlign: 'right' }}>
+                            <Button type="link" icon={<BiTimeFive />}>
+                                View History
+                            </Button>
+                        </Col>
                         <Divider />
-                        <Descriptions.Item label="Corporate Type">{formData?.corporateType}</Descriptions.Item>
-                        <Descriptions.Item label="Corporate Name">{formData?.corporateName}</Descriptions.Item>
-                        <Descriptions.Item label="Corporate Category">{formData?.corporateCategory}</Descriptions.Item>
-                        <Descriptions.Item label="Membership Type">{formData?.membershipTypeType}</Descriptions.Item>
-                    </Descriptions>
-                </Card>
-            </Space>
-        </div>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                            {formData?.title}
+                            <span className={styles.nameSpacing}></span>
+                            {formData?.firstName}
+                            <span className={styles.nameSpacing}></span>
+                            {formData?.middleName}
+                            <span className={styles.nameSpacing}></span>
+                            {formData?.lastName}
+                        </Col>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={12} style={{ textAlign: 'right' }}>
+                            <Button type="link" icon={<FiEdit />}>
+                                Edit
+                            </Button>
+                        </Col>
+                    </Row>
+                </div>
+                <Descriptions {...viewProps}>
+                    <Descriptions.Item label="Email Id">{formData?.emailId}</Descriptions.Item>
+                    <Descriptions.Item label="Do you want to contact over whatsapp?">{formData?.contactOnWhatsAppAllowed}</Descriptions.Item>
+                    <Descriptions.Item label="Want to use Mobile no as whatsapp no?">{formData?.contactAsMobileOnWhatApp}</Descriptions.Item>
+                    <Divider />
+                    <Descriptions.Item label="Corporate Type">{formData?.corporateType}</Descriptions.Item>
+                    <Descriptions.Item label="Corporate Name">{formData?.corporateName}</Descriptions.Item>
+                    <Descriptions.Item label="Corporate Category">{formData?.corporateCategory}</Descriptions.Item>
+                    <Descriptions.Item label="Membership Type">{formData?.membershipTypeType}</Descriptions.Item>
+                </Descriptions>
+            </div>
+        </Card>
     );
 };
 
