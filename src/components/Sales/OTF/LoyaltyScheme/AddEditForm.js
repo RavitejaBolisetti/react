6a4cb2d14eab6ba7e8ac2c09abd9e60fd38c39b2
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-06-22 23:05:12 +0530

    Working on code structure

--- src/components/Sales/OTF/LoyaltyScheme/AddEditForm.js
+++ src/components/Sales/OTF/LoyaltyScheme/AddEditForm.js
@@ -1,12 +1,11 @@
 /*
- *   Copyright (c) 2023 Mahindra & Mahindra Ltd. 
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
 import React, { useEffect } from 'react';
 
 import styles from 'components/common/Common.module.css';
-import { ViewDetail } from './ViewDetail';
 import { preparePlaceholderText } from 'utils/preparePlaceholder';
 
 import { validateRequiredInputField } from 'utils/validation';
@@ -37,121 +36,113 @@ const AddEditFormMain = (props) => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [formdata]);
-    const viewProps = {
-        styles,
-        customerForm: formdata,
-        isLoyaltySchemeDataLoaded,
-    };
+    
 
-    return !formActionType?.viewMode ? (
-        <Skeleton loading={!isLoyaltySchemeDataLoaded}>
-            <Card className={styles.ExchangeCard}>
-                <Form form={form} autoComplete="off" layout="vertical" colon={false} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-                    <Row gutter={20}>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="customerId" label="Customer ID" initialValue={formData?.customerId}>
-                                <Input maxLength={6} placeholder={preparePlaceholderText('id')} disabled={true} />
-                            </Form.Item>
-                        </Col>
+    return (
+        <Card className={styles.ExchangeCard}>
+            <Form form={form} autoComplete="off" layout="vertical" colon={false} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+                <Row gutter={20}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="customerId" label="Customer ID" initialValue={formData?.customerId}>
+                            <Input maxLength={6} placeholder={preparePlaceholderText('id')} disabled={true} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="customerName" label="Customer Name" initialValue={formData?.customerName}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('customer name')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="customerName" label="Customer Name" initialValue={formData?.customerName}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('customer name')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="make" label="Make" initialValue={formData?.make}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('make')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="make" label="Make" initialValue={formData?.make}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('make')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-                    <Row gutter={20}>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="modelGroup" label="Model Group" initialValue={formData?.modelGroup}>
-                                <Input maxLength={6} placeholder={preparePlaceholderText('Model Group')} disabled={true} />
-                            </Form.Item>
-                        </Col>
+                <Row gutter={20}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="modelGroup" label="Model Group" initialValue={formData?.modelGroup}>
+                            <Input maxLength={6} placeholder={preparePlaceholderText('Model Group')} disabled={true} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="variant" label="Variant" initialValue={formData?.variant}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('variant')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="variant" label="Variant" initialValue={formData?.variant}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('variant')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="oldRegNumber" label="Old Registration No" initialValue={formData?.oldRegNumber}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('old registration no')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="oldRegNumber" label="Old Registration No" initialValue={formData?.oldRegNumber}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('old registration no')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-                    <Row gutter={20}>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="oldChassisNumber" label="Old Chassis No" initialValue={formData?.oldChassisNumber}>
-                                <Input maxLength={50} placeholder={preparePlaceholderText('old chassis no')} disabled={true} />
-                            </Form.Item>
-                        </Col>
+                <Row gutter={20}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="oldChassisNumber" label="Old Chassis No" initialValue={formData?.oldChassisNumber}>
+                            <Input maxLength={50} placeholder={preparePlaceholderText('old chassis no')} disabled={true} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="dob" label="Date Of Birth" initialValue={formData?.dob}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('date of birth')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="dob" label="Date Of Birth" initialValue={formData?.dob}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('date of birth')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="relationship" label="Relationship" initialValue={formData?.relationship}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('relationship')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="relationship" label="Relationship" initialValue={formData?.relationship}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('relationship')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-                    <Row gutter={20}>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="year" label="Year Of Registration" initialValue={formData?.year}>
-                                <Input disabled={true} maxLength={50} placeholder={preparePlaceholderText('year of registration')} />
-                            </Form.Item>
-                        </Col>
+                <Row gutter={20}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="year" label="Year Of Registration" initialValue={formData?.year}>
+                            <Input disabled={true} maxLength={50} placeholder={preparePlaceholderText('year of registration')} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="month" label="Month Of Registration" initialValue={formData?.month}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('month of registration')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="month" label="Month Of Registration" initialValue={formData?.month}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('month of registration')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="usage" label="Usage" initialValue={formData?.usage}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('usage')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="usage" label="Usage" initialValue={formData?.usage}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('usage')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-                    <Row gutter={20}>
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="schemeName" label="Scheme Name" initialValue={formData?.schemeName}>
-                                <Input disabled={true} maxLength={50} placeholder={preparePlaceholderText('scheme name')} />
-                            </Form.Item>
-                        </Col>
+                <Row gutter={20}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="schemeName" label="Scheme Name" initialValue={formData?.schemeName}>
+                            <Input disabled={true} maxLength={50} placeholder={preparePlaceholderText('scheme name')} />
+                        </Form.Item>
+                    </Col>
 
-                        <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
-                            <Form.Item name="schemeAmount" label="Scheme Amount" initialValue={formData?.schemeAmount}>
-                                <Input disabled={true} placeholder={preparePlaceholderText('scheme amount')} maxLength={50} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row gutter={20}>
-                        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-                            <Form.Item name="remarks" label="Remarks" initialValue={formData?.remarks}>
-                                <TextArea disabled={true} placeholder={preparePlaceholderText('remarks')} maxLength={200} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Form>
-            </Card>
-        </Skeleton>
-    ) : (
-        <ViewDetail {...viewProps} />
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8}>
+                        <Form.Item name="schemeAmount" label="Scheme Amount" initialValue={formData?.schemeAmount}>
+                            <Input disabled={true} placeholder={preparePlaceholderText('scheme amount')} maxLength={50} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={20}>
+                    <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+                        <Form.Item name="remarks" label="Remarks" initialValue={formData?.remarks}>
+                            <TextArea disabled={true} placeholder={preparePlaceholderText('remarks')} maxLength={200} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </Form>
+        </Card>
     );
 };
 
--- src/components/Sales/OTF/LoyaltyScheme/LoyaltySchemeMaster.js
+++ src/components/Sales/OTF/LoyaltyScheme/LoyaltySchemeMaster.js
@@ -1,17 +1,21 @@
 /*
- *   Copyright (c) 2023 Mahindra & Mahindra Ltd. 
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
 import React, { useEffect, useState } from 'react';
 import styles from 'components/common/Common.module.css';
 import { AddEditForm } from './AddEditForm';
-import { Form } from 'antd';
+import { Form, Row, Col } from 'antd';
 import { showGlobalNotification } from 'store/actions/notification';
 import { otfLoyaltySchemeDataActions } from 'store/actions/data/otf/loyaltyAndScheme';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { CardSkeleton } from 'components/common/Skeleton';
+import { InputSkeleton } from 'components/common/Skeleton';
+import { OTFStatusBar } from '../utils/OTFStatusBar';
+import { OTFFormButton } from '../OTFFormButton';
+import { ViewDetail } from './ViewDetail';
+
 const mapStateToProps = (state) => {
     const {
         auth: { userId },
@@ -48,8 +52,9 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const LoyaltySchemeMasterMain = (props) => {
-    const { isLoyaltySchemeDataLoaded, resetData, listShowLoading, fetchList, LoyaltySchemeData, userId, showGlobalNotification } = props;
-    const [form] = Form.useForm();
+    const { isLoyaltySchemeDataLoaded, isLoading, resetData, section, listShowLoading, fetchList, LoyaltySchemeData, userId, showGlobalNotification } = props;
+    const { form, selectedOrderId, formActionType, handleFormValueChange } = props;
+
     const [formdata, setformdata] = useState();
 
     const onSuccessAction = (res) => {};
@@ -62,7 +67,7 @@ const LoyaltySchemeMasterMain = (props) => {
         {
             key: 'otfNumber',
             title: 'otfNumber',
-            value: 'OTF001',
+            value: selectedOrderId,
             name: 'otfNumber',
         },
     ];
@@ -86,7 +91,7 @@ const LoyaltySchemeMasterMain = (props) => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [LoyaltySchemeData]);
-    const LoyaltySchemeMasterProps = {
+    const formProps = {
         ...props,
         form,
         onFinishFailed,
@@ -96,16 +101,40 @@ const LoyaltySchemeMasterMain = (props) => {
         setformdata,
         isLoyaltySchemeDataLoaded,
     };
+    const viewProps = {
+        styles,
+        customerForm: formdata,
+        isLoyaltySchemeDataLoaded,
+    };
+    const formContainer = formActionType?.viewMode ? <ViewDetail {...viewProps} /> : <AddEditForm {...formProps} />;
+    const formSkeleton = (
+        <Row>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                <InputSkeleton height={'100vh'} />
+            </Col>
+        </Row>
+    );
     return (
-        <>
-            {isLoyaltySchemeDataLoaded ? (
-                <div className={styles.drawerCustomerMaster}>
-                    <AddEditForm {...LoyaltySchemeMasterProps} />
-                </div>
-            ) : (
-                <CardSkeleton />
-            )}
-        </>
+        <Form layout="vertical" autoComplete="off" form={form} onValuesChange={handleFormValueChange} onFieldsChange={handleFormValueChange} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+            <Row gutter={20} className={styles.drawerBodyRight}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                    <Row>
+                        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+                            <h2>{section?.title}</h2>
+                        </Col>
+                        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+                            <OTFStatusBar status={1} />
+                        </Col>
+                    </Row>
+                    {isLoading ? formSkeleton : formContainer}
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+                    <OTFFormButton {...props} />
+                </Col>
+            </Row>
+        </Form>
     );
 };
 export const LoyaltySchemeMaster = connect(mapStateToProps, mapDispatchToProps)(LoyaltySchemeMasterMain);
