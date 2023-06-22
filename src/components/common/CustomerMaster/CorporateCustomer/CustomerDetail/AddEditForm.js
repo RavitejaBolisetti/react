d8cf3448de660857fa744b02c1b637f9756afa85
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-06-22 12:18:04 +0000

    Squashed commit of the following
    commit cf9b63b5 
    Author: K Aman Rao <aman.rao1@wipro.com-at-803584912514> 
    Date: Thu Jun 22 2023 16:09:10 GMT+0530 (India Standard Time) 
    
        Button render on Edit View
    
    
    commit 2cf981cd 
    Author: K Aman Rao <aman.rao1@wipro.com-at-803584912514> 
    Date: Thu Jun 22 2023 14:25:44 GMT+0530 (India Standard Time) 
    
        Update view details keys and work on edit mode mobile number validation
    #wip
    
    
    commit f09c1921 
    Author: K Aman Rao <aman.rao1@wipro.com-at-803584912514> 
    Date: Thu Jun 22 2023 13:23:02 GMT+0530 (India Standard Time) 
    
        Disabled field Customer Type
    WIP

--- src/components/common/CustomerMaster/CorporateCustomer/CustomerDetail/AddEditForm.js
+++ src/components/common/CustomerMaster/CorporateCustomer/CustomerDetail/AddEditForm.js
@@ -3,17 +3,20 @@
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
-import { Col, Input, Form, Row, Select, Space, Divider, Card } from 'antd';
+import { Col, Input, Form, Row, Select, Space, Divider, Card, Button } from 'antd';
 import { validateMobileNoField, validateRequiredInputField, validateRequiredSelectField } from 'utils/validation';
 import { preparePlaceholderText } from 'utils/preparePlaceholder';
 import { useEffect, useState } from 'react';
-import { BiEdit } from 'react-icons/bi';
-import styles from 'components/common/Common.module.css';
+import { BiLockAlt } from 'react-icons/bi';
+import { CheckOutlined } from '@ant-design/icons';
+import { ValidateMobileNumberModal } from '../../IndividualCustomer/CustomerDetail/ValidateMobileNumberModal';
 const { Option } = Select;
 
 const AddEditFormMain = (props) => {
-    const { configurableTypedata, formData, form, corporateLovData, formActionType: { editMode } = undefined } = props;
+    const { configurableTypedata, formData, form, corporateLovData, formActionType: { editMode } = undefined,customerType } = props;
     const [corporateType, setCorporateType] = useState();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [mobileLoader, setmobileLoader] = useState(false);
     useEffect(() => {
         form.setFieldsValue({
             ...formData,
@@ -23,19 +26,69 @@ const AddEditFormMain = (props) => {
     const handleCorporateChange = (value) => {
         setCorporateType(value);
     };
+    const handleNumberValidation = (event) => {
+        const Mno = event.target.value;
+        const regex = new RegExp('^([5-9]){1}([0-9]){9}$');
+        if (Mno?.length === 10 && regex.test(Mno)) {
+            setmobileLoader(true);
+            setTimeout(() => {
+                setIsModalOpen(true);
+            }, 1000);
+        } else {
+            setmobileLoader(false);
+        }
+    };
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
 
+    const handleCancel = () => {
+        setIsModalOpen(false);
+        setmobileLoader(false);
+    };
+
+    const modalProps = {
+        isVisible: isModalOpen,
+        icon: <BiLockAlt />,
+        titleOverride: 'Mobile Number Validation',
+        closable: false,
+        onCloseAction: handleCancel,
+    };
     return (
         <Space direction="vertical" size="small" style={{ display: 'flex' }}>
             <Card style={{ backgroundColor: '#F2F2F2' }}>
                 <Row gutter={20}>
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                        <Form.Item initialValue={formData?.mobileNumber} label="Mobile Number" name="mobileNumber" data-testid="mobileNumber" rules={[validateMobileNoField('mobile number')]}>
-                            <Input placeholder={preparePlaceholderText('mobile number')} maxLength={10} suffix={<BiEdit className={styles.icon} size={18} />} disabled={editMode} />
-                        </Form.Item>
+                    {editMode ? (
+                                <Form.Item label="Mobile Number" initialValue={formData?.mobileNumber} name="mobileNumber" data-testid="mobileNumber" rules={[validateMobileNoField('mobile number')]}>
+                                    <Input
+                                        placeholder={preparePlaceholderText('mobile number')}
+                                        onChange={handleNumberValidation}
+                                        maxLength={10}
+                                        size="small"
+                                        suffix={
+                                            <>
+                                                {false ? (
+                                                    <Button loading={mobileLoader} onClick={showModal} type="link">
+                                                        Validate
+                                                    </Button>
+                                                ) : (
+                                                    <CheckOutlined style={{ color: '#70c922', fontSize: '16px', fotWeight: 'bold' }} />
+                                                )}
+                                                <ValidateMobileNumberModal {...modalProps} />
+                                            </>
+                                        }
+                                    />
+                                </Form.Item>
+                            ) : (
+                                <Form.Item label="Mobile Number" initialValue={formData?.mobileNumber} name="mobileNumber" data-testid="mobileNumber" rules={[validateMobileNoField('mobile number')]}>
+                                    <Input placeholder={preparePlaceholderText('mobile number')} maxLength={10} size="small"  />
+                                </Form.Item>
+                            )}
                     </Col>
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                         <Form.Item initialValue={formData?.customerType} label="Customer Type" name="customerType" data-testid="customerType" rules={[validateRequiredSelectField('customer Type')]}>
-                            <Select placeholder="Select" disabled={false} loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['CUST_TYPE']}></Select>
+                            <Select placeholder="Select"  defaultValue={{ label: 'Individual', value: 'IND' }}  allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['CUST_TYPE']}></Select>
                         </Form.Item>
                     </Col>
                 </Row>
@@ -62,14 +115,14 @@ const AddEditFormMain = (props) => {
                 <Row gutter={20}>
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                         <Form.Item initialValue={formData?.corporateType} label="Corporate Type" name="corporateType" data-testid="corporateType">
-                            <Select placeholder="Select" disabled={editMode} loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['CORP_TYPE']} onChange={handleCorporateChange}></Select>
+                            <Select placeholder="Select" loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['CORP_TYPE']} onChange={handleCorporateChange}></Select>
                         </Form.Item>
                     </Col>
                     {corporateType === 'LIS' ? (
                         <>
                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                                 <Form.Item label="Corporate Name" initialValue={formData?.corporateName} name="corporateName" data-testid="corporateName">
-                                    <Select disabled={editMode} loading={false} placeholder="Select" allowClear>
+                                    <Select loading={false} placeholder="Select" allowClear>
                                         {corporateLovData?.map((item) => (
                                             <Option key={'lv' + item?.key} value={item?.key}>
                                                 {item?.value}
@@ -98,7 +151,7 @@ const AddEditFormMain = (props) => {
                     </Col>
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                         <Form.Item initialValue={formData?.membershipType} label="Membership Type" name="membershipType" data-testid="membershipType" rules={[validateRequiredSelectField('membership type')]}>
-                            <Select placeholder="Select" disabled={editMode} loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['MEM_TYPE']}></Select>
+                            <Select placeholder="Select" loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['MEM_TYPE']}></Select>
                         </Form.Item>
                     </Col>
                 </Row>
--- src/components/common/CustomerMaster/IndividualCustomer/CustomerDetail/ValidateMobileNumberModal.js
+++ src/components/common/CustomerMaster/IndividualCustomer/CustomerDetail/ValidateMobileNumberModal.js
@@ -15,16 +15,13 @@ import { FiLock } from 'react-icons/fi';
 
 const ValidateMobileModalMain = (props) => {
     const { onCloseAction } = props;
-    const [otpModal, setOtpModal] = useState(false);
     const [counter, setCounter] = useState(false);
     const [otpMessage, setOTPMessage] = useState();
     const [inValidOTP, setInValidOTP] = useState(false);
     const [otpInput, setOTPInput] = useState();
     const [disableVerifyOTP, setDisableVerifyOTP] = useState(true);
 
-    const handleOtpModal = () => {
-        setOtpModal(true);
-    };
+
     useEffect(() => {
         const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
         return () => {
@@ -45,84 +42,62 @@ const ValidateMobileModalMain = (props) => {
 
     return (
         <>
-            {!otpModal ? (
-                <>
-                    <Row>
-                        <Col xs={24} sm={24} md={24} lg={24} xl={24} className={styles.information}>
-                            The mobile number you have entered is already used for customer ID-C12345678. Do you want to continue with the same number?
-                        </Col>
-                    </Row>
-                    <Row gutter={20}>
-                        <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                            <Space>
-                                <Button ghost onClick={onCloseAction} htmlType="submit" type="primary" className={styles.modalButton}>
-                                    No
-                                </Button>
-                                <Button onClick={handleOtpModal} htmlType="submit" type="primary" className={styles.modalButton}>
-                                    Yes, Continue
-                                </Button>
-                            </Space>
-                        </Col>
-                    </Row>
-                </>
-            ) : (
-                <>
-                    <div className={styles.centerInner}>
-                        <div className={styles.loginForm}>
-                            <div className={styles.loginHeading}>
-                                <h1>OTP Verification</h1>
-                                <div className={styles.loginSubHeading}>{otpMessage}</div>
-                            </div>
-                            <Row gutter={20}>
-                                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                                    <div className={styles.otpTitle}>Enter OTP</div>
-                                </Col>
-                            </Row>
-                            <Row gutter={20}>
-                                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                                    <OTPInput className={`${styles.changer} ${inValidOTP ? styles.otpFilled : styles.otpEmpty}`} otpType="number" value={otpInput} onChange={handleOTPInput} autoFocus OTPLength={6} disabled={false} />
-                                </Col>
-                            </Row>
-                            <Row gutter={20}>
-                                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                                    <Row gutter={20} className={styles.otpVerificationContainer}>
-                                        <Col xs={14} sm={14} md={16} lg={16} xl={16}>
-                                            {counter ? (
-                                                <div className={styles.otpCounter}>{`${counter >= 10 ? `00:${counter}` : `00:0${counter}`}s`}</div>
-                                            ) : (
-                                                <div className={styles.otpNotReceive}>
-                                                    {inValidOTP ? (
-                                                        <span>
-                                                            <RxCrossCircled />
-                                                            {'Incorrect OTP'}
-                                                        </span>
-                                                    ) : (
-                                                        <span>{"Didn't receive an OTP?"}</span>
-                                                    )}
-                                                </div>
-                                            )}
-                                        </Col>
-                                        <Col xs={10} sm={10} md={8} lg={8} xl={8}>
-                                            <div onClick={false} className={counter ? styles.resendDisabled : styles.resendEnabled} type="radio">
-                                                <TbRefresh /> Resend OTP
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            </Row>
-                            <Button icon={<FiLock size={18} />} onClick={onCloseAction} disabled={false} loading={false} className={styles.button} type="primary">
-                                Verify OTP
-                            </Button>
-
-                            <Row gutter={20}>
-                                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                                    <div className={styles.loginFooter} type="radio"></div>
-                                </Col>
-                            </Row>
+            <>
+                <div className={styles.centerInner}>
+                    <div className={styles.loginForm}>
+                        <div className={styles.loginHeading}>
+                            <h1>OTP Verification</h1>
+                            <div className={styles.loginSubHeading}>{otpMessage}</div>
                         </div>
+                        <Row gutter={20}>
+                            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                <div className={styles.otpTitle}>Enter OTP</div>
+                            </Col>
+                        </Row>
+                        <Row gutter={20}>
+                            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                <OTPInput className={`${styles.changer} ${inValidOTP ? styles.otpFilled : styles.otpEmpty}`} otpType="number" value={otpInput} onChange={handleOTPInput} autoFocus OTPLength={6} disabled={false} />
+                            </Col>
+                        </Row>
+                        <Row gutter={20}>
+                            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                <Row gutter={20} className={styles.otpVerificationContainer}>
+                                    <Col xs={14} sm={14} md={16} lg={16} xl={16}>
+                                        {counter ? (
+                                            <div className={styles.otpCounter}>{`${counter >= 10 ? `00:${counter}` : `00:0${counter}`}s`}</div>
+                                        ) : (
+                                            <div className={styles.otpNotReceive}>
+                                                {inValidOTP ? (
+                                                    <span>
+                                                        <RxCrossCircled />
+                                                        {'Incorrect OTP'}
+                                                    </span>
+                                                ) : (
+                                                    <span>{"Didn't receive an OTP?"}</span>
+                                                )}
+                                            </div>
+                                        )}
+                                    </Col>
+                                    <Col xs={10} sm={10} md={8} lg={8} xl={8}>
+                                        <div onClick={false} className={counter ? styles.resendDisabled : styles.resendEnabled} type="radio">
+                                            <TbRefresh /> Resend OTP
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Button icon={<FiLock size={18} />} onClick={onCloseAction} disabled={disableVerifyOTP} loading={false} className={styles.button} type="primary">
+                            Verify OTP
+                        </Button>
+
+                        <Row gutter={20}>
+                            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                                <div className={styles.loginFooter} type="radio"></div>
+                            </Col>
+                        </Row>
                     </div>
-                </>
-            )}
+                </div>
+            </>
         </>
     );
 };
