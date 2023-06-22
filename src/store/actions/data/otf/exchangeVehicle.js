1a6c992939bb86a494eddfd48ce1ca0eff139312
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-06-22 12:03:42 +0000

    Squashed commit of the following
    commit 8fac7fef 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Thu Jun 22 2023 16:23:21 GMT+0530 (India Standard Time) 
    
        worked on API integration
    
    WIP #1411
    
    
    commit 49a935b6 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Thu Jun 22 2023 13:51:27 GMT+0530 (India Standard Time) 
    
        worked on view mode
    
    WIP #1411
    
    
    commit 487fc4b7 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Thu Jun 22 2023 12:59:16 GMT+0530 (India Standard Time) 
    
        worked on API integration in exchange vehicle master
    
    WIP #1411
    
    
    commit 75a8aed6 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Thu Jun 22 2023 00:11:22 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-1411-Exchange-Vehicle-Master
    
    
    commit 622354cf 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Thu Jun 22 2023 00:00:13 GMT+0530 (India Standard Time) 
    
        worked on exchange customer
    
    WIP #1411

--- src/store/actions/data/otf/exchangeVehicle.js
+++ src/store/actions/data/otf/exchangeVehicle.js
@@ -4,10 +4,10 @@
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
 import { dataActions } from 'store/actions/crud/dataAction';
-import { BASE_URL_CRITICALITY_GROUP as baseURL } from 'constants/routingApi';
+import { BASE_URL_OTF_EXCHANGE_VEHICLE as baseURL } from 'constants/routingApi';
 
-const PREFIX = 'CRITICALITY_';
-const moduleName = 'Application Criticality Group';
+const PREFIX = 'OTF_EXCHANGE_VEHICLE_';
+const moduleName = 'Exchange Vehicle';
 
 export const RECEIVE_DATA_LOADING_ACTION_CONSTANT = PREFIX + 'LOADING_DATA';
 export const RECEIVE_DATA_ACTION_CONSTANT = PREFIX + 'LIST_RECIEVE_DATA';
@@ -15,9 +15,8 @@ export const RECEIVE_DATA_ACTION_APPLY_FILTER_CONSTANT = PREFIX + 'LIST_APPLY_FI
 export const RECIEVE_DATA_DETAIL_ACTION_CONSTANT = PREFIX + 'RECIEVE_DETAIL_DATA';
 export const SAVE_DATA_ACTION_CONSTANT = PREFIX + 'SAVE_DATA';
 export const RESET_DATA_ACTION_CONSTANT = PREFIX + 'RESET_DATA';
-export const SAVE_FORM_DATA_LOADING_CONSTANT = PREFIX + 'SAVE_FORM_DATA_LOADING_CONSTANT';
 
-export const criticalityDataActions = dataActions({
+export const otfExchangeVehicleDataActions = dataActions({
     baseURL,
     moduleName,
     RECEIVE_DATA_LOADING_ACTION_CONSTANT,
@@ -26,6 +25,4 @@ export const criticalityDataActions = dataActions({
     RECIEVE_DATA_DETAIL_ACTION_CONSTANT,
     SAVE_DATA_ACTION_CONSTANT,
     RESET_DATA_ACTION_CONSTANT,
-    SAVE_FORM_DATA_LOADING_CONSTANT,
 });
- 
