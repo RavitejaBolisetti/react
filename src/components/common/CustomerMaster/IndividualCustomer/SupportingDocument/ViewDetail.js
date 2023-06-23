1df4dc131aa5f4de8df703875c636bbd263e637f
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-06-23 12:11:49 +0000

    Squashed commit of the following
    commit e131e09e 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Fri Jun 23 2023 17:26:46 GMT+0530 (India Standard Time) 
    
        Fixed design issue
    
    
    commit 850724dc 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Fri Jun 23 2023 17:19:51 GMT+0530 (India Standard Time) 
    
        Fixed supporting doc issue
    
    
    commit 864aaa23 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Fri Jun 23 2023 14:18:48 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-2144-Supporting-Files-Vault
    
    
    commit acca5630 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Fri Jun 23 2023 14:16:02 GMT+0530 (India Standard Time) 
    
        Merge branch 'US-2144-Supporting-Files-Vault' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/dms-ui-application into US-2144-Supporting-Files-Vault
    
    
    commit 26d6a59f 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Fri Jun 23 2023 14:14:37 GMT+0530 (India Standard Time) 
    
        Fixed supporting doc issue
    
    
    commit 88584d81 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 14:10:32 GMT+0530 (India Standard Time) 
    
        worked on download
    
    
    commit 3a896539 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Fri Jun 23 2023 14:14:04 GMT+0530 (India Standard Time) 
    
        Merge branch 'US-2144-Supporting-Files-Vault' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/dms-ui-application into US-2144-Supporting-Files-Vault
    
    
    commit 05839c25 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 12:27:33 GMT+0530 (India Standard Time) 
    
        worked on view mode
    
    WIP #2144
    
    
    commit 4eb408a3 
    Author: Kuldeep Singh <kuldeep.singh1@wipro.com> 
    Date: Fri Jun 23 2023 08:49:24 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-2144-Supporting-Files-Vault
    
    
    commit 1d52e571 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 09:01:04 GMT+0530 (India Standard Time) 
    
        code clean
    
    WIP #2144
    
    
    commit 175440ce 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 06:43:14 GMT+0530 (India Standard Time) 
    
        worked on code clean
    
    WIP #2144
    
    
    commit c1f3d9d7 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 08:58:31 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-2144-Supporting-Files-Vault
    
    
    commit 3124cf8e 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 06:42:03 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-2144-Supporting-Files-Vault
    
    
    commit 07ddf7cc 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 08:45:38 GMT+0530 (India Standard Time) 
    
        code clean
    
    WIP #2144
    
    
    commit cd24ffb1 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 03:57:31 GMT+0530 (India Standard Time) 
    
        worked on api integration
    
    WIP #2144
    
    
    commit aa27791a 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 08:44:42 GMT+0530 (India Standard Time) 
    
        worked on view mode
    
    WIP #2144
    
    
    commit 491f1cf1 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 03:31:26 GMT+0530 (India Standard Time) 
    
        worked on view mode
    
    
    commit e8e211c7 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 01:59:40 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-2144-Supporting-Files-Vault
    
    
    commit 9049cae5 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Fri Jun 23 2023 01:59:16 GMT+0530 (India Standard Time) 
    
        worked on
    
    
    commit c1b3112f 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Thu Jun 22 2023 17:27:29 GMT+0530 (India Standard Time) 
    
        working on view docs
    
    WIP #2144
    
    
    commit a217ddb3 
    Author: Nishant Malik <nishant.malik.ext@wipro.com> 
    Date: Wed Jun 21 2023 18:24:36 GMT+0530 (India Standard Time) 
    
        worked on view mode
    
    WIP #2144

--- src/components/common/CustomerMaster/IndividualCustomer/SupportingDocument/ViewDetail.js
+++ src/components/common/CustomerMaster/IndividualCustomer/SupportingDocument/ViewDetail.js
@@ -1,11 +1,35 @@
 /*
- *   Copyright (c) 2023 
+ *   Copyright (c) 2023
  *   All rights reserved.
  */
-import React from 'react'
+import React from 'react';
 
-export const ViewDetail = () => {
-  return (
-    <div>ViewDetail</div>
-  )
-}
+import { Row, Col, Card } from 'antd';
+import { FiEye } from 'react-icons/fi';
+
+export const ViewDetail = (props) => {
+    const { supportingData, handlePreview, viewDocument, showGlobalNotification } = props;
+
+    const downloadFile = (uploadData) => {
+        showGlobalNotification({ notificationType: 'success', title: 'Success', message: 'Your donload will start soon' });
+
+        handlePreview(uploadData);
+        let a = document.createElement('a');
+        a.href = `data:image/png;base64,${viewDocument?.base64}`;
+        a.download = viewDocument?.fileName;
+        a.click();
+    };
+    return (
+        <>
+            {supportingData.length > 0 ? (
+                <Card style={{ backgroundColor: '#F2F2F2' }}>
+                    {supportingData.map((uploadData) => {
+                        return <Card style={{ backgroundColor: '#E6E6E6' }} key={uploadData.id} title={uploadData?.documentName} extra={<FiEye onClick={() => downloadFile(uploadData)} />}></Card>;
+                    })}
+                </Card>
+            ) : (
+                <div>No Data Found</div>
+            )}
+        </>
+    );
+};
