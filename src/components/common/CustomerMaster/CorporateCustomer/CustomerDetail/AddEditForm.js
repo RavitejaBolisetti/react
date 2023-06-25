be8b6398ff36f7413c503bfb2117ee3f8a43a474
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-06-25 08:33:10 +0000

    Squashed commit of the following
    commit 6845e661 
    Author: Simran <Simran.sehgal@wipro.com> 
    Date: Sun Jun 25 2023 13:33:52 GMT+0530 (India Standard Time) 
    
        changes
    
    
    commit 2ec260e3 
    Author: Simran <Simran.sehgal@wipro.com> 
    Date: Sun Jun 25 2023 13:12:42 GMT+0530 (India Standard Time) 
    
        Merge branch 'US-2138-Customer-Details-FIXES' into US-Customer-View-Details
    
    
    commit 8703ad64 
    Author: Simran <Simran.sehgal@wipro.com> 
    Date: Sun Jun 25 2023 13:07:34 GMT+0530 (India Standard Time) 
    
        in corporate
    
    
    commit 2284d949 
    Author: K Aman Rao <aman.rao1@wipro.com-at-803584912514> 
    Date: Sun Jun 25 2023 13:01:07 GMT+0530 (India Standard Time) 
    
        typeData changed and Options mapping
    
    
    commit d10966b3 
    Author: Simran <Simran.sehgal@wipro.com> 
    Date: Sun Jun 25 2023 12:57:39 GMT+0530 (India Standard Time) 
    
        changes of inidiviual customer view details

--- src/components/common/CustomerMaster/CorporateCustomer/CustomerDetail/AddEditForm.js
+++ src/components/common/CustomerMaster/CorporateCustomer/CustomerDetail/AddEditForm.js
@@ -12,8 +12,10 @@ import { BiLockAlt } from 'react-icons/bi';
 import { CheckOutlined } from '@ant-design/icons';
 import { ValidateMobileNumberModal } from '../../IndividualCustomer/CustomerDetail/ValidateMobileNumberModal';
 
+const { Option } = Select;
+
 const AddEditFormMain = (props) => {
-    const { configurableTypedata, formData, form, corporateLovData, formActionType: { editMode } = undefined, validateParentCode, customerType, customerParentCompanyData } = props;
+    const { typeData, formData, form, corporateLovData, formActionType: { editMode } = undefined, validateParentCode, customerType, customerParentCompanyData } = props;
     const [corporateType, setCorporateType] = useState();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [mobileLoader, setmobileLoader] = useState(false);
@@ -89,7 +91,13 @@ const AddEditFormMain = (props) => {
                     </Col>
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                         <Form.Item initialValue={customerType} label="Customer Type" name="customerType" data-testid="customerType" rules={[validateRequiredSelectField('customer Type')]}>
-                            <Select disabled={true} placeholder={preparePlaceholderSelect('customer type')} fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['CUST_TYPE']} allowClear></Select>
+                            <Select disabled={true} placeholder={preparePlaceholderSelect('customer type')} allowClear>
+                                {typeData?.CUST_TYPE?.map((item) => (
+                                    <Option key={'ct' + item.key} value={item.key}>
+                                        {item.value}
+                                    </Option>
+                                ))}
+                            </Select>
                         </Form.Item>
                     </Col>
                 </Row>
@@ -116,7 +124,13 @@ const AddEditFormMain = (props) => {
                 <Row gutter={20}>
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                         <Form.Item initialValue={formData?.corporateType} label="Corporate Type" name="corporateType" data-testid="corporateType" rules={[validateRequiredSelectField('corporate type')]}>
-                            <Select placeholder={preparePlaceholderSelect('corporate type')} loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['CORP_TYPE']} onChange={handleCorporateChange}></Select>
+                            <Select placeholder={preparePlaceholderSelect('corporate type')} loading={false} allowClear onChange={handleCorporateChange}>
+                                {typeData?.CORP_TYPE?.map((item) => (
+                                    <Option key={'ct' + item.key} value={item.key}>
+                                        {item.value}
+                                    </Option>
+                                ))}
+                            </Select>
                         </Form.Item>
                     </Col>
 
@@ -142,12 +156,24 @@ const AddEditFormMain = (props) => {
                     )}
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                         <Form.Item initialValue={formData?.corporateCategory} label="Corporate Category" name="corporateCategory" data-testid="corporateCategory">
-                            <Select placeholder={preparePlaceholderSelect('corporate category')} disabled={editMode} loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['CORP_CATE']}></Select>
+                            <Select placeholder={preparePlaceholderSelect('corporate category')} disabled={editMode} loading={false} allowClear>
+                                {typeData?.CORP_CATE?.map((item) => (
+                                    <Option key={'cct' + item.key} value={item.key}>
+                                        {item.value}
+                                    </Option>
+                                ))}
+                            </Select>
                         </Form.Item>
                     </Col>
                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                         <Form.Item initialValue={formData?.membershipType} label="Membership Type" name="membershipType" data-testid="membershipType" rules={[validateRequiredSelectField('membership type')]}>
-                            <Select placeholder={preparePlaceholderSelect('membership type')} loading={false} allowClear fieldNames={{ label: 'value', value: 'key' }} options={configurableTypedata['MEM_TYPE']}></Select>
+                            <Select placeholder={preparePlaceholderSelect('membership type')} loading={false} allowClear>
+                                {typeData?.MEM_TYPE?.map((item) => (
+                                    <Option key={'mt' + item.key} value={item.key}>
+                                        {item.value}
+                                    </Option>
+                                ))}
+                            </Select>
                         </Form.Item>
                     </Col>
                 </Row>
