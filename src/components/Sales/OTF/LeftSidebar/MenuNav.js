fbdbccb46e9849e4379afb67579180e123089b5d
Ashutosh <ashutosh.sahu@wipro.com>
2023-06-27 01:19:00 +0530

    OTF master drawer profile card design update and other design fixes
    WIP

--- src/components/Sales/OTF/LeftSidebar/MenuNav.js
+++ src/components/Sales/OTF/LeftSidebar/MenuNav.js
@@ -3,7 +3,7 @@
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Timeline } from 'antd';
 import { BsRecordCircleFill } from 'react-icons/bs';
 import { FaCheckCircle } from 'react-icons/fa';
@@ -12,6 +12,25 @@ import { OTF_SECTION } from 'constants/OTFSection';
 
 const MenuNav = (props) => {
     const { currentSection, setCurrentSection } = props;
+
+    useEffect(() => {
+        if (currentSection) {
+            const TimeLineClass = document.getElementsByClassName('ant-timeline-item');
+            for (let i = 0; i < TimeLineClass.length; i++) {
+                const activeForm = TimeLineClass[i]['children']['1']['children']['0']['classList']['0'];
+                if (activeForm !== undefined && activeForm.match('Common_activeForm')) {
+                    TimeLineClass[i].firstChild.style.backgroundColor = '#ff3e5b';
+                    TimeLineClass[i].lastChild.firstChild.style.color = '#ff3e5b';
+                } else {
+                    TimeLineClass[i].firstChild.style.backgroundColor = '#b6b6b6';
+                    TimeLineClass[i].lastChild.firstChild.style.color = '#0b0b0c';
+                }
+            }
+            console.log('TimeLineClass', TimeLineClass);
+            TimeLineClass[TimeLineClass?.length - 1].firstChild.style.display = 'none';
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentSection]);
 
     const onHandle = (key) => {
         setCurrentSection(key);
--- src/components/Sales/OTF/LeftSidebar/OTFDetailCard.js
+++ src/components/Sales/OTF/LeftSidebar/OTFDetailCard.js
@@ -4,13 +4,14 @@
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
 import React from 'react';
-import { Collapse, Space, Avatar } from 'antd';
+import { Collapse, Space, Avatar, Typography } from 'antd';
 import { SlArrowDown, SlArrowUp } from 'react-icons/sl';
 import { convertDateTime } from 'utils/formatDateTime';
 import { CUSTOMER_TYPE } from 'constants/CustomerType';
 import { getCodeValue } from 'utils/getCodeValue';
 
 const { Panel } = Collapse;
+const { Text, Title } = Typography;
 const expandIcon = ({ isActive }) => (isActive ? <SlArrowUp size={13} /> : <SlArrowDown size={13} />);
 
 const OTFDetailCard = (props) => {
@@ -22,13 +23,11 @@ const OTFDetailCard = (props) => {
             <Panel
                 header={
                     <>
-                        <Space direction="vertical">
+                        <Space>
                             <Avatar size={60}>{userAvatar?.toUpperCase()}</Avatar>
                             <div>
-                                <p>
-                                    <span>{selectedOrder?.customerName}</span>
-                                </p>
-                                <p>{selectedOrder?.otfNumber}</p>
+                                <Title level={5}>{selectedOrder?.customerName}</Title>
+                                <Text>OTF N0.:&nbsp;{selectedOrder?.otfNumber}</Text>
                             </div>
                         </Space>
                     </>
