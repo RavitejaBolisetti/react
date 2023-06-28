562617bcb9fcafb954baf7e49912ff03a9109a65
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-06-28 15:09:29 +0530

    Added copy right into file
    wip #configure

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,27 @@
-module.exports = { 
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
+module.exports = {
     rootDir: 'src',
-    testEnvironment:'jsdom',
-    moduleFileExtensions: ['js','jsx'],
+    testEnvironment: 'jsdom',
+    moduleFileExtensions: ['js', 'jsx'],
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-        "\\.(css|scss|sass|less)$": "identity-obj-proxy",
-        "^@components(.*)$": "<rootDir>/components$1",
-        "^@pages(.*)$": "<rootDir>/pages$1",
-        "^@Icons(.*)$": "<rootDir>/Icons$1",
-        "^@store(.*)$": "<rootDir>/store$1",
-        "^@utils(.*)$": "<rootDir>/utils$1",
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|scss|sass|less)$': 'identity-obj-proxy',
+        '^@components(.*)$': '<rootDir>/components$1',
+        '^@pages(.*)$': '<rootDir>/pages$1',
+        '^@Icons(.*)$': '<rootDir>/Icons$1',
+        '^@store(.*)$': '<rootDir>/store$1',
+        '^@utils(.*)$': '<rootDir>/utils$1',
     },
-    moduleDirectories: ['node_modules','src'],
+    moduleDirectories: ['node_modules', 'src'],
     testMatch: ['**/__tests__/**/*.test.(js|jsx)'],
-    transform: { 
-        '^.+\\.(js|jsx)$':'babel-jest',
-     }, 
-    setupFilesAfterEnv: [
-        "@testing-library/jest-dom",
-        "<rootDir>/jest.setup.js",
-    ],
-    coveragePathIgnorePatterns: ["<rootDir>/state/","<rootDir>/language/","<rootDir>/utils/"],
-    transformIgnorePatterns: ["<rootDir>/node_modules/(?![a-z])", "<rootDir>/assets/(?![a-z])"],
- };
+    transform: {
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom', '<rootDir>/jest.setup.js'],
+    coveragePathIgnorePatterns: ['<rootDir>/state/', '<rootDir>/language/', '<rootDir>/utils/'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?![a-z])', '<rootDir>/assets/(?![a-z])'],
+};
--- src/__tests__/components/common/header/header.test.jsx
+++ src/__tests__/components/common/header/header.test.jsx
@@ -1,9 +1,14 @@
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
 import '@testing-library/jest-dom/extend-expect';
-import customRender from "@utils/test-utils";
-import { HeaderSkeleton } from "@components/common/Header/HeaderSkeleton";
+import customRender from '@utils/test-utils';
+import { HeaderSkeleton } from '@components/common/Header/HeaderSkeleton';
 
-describe("Header Components", () => {
-    it("should render HeaderSkeleton components", ()=> {
-      customRender(<HeaderSkeleton />);
+describe('Header Components', () => {
+    it('should render HeaderSkeleton components', () => {
+        customRender(<HeaderSkeleton />);
     });
 });
