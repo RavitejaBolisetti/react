b856c9aa1fff43ab128b4c97fcee60c9274293e8
Sujeet Kumar Singh <SU20133297@wipro.com>
2023-07-05 18:16:38 +0530

    US-5207-Term-Condition-Issue-Fixed
    #Fixed issue of Company Profile

--- src/components/common/CustomerMaster/CorporateCustomer/CompanyProfile/AddEditForm.js
+++ src/components/common/CustomerMaster/CorporateCustomer/CompanyProfile/AddEditForm.js
@@ -200,7 +200,7 @@ const AddEditFormMain = (props) => {
                                         </Form.Item>
                                     </Col>
                                 </Row>
-                                {customerCategory === 'CUS_CAT_2' && (
+                                {(customerCategory === 'CUS_CAT_2' || formData?.customerCategory === 'CUS_CAT_2') && (
                                     <>
                                         <Divider />
                                         <Row gutter={20}>
