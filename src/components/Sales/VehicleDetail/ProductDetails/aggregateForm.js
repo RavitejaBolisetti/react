ac47058f9a57f00c2c78dff6d766a5c7ba956f0b
Vivek Verma <vivek.verma.ext@wipro.com>
2023-07-12 16:10:22 +0530

    changes in options
    wip #productDetails

--- src/components/Sales/VehicleDetail/ProductDetails/aggregateForm.js
+++ src/components/Sales/VehicleDetail/ProductDetails/aggregateForm.js
@@ -4,7 +4,7 @@
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Input, Form, Row, Select, Button } from 'antd';
 import { preparePlaceholderText, preparePlaceholderSelect } from 'utils/preparePlaceholder';
 
@@ -12,6 +12,34 @@ import { validateRequiredInputField, validateNumberWithTwoDecimalPlaces, validat
 
 const AggregatesFormMain = (props) => {
     const { typeData, handleCancel, handleFormValueChange, optionsServicesMapping, setoptionsServicesMapping, optionsServiceModified, setoptionsServiceModified, showGlobalNotification, formData, optionForm } = props;
+    const [MakeOptions, setMakeOptions] = useState([
+        {
+            value: '1',
+            label: 'Year',
+        },
+        {
+            value: '2',
+            label: 'Month',
+        },
+        {
+            value: '3',
+            label: 'WeekDays',
+        },
+    ]);
+    const [serviceNameOptions, setserviceNameOptions] = useState([
+        {
+            value: '1',
+            label: 'Not Identified',
+        },
+        {
+            value: '2',
+            label: 'Closed',
+        },
+        {
+            value: '3',
+            label: 'Identified',
+        },
+    ]);
     const isServiceNamePresent = (serviceName) => {
         let found = false;
         optionsServiceModified?.find((element, index) => {
@@ -40,7 +68,7 @@ const AggregatesFormMain = (props) => {
             })
             .catch((err) => {});
     };
-
+    const MakefieldNames = { label: '', value: '' };
     return (
         <>
             <Row gutter={20}>
@@ -49,48 +77,18 @@ const AggregatesFormMain = (props) => {
                         <Row gutter={20}>
                             <Col xs={24} sm={24} md={8} lg={8} xl={8} xxl={8}>
                                 <Form.Item name="serviceName" label="Item" rules={[validateRequiredSelectField('Item')]}>
-                                    <Select
-                                        options={[
-                                            {
-                                                value: '1',
-                                                label: 'Not Identified',
-                                            },
-                                            {
-                                                value: '2',
-                                                label: 'Closed',
-                                            },
-                                            {
-                                                value: '3',
-                                                label: 'Identified',
-                                            },
-                                        ]}
-                                    />
+                                    <Select placeholder={preparePlaceholderSelect('Srl no')} options={serviceNameOptions} />
                                     {/* placeholder="Select item" allowClear options={typeData['VEHCL_TYPE']} fieldNames={{ label: 'value', value: 'key' }}  */}
                                 </Form.Item>
                             </Col>
                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
                                 <Form.Item label="Make" name="make" rules={[validateRequiredInputField('Make')]}>
-                                    <Select
-                                        options={[
-                                            {
-                                                value: '1',
-                                                label: 'Year',
-                                            },
-                                            {
-                                                value: '2',
-                                                label: 'Month',
-                                            },
-                                            {
-                                                value: '3',
-                                                label: 'WeekDays',
-                                            },
-                                        ]}
-                                    />
+                                    <Select placeholder={preparePlaceholderSelect('Srl no')} fieldNames={MakefieldNames} options={MakeOptions} />
                                     {/* placeholder="Select make" allowClear options={typeData['VEHCL_TYPE']} fieldNames={{ label: 'value', value: 'key' }} */}
                                 </Form.Item>
                             </Col>
                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                <Form.Item label="Seriol No." name="amount" rules={[validateRequiredInputField('Srl no')]}>
+                                <Form.Item label="Serial No." name="amount" rules={[validateRequiredInputField('Srl no')]}>
                                     <Input maxLength={50} placeholder={preparePlaceholderText('Srl no')} />
                                 </Form.Item>
                             </Col>
