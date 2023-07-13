464989e104489b2cd5062ec9bc985f810b476b70
Kartik Gupta <KA20371579@wipro.com>
2023-07-13 15:22:30 +0530

    working on code coverage
    WIP

--- src/__tests__/components/common/qualificationmaster/addeditform.test.jsx
+++ src/__tests__/components/common/qualificationmaster/addeditform.test.jsx
@@ -1,7 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
-import { screen } from '@testing-library/react';
 import customRender from '@utils/test-utils';
-import { act } from 'react-dom/test-utils';
 
 import { AddEditForm } from '@components/common/QualificationMaster/AddEditForm';
 
