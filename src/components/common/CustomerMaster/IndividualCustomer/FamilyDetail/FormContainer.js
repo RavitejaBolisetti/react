43da8c753388404ed77624e29bd957b8426ba13a
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-07-19 18:52:33 +0530

    Update class name

--- src/components/common/CustomerMaster/IndividualCustomer/FamilyDetail/FormContainer.js
+++ src/components/common/CustomerMaster/IndividualCustomer/FamilyDetail/FormContainer.js
@@ -82,13 +82,13 @@ const FormBase = (props) => {
                     </Form.Item>
                 </Col>
                 {customer ? (
-                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8} className={styles.searchRedButt}>
+                    <Col xs={8} sm={8} md={8} lg={8} xl={8} xxl={8} className={styles.noLeftPadding}>
                         <Form.Item initialValue={props?.relationCustomerId ? props?.relationCustomerId : ''} label="Customer Id" name="relationCustomerId">
                             <Search placeholder={preparePlaceholderText('Customer Id')} allowClear loading={isSearchLoading} onSearch={onSearch} style={{ width: '100%' }} />
                         </Form.Item>
                     </Col>
                 ) : (
-                    <Col xs={0} sm={0} md={0} lg={0} xl={0} xxl={0} className={styles.searchRedButt}>
+                    <Col xs={0} sm={0} md={0} lg={0} xl={0} xxl={0} className={styles.noLeftPadding}>
                         <Form.Item initialValue={props?.relationCustomerId ? props?.relationCustomerId : ''} label="Customer Id" name="relationCustomerId">
                             <Search placeholder={preparePlaceholderText('Customer Id')} allowClear loading={isSearchLoading} onSearch={onSearch} style={{ width: '100%' }} />
                         </Form.Item>
