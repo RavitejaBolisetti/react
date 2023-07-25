54856c32b6ea4f18a7202611ffd06d33b4f5cb99
Sushil Kumar <sushil.kumar74@wipro.com>
2023-07-25 15:54:59 +0530

    page header coverage improve 55 to 84%

--- src/__tests__/pages/common/pageheader/pageheader.test.jsx
+++ src/__tests__/pages/common/pageheader/pageheader.test.jsx
@@ -1,69 +1,42 @@
 import '@testing-library/jest-dom/extend-expect';
 import customRender from '@utils/test-utils';
 import PageHeader from '@pages/common/PageHeader/PageHeader';
-import { screen} from "@testing-library/react";
-describe('PageHeader Component', () => {
-    it('should render PageHeader component', async () => {
-        const { container } = customRender(<PageHeader />);
-        expect(container).toMatchSnapshot();
-    });
-});
-
-
-// Mock the Redux store state
-const mockState = {
- auth: { userId: 'user123' },
- data: {
-   Menu: {
-     isLoaded: true,
-     filter: null,
-     data: [],
-     favouriteMenu: [],
-     flatternData: [],
-   },
- },
- common: {
-   LeftSideBar: { collapsed: false },
- },
-};
+import { screen, fireEvent} from "@testing-library/react";
 
 const mockFetchList = jest.fn();
 const mockMarkFavourite = jest.fn();
 const mockListShowLoading = jest.fn();
 const mockShowGlobalNotification = jest.fn();
 
-jest.mock('store/actions/data/menu', () => ({
- menuDataActions: {
-   fetchList: () => mockFetchList,
-   markFavourite: () => mockMarkFavourite,
-   listShowLoading: () => mockListShowLoading,
- },
-}));
+describe('PageHeader Component', () => {
+  it('should render PageHeader component', async () => {
+      const { container } = customRender(<PageHeader isFavourite={false} canMarkFavourite={true} mockFetchList={mockFetchList} mockMarkFavourite={mockMarkFavourite} mockShowGlobalNotification={mockShowGlobalNotification} mockListShowLoading={mockListShowLoading} />);
+      expect(container).toMatchSnapshot();
+      screen.debug();
+  });
 
-jest.mock('store/actions/notification', () => ({
- showGlobalNotification: () => mockShowGlobalNotification,
-}));
-
-describe('PageHeader', () => {
- beforeEach(() => {
-   // Reset mock function calls before each test
-   jest.clearAllMocks();
- });
-
- test('renders page title correctly', () => {
+  it('renders page title correctly', () => {
     customRender(<PageHeader pageTitle="Test Page" />);
+    const pageTitleElement = screen.getByText('Test Page');
+    expect(pageTitleElement).toBeInTheDocument();
+  });
 
-   const pageTitleElement = screen.getByText('Test Page');
-   expect(pageTitleElement).toBeInTheDocument();
+ it('displays "Mark as favorite" icon when canMarkFavourite is true', () => {
+    customRender(<PageHeader pageTitle="Mark as favourite" canMarkFavourite={true} onError={jest.fn()} handleFavouriteClick={jest.fn()} isFavourite={false}/>);
+    const markAsFavoriteIcon = screen.getByTestId('addfav');
+    expect(markAsFavoriteIcon).toBeInTheDocument();
  });
 
-//  test('displays "Mark as favorite" icon when canMarkFavourite is true', () => {
-//     customRender(
+ it('displays "Mark as favorite" icon when canMarkFavourite is true', () => {
+    customRender(<PageHeader pageTitle="Mark as favourite" canMarkFavourite={true} onError={jest.fn()} handleFavouriteClick={jest.fn()} isFavourite={false}/>);
+    const markAsFavoriteIcon = screen.getByTestId('addfav');
+    fireEvent.click(markAsFavoriteIcon);
+  });
 
-// <PageHeader canMarkFavourite="Mark as favourite" canMarkFavourite={true} onError={jest.fn()} handleFavouriteClick={jest.fn()} isFavourite={false}/>
-//    );
-
-//    const markAsFavoriteIcon = screen.getByLabelText('Mark as favourite');
-//    expect(markAsFavoriteIcon).toBeInTheDocument();
-//  });
+  it('calls handleFavouriteClick when "Mark as favorite" icon is clicked', () => {
+    customRender(<PageHeader pageTitle="Test Page" canMarkFavourite={true}  onError={jest.fn()} handleFavouriteClick={jest.fn()} isFavourite={true} />);
+    const markAsFavoriteIcon = screen.getByTestId('addfav');
+    fireEvent.click(markAsFavoriteIcon);
+    expect(mockMarkFavourite).toHaveBeenCalledTimes(1);
+  });
 });
