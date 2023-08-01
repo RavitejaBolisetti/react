5405346ade5b58ea066df0a7ea9c8f5c8a13c82b
Simran <Simran.sehgal@wipro.com>
2023-08-01 16:26:52 +0530

    changes

--- src/components/common/CustomerMaster/ChangeHistory/customerfChangeHistory.js
+++ src/components/common/CustomerMaster/ChangeHistory/customerfChangeHistory.js
@@ -3,7 +3,7 @@
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { convertDateTime } from 'utils/formatDateTime';
@@ -23,14 +23,14 @@ const mapStateToProps = (state) => {
     const {
         auth: { userId },
         customer: {
-            customerDetail: { detailData: data = [], isChangeHistoryLoaded, isChangeHistoryLoading, changeHistoryData = [] },
+            customerDetail: { detailData: data = [], isChangeHistoryLoaded, isChangeHistoryLoading, filter: changeHistoryData = [] },
         },
-
     } = state;
 
     let returnValue = {
         userId,
         data,
+        totalRecords: changeHistoryData?.totalRecords || [],
         isChangeHistoryLoaded,
         isChangeHistoryLoading,
         changeHistoryData,
@@ -49,18 +49,56 @@ const mapDispatchToProps = (dispatch) => ({
     ),
 });
 
-const ChangeHistoryMain = ({ fetchCustomerChangeHistory, onCloseAction, listShowChangeHistoryLoading, isChangeHistoryLoading, userId, isChangeHistoryLoaded, changeHistoryData, selectedCustomerId }) => {
+const ChangeHistoryMain = ({ fetchCustomerChangeHistory, onCloseAction, listShowChangeHistoryLoading, totalRecords, isChangeHistoryLoading, userId, isChangeHistoryLoaded, changeHistoryData, selectedCustomerId }) => {
+    const [page, setPage] = useState({ pageSize: 10, current: 1 });
+    const dynamicPagination = true;
+
+    const defaultExtraParam = useMemo(() => {
+        return [
+            {
+                key: 'pageSize',
+                title: 'Value',
+                value: page?.pageSize,
+                canRemove: true,
+                filter: false,
+            },
+            {
+                key: 'pageNumber',
+                title: 'Value',
+                value: page?.current,
+                canRemove: true,
+                filter: false,
+            },
+            {
+                key: 'sortBy',
+                title: 'Sort By',
+                value: page?.sortBy,
+                canRemove: true,
+                filter: false,
+            },
+            {
+                key: 'sortIn',
+                title: 'Sort Type',
+                value: page?.sortType,
+                canRemove: true,
+                filter: false,
+            },
+        ];
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page]);
+
     useEffect(() => {
         if (selectedCustomerId) {
             const extraParams = [
+                ...defaultExtraParam,
                 {
-                    key: 'customerCode',
-                    title: 'customerCode',
+                    key: 'customerId',
+                    title: 'customerId',
                     value: selectedCustomerId,
-                    name: 'Customer Code',
+                    name: 'Customer Id',
                 },
             ];
-            fetchCustomerChangeHistory({ customURL, setIsLoading: listShowChangeHistoryLoading, userId, extraParams });
+            fetchCustomerChangeHistory({ customURL, setIsLoading: listShowChangeHistoryLoading, userId, extraParams: extraParams || defaultExtraParam });
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [selectedCustomerId]);
@@ -100,9 +138,12 @@ const ChangeHistoryMain = ({ fetchCustomerChangeHistory, onCloseAction, listShow
     ];
 
     const tableProps = {
+        setPage,
+        totalRecords,
+        dynamicPagination,
         isChangeHistoryLoading,
         tableColumn,
-        // tableData: changeHistoryData?.otfChangeHistoryListResponse || [],
+        tableData: changeHistoryData?.dataList,
     };
 
     return (
