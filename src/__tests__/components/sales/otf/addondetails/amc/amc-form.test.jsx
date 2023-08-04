3fed77c34659625d00e4205072eb1fd3ce5e78f8
jatin kumar <jatin.kumar13@wipro.com>
2023-08-04 17:52:16 +0530

    add-on-details unit-tests done

---
+++ src/__tests__/components/sales/otf/addondetails/amc/amc-form.test.jsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom/extend-expect';
+import customRender from '@utils/test-utils';
+import { screen, fireEvent, logRoles } from '@testing-library/react';
+import AMCForm from 'components/Sales/OTF/AddOnDetails/AMC/AMCForm';
+import { Form } from 'antd';
+
+const FormWrapper = (props) => {
+    const [amcForm] = Form.useForm();
+    return <AMCForm amcForm={amcForm} {...props} />
+}
+
+describe('AMC Form Component', () => {
+    it('should render amc form component', () => {
+        customRender(<AMCForm />);
+    });
+
+    it('amc form input should work', () => {
+        customRender(<FormWrapper setformDataSetter={jest.fn}/>);
+        const amcInput=screen.getByRole('textbox', { name: 'AMC'});
+        fireEvent.change(amcInput, { target: { value: 'Test' }})
+    });
+});
---
+++ src/__tests__/components/sales/otf/otfformbutton/otf-form-button.test.jsx
@@ -0,0 +1,54 @@
+import '@testing-library/jest-dom/extend-expect';
+import customRender from '@utils/test-utils';
+import { screen, fireEvent, logRoles, act } from '@testing-library/react';
+import { OTFFormButton } from 'components/Sales/OTF/OTFFormButton';
+
+const buttonData={
+    closeBtn: true,
+    cancelBtn: true,
+    editBtn: true,
+    allotBtn: true,
+    unAllotBtn: true,
+    invoiceBtn: true,
+    deliveryNoteBtn: true,
+    transferOTFBtn: true,
+    cancelOTFBtn: true,
+    changeHistory: true,
+    nextBtn: true,
+    saveBtn: true,
+    formBtnActive:true
+}
+
+describe('OTF Form Button Component', () => {
+    it('should render otf form button component', () => {
+        customRender(<OTFFormButton/>);
+    });
+
+    it('test 1', async () => {
+        customRender(<OTFFormButton setButtonData={jest.fn()} buttonData={buttonData} handleButtonClick={jest.fn()} />);
+        const closeBtn=screen.getByRole('button', {name:'Close'});
+        fireEvent.click(closeBtn);
+        const cancelBtn=screen.getByRole('button', {name:'Cancel'});
+        fireEvent.click(cancelBtn);
+        const editBtn=screen.getByRole('button', {name:'Edit'});
+        fireEvent.click(editBtn);
+        const allotBtn=screen.getByRole('button', {name:'Allot'});
+        fireEvent.click(allotBtn);
+        const unAllotBtn=screen.getByRole('button', {name:'Un-Allot'});
+        fireEvent.click(unAllotBtn);
+        const invoiceBtn=screen.getByRole('button', {name:'Invoice'});
+        fireEvent.click(invoiceBtn);
+        const deliveryNoteBtn=screen.getByRole('button', {name:'Delivery Note'});
+        fireEvent.click(deliveryNoteBtn);
+        const transferOTFBtn=screen.getByRole('button', {name:'Transfer OTF'});
+        fireEvent.click(transferOTFBtn);
+        const cancelOTFBtn=screen.getByRole('button', {name:'Cancel OTF'});
+        fireEvent.click(cancelOTFBtn);
+        const changeHistory=screen.getByRole('button', {name:'Change History'});
+        fireEvent.click(changeHistory);
+        const nextBtn=screen.getByRole('button', {name:'Next'});
+        fireEvent.click(nextBtn);
+        const saveBtn=screen.getByRole('button', {name:'Save & Next'});
+        fireEvent.click(saveBtn);
+    });
+});
