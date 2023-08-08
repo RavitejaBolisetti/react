6035ebba0a65f9c1a8b53f3c50af428712e6a993
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-08-08 12:13:58 +0530

    routing changes
    wip #2190

--- src/components/Sales/VehicleRecieptChecklist/QueryButtons/QueryButtonConstants.js
+++ src/components/Sales/VehicleRecieptChecklist/QueryButtons/QueryButtonConstants.js
@@ -13,7 +13,7 @@ export const QUERY_BUTTONS = {
     PARTIALLY_COMPLETED: {
         id: 'partially',
         title: 'Partially Completed',
-        key: 'partially',
+        key: 'partiallyCompleted',
         active: false,
     },
     COMPLETED: {
