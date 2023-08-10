a7ebda6960c0f03533308208c2f64715f2b3539a
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-08-10 16:39:03 +0530

    Fixed issue

--- src/utils/dataTable/DataTable.js
+++ src/utils/dataTable/DataTable.js
@@ -9,10 +9,12 @@
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
 import { useState, useEffect } from 'react';
-import { Table } from 'antd';
+import { Table, Row, Col, Select, Pagination } from 'antd';
 import { InputSkeleton } from 'components/common/Skeleton';
 import { tblSerialNumberColumn } from 'utils/tableColumn';
 import styles from 'components/common/Common.module.css';
+
+const { Option } = Select;
 
 export default function DataTable({ isLoading, rowSelection = undefined, showSizeChanger = true, dynamicPagination = false, totalRecords = '10', pagination = true, removePagination = false, srl = true, srlTitle = '#', tableColumn, scroll = 'auto', tableData, rowKey = 'index', setPage = () => {} }) {
     useEffect(() => {
@@ -33,9 +35,9 @@ export default function DataTable({ isLoading, rowSelection = undefined, showSiz
         pageSize: 10,
         current: 1,
         position: ['bottomRight'],
-        showSizeChanger: showSizeChanger,
+        showSizeChanger: false,
         hideOnSinglePage: false,
-        showTotal,
+        showTotal: false,
     });
 
     const handleTableChange = (pagination, filters, sorter) => {
@@ -46,7 +48,19 @@ export default function DataTable({ isLoading, rowSelection = undefined, showSiz
         } else {
             setPage({ ...pagination });
         }
-        setPagination({ ...pagination, showTotal });
+        // setPagination({ ...pagination, showTotal });
+    };
+
+    const handlePageChange = (page, pageSize) => {
+        // if (dynamicPagination) {
+        //     const sortBy = sorter?.column?.dataIndex || undefined;
+        //     const sortType = sorter && sorter.order ? (sorter.order === 'descend' ? 'DESC' : 'ASC') : undefined;
+        //     setPage({ ...pagination, sortBy, sortType });
+        // } else {
+        //     setPage({ ...pagination });
+        // }
+        setPagination({ ...pagination, current: page, pageSize });
+        setPage({ ...pagination, current: page, pageSize });
     };
 
     const skeletonData = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
@@ -57,9 +71,35 @@ export default function DataTable({ isLoading, rowSelection = undefined, showSiz
         return { ...item, render: () => <InputSkeleton height={40} /> };
     });
 
+    const options = [];
+    for (let i = 1; i < 5; i++) {
+        options.push({
+            value: i * 10,
+            label: `${i * 10 + '/Page'}`,
+        });
+    }
+
+    const handleChange = (value) => {
+        console.log(`Selected: ${value}`);
+    };
+
     return (
         <div className={styles.marB20}>
-            <Table rowSelection={rowSelection} columns={isLoading ? tableSkeletonColumn : tableColumnWithSrl} dataSource={isLoading ? skeletonData : tableData} onChange={handleTableChange} pagination={pagination ? !isLoading && tablePagination : false} rowKey={rowKey} scroll={scroll} />
+            {/* <Table rowSelection={rowSelection} columns={isLoading ? tableSkeletonColumn : tableColumnWithSrl} dataSource={isLoading ? skeletonData : tableData} onChange={handleTableChange} pagination={pagination ? !isLoading && tablePagination : false} rowKey={rowKey} scroll={scroll} /> */}
+            <Table rowSelection={rowSelection} columns={isLoading ? tableSkeletonColumn : tableColumnWithSrl} dataSource={isLoading ? skeletonData : tableData} onChange={handleTableChange} pagination={false} rowKey={rowKey} scroll={scroll} />
+            <Row gutter={20}>
+                <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+                    {totalRecords && (
+                        <>
+                            Total <span style={{ color: '#0b0b0c' }}> {totalRecords} </span> items
+                        </>
+                    )}
+                    <Select defaultValue="10/Page" onChange={handleChange} style={{ width: 200 }} options={options} />
+                </Col>
+                <Col xs={24} sm={12} md={12} lg={12} xl={12} className={styles.alignRight}>
+                    <Pagination {...tablePagination} onChange={handlePageChange} />
+                </Col>
+            </Row>
         </div>
     );
 }
