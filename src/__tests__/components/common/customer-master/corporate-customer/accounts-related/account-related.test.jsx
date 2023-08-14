f5155aa109a701cbf49c719850b5779447dbe242
Kartik Gupta <KA20371579@wipro.com>
2023-08-14 12:23:28 +0530

    did coverage in account related in customer master

--- src/__tests__/components/common/customer-master/corporate-customer/accounts-related/account-related.test.jsx
+++ src/__tests__/components/common/customer-master/corporate-customer/accounts-related/account-related.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import customRender from '@utils/test-utils';
 import { AccountRelatedMaster } from '@components/common/CustomerMaster/CorporateCustomer/AccountRelated/AccountRelatedMaster';
+import createMockStore from '__mocks__/store';
+import { Provider } from 'react-redux';
+import { Form } from 'antd';
 
 const props = {
-    formActionType: { viewMode: true },
-    selectedCustomerId: '',
+    formActionType: { addMode: false, editMode: true, viewMode: false },
+    selectedCustomerId: 'CUS1687508619061',
     handleButtonClick: jest.fn(),
     NEXT_ACTION: jest.fn(),
     onCloseAction: jest.fn(),
@@ -26,14 +29,35 @@ const props = {
 
     record: { chassisNumber: null, customerId: 'CUS1687368732514', customerName: 'Company1', customerType: 'CRP', customerTypeName: 'CORPORATE', dateOfBirth: null, emailId: 'abhishek@gm.co', membershipType: 'SL', membershipTypeName: 'Silver', mobileNumber: '9778675564', profilePicDocId: null, registrationNumber: null },
 };
-const buttonData = { cancelBtn: true, closeBtn: true, editBtn: true, formBtnActive: false, nextBtn: true, saveAndNewBtn: false, saveAndNewBtnClicked: false, saveBtn: false };
+
+const FormWrapper = (props) => {
+    const [form] = Form.useForm();
+    return <AccountRelatedMaster form={form} {...props} />;
+};
+
 describe('AccountRelated Master  Component', () => {
+    const defaultBtnVisiblity = {
+        editBtn: true,
+        saveBtn: true,
+        cancelBtn: true,
+        saveAndNewBtn: true,
+        saveAndNewBtnClicked: true,
+        closeBtn: true,
+        formBtnActive: true,
+        cancelOTFBtn: true,
+        transferOTFBtn: true,
+        allotBtn: true,
+        unAllotBtn: true,
+        invoiceBtn: true,
+        deliveryNote: true,
+        changeHistory: true,
+    };
     it('should render AccountRelated Master ', async () => {
-        customRender(<AccountRelatedMaster {...props} {...buttonData} />);
+        customRender(<FormWrapper {...props} />);
     });
 
     it('should render Label Text', async () => {
-        customRender(<AccountRelatedMaster {...props} {...buttonData} />);
+        customRender(<FormWrapper {...props} onError={jest.fn()} showGlobalNotification={jest.fn()} />);
 
         const renderText = screen.getByText('Account Related');
         expect(renderText).toBeTruthy();
@@ -47,13 +71,68 @@ describe('AccountRelated Master  Component', () => {
         const outstandingText = screen.getByText('Outstanding Amount');
         expect(outstandingText).toBeTruthy();
 
-        const partDiscount = screen.getByText('Parts Discount');
+        const partDiscount = screen.getByText('Parts Discount %');
         expect(partDiscount).toBeTruthy();
 
-        const labourDiscount = screen.getByText('Labour Discount');
+        const labourDiscount = screen.getByText('Labour Discount %');
         expect(labourDiscount).toBeTruthy();
 
         const remarks = screen.getByText('Remarks');
         expect(remarks).toBeTruthy();
+    });
+    it('cancel button should work', async () => {
+        const mockStore = createMockStore({
+            auth: { userId: 123 },
+        });
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper {...props} buttonData={defaultBtnVisiblity} showGlobalNotification={jest.fn()} onError={jest.fn()} onCloseAction={jest.fn()} resetData={jest.fn()} />
+            </Provider>
+        );
+
+        const cancelBtn = screen.getByRole('button', { name: /close/i });
+        fireEvent.click(cancelBtn);
+    });
+    it('save button should work with on finish', async () => {
+        const mockStore = createMockStore({
+            auth: { userId: 123 },
+        });
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper {...props} onSuccessAction={jest.fn()} showGlobalNotification={jest.fn()} onError={jest.fn()} buttonData={defaultBtnVisiblity} onCloseAction={jest.fn()} resetData={jest.fn()} onSuccess={jest.fn()} handleFormValueChange={jest.fn()} handleFieldsChange={jest.fn()} onFinish={jest.fn()} onFinishFailed={jest.fn()} />
+            </Provider>
+        );
+        const laborDisc = screen.getByRole('textbox', { name: /labour discount %/i });
+        fireEvent.change(laborDisc, { target: { value: '1221' } });
+
+        const saveBtn = screen.getByRole('button', { name: 'loading Save & Next', exact: false });
+        fireEvent.click(saveBtn);
+    });
+
+    it('should validate fields on finish failed', async () => {
+        const mockStore = createMockStore({
+            auth: { userId: 123 },
+        });
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper {...props} buttonData={defaultBtnVisiblity} showGlobalNotification={jest.fn()} onError={jest.fn()} onCloseAction={jest.fn()} resetData={jest.fn()} onSuccess={jest.fn()} handleFormValueChange={jest.fn()} handleFieldsChange={jest.fn()} onFinish={jest.fn()} onFinishFailed={jest.fn()} />
+            </Provider>
+        );
+        const addBtn = screen.getByRole('button', { name: 'Edit', exact: false });
+        fireEvent.click(addBtn);
+
+        const exchangeValue = screen.getByRole('textbox', { name: 'Labour Discount %', exact: false });
+        fireEvent.change(exchangeValue, { target: { value: '2020-05-24' } });
+        fireEvent.click(exchangeValue);
+
+        const saveBtn = screen.getByRole('button', { name: 'loading Save & Next', exact: false });
+        fireEvent.click(saveBtn);
+    });
+});
+
+describe('Component render when viewmode is true', () => {
+    const formActionType = { viewMode: true };
+    it('should render addedit page', async () => {
+        customRender(<FormWrapper {...formActionType} onError={jest.fn()} showGlobalNotification={jest.fn()} resetData={jest.fn()} />);
     });
 });
