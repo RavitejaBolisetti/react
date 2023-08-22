0045117c6439be8b6832321a592e71677c08fe45
Sushil Kumar <sushil.kumar74@wipro.com>
2023-08-22 13:48:35 +0530

    lint and pre commit test failed check

--- src/__tests__/pages/common/role-management/role-management-page.test.jsx
+++ src/__tests__/pages/common/role-management/role-management-page.test.jsx
@@ -2,8 +2,8 @@ import '@testing-library/jest-dom/extend-expect';
 import customRender from "@utils/test-utils";
 import { RoleManagementPageBase } from '@pages/common/RoleManagement/RoleManagementPage';
 
-describe("RoleManagementPage Components", () => {
-    it("should render RoleManagementPage components", ()=> {
+describe("RoleManagementPageBase Components", () => {
+    it("should render RoleManagementPageBase components", ()=> {
         customRender(<RoleManagementPageBase />);
     });
 });
