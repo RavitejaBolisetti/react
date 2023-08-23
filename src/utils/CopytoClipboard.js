e58dcad644d105b230b5fcc3c2aa330a5d7b4447
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-08-23 13:10:10 +0000

    Squashed commit of the following
    commit ef29466e 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Wed Aug 23 2023 18:38:11 GMT+0530 (India Standard Time) 
    
        changes
    
    
    commit 61b36e0b 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Wed Aug 23 2023 17:34:37 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-2190-Vehicle-Receipt-Checklist-Refactor
    
    
    commit f0b20d49 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Wed Aug 23 2023 17:26:51 GMT+0530 (India Standard Time) 
    
        copyto clipboard
    
    wip #2190
    
    
    commit 37607592 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Wed Aug 23 2023 17:15:24 GMT+0530 (India Standard Time) 
    
        Refactoring vehicle receipt checklist
    
    wip #2190
    
    
    commit 365319bf 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Wed Aug 23 2023 16:10:52 GMT+0530 (India Standard Time) 
    
        copied message
    
    
    commit 8f56d1aa 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Wed Aug 23 2023 15:50:49 GMT+0530 (India Standard Time) 
    
        copytoClipboard functionality
    
    wip #2190
    
    
    commit 5f12b9bc 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Tue Aug 22 2023 19:26:33 GMT+0530 (India Standard Time) 
    
        changes and validation
    
    
    commit 5dbcb255 
    Author: Shakambhar Pujari <shakambhar.pujari.ext@wipro.com> 
    Date: Tue Aug 22 2023 19:01:44 GMT+0530 (India Standard Time) 
    
        changes and refactoring using switch
    
    wip #2190

---
+++ src/utils/CopytoClipboard.js
@@ -0,0 +1,33 @@
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { FiCopy } from 'react-icons/fi';
+import { Button } from 'antd';
+import { showGlobalNotification } from 'store/actions/notification';
+import styles from 'components/common/Common.module.css';
+
+const mapDispatchToProps = (dispatch) => ({
+    dispatch,
+    ...bindActionCreators(
+        {
+            showGlobalNotification,
+        },
+        dispatch
+    ),
+});
+
+const CopytoClipboardMaster = (props) => {
+    const { text, showGlobalNotification } = props;
+    const handleClick = () => {
+        text && navigator.clipboard.writeText(text);
+        showGlobalNotification({ notificationType: 'success', title: 'Copied', message: '' });
+    };
+    if (!text) return '';
+    return <Button type="link" onClick={handleClick} icon={<FiCopy className={styles.activeForm} />} className={` ${styles.marL5}`} />;
+};
+export const CopytoClipboard = connect(null, mapDispatchToProps)(CopytoClipboardMaster);
