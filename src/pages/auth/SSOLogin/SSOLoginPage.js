74e48b98a6c38d6e2b2cc9989fc505ca54d05b27
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-08-23 18:29:03 +0530

    Fix SSO login issue

--- src/pages/auth/SSOLogin/SSOLoginPage.js
+++ src/pages/auth/SSOLogin/SSOLoginPage.js
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2023 Mahindra & Mahindra Ltd. 
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
@@ -35,12 +35,17 @@ export const SSOLoginPage = () => {
     }
 
     if (canLogin && idToken && accessToken) {
+        const decodeToken = jwtDecode(idToken);
+        const userId = decodeToken?.['cognito:username'];
+
         localStorage.setItem(LOCAL_STORAGE_KEY_AUTH_ID_TOKEN, idToken);
         localStorage.setItem(LOCAL_STORAGE_KEY_AUTH_ACCESS_TOKEN, accessToken);
-        localStorage.setItem(LOCAL_STORAGE_KEY_AUTH_USER_ID, '11112');
-        const decodeToken = jwtDecode(idToken);
-        if (decodeToken && decodeToken['cognito:username']) {
+        localStorage.setItem(LOCAL_STORAGE_KEY_AUTH_USER_ID, userId);
+
+        if (decodeToken && userId) {
             window.location.href = ROUTING_DASHBOARD;
+        } else {
+            window.location.href = ROUTING_LOGIN;
         }
     } else {
         window.location.href = ROUTING_LOGIN;
