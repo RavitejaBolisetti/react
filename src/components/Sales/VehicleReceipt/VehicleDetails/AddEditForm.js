88ae85a9a8ef2da9ffb206590eed3b94858e8240
Sujeet Kumar <sujeet.singh10@wipro.com>
2023-08-24 20:43:45 +0530

    US-2203-Vehicle-Receipt-Change

--- src/components/Sales/VehicleReceipt/VehicleDetails/AddEditForm.js
+++ src/components/Sales/VehicleReceipt/VehicleDetails/AddEditForm.js
@@ -13,6 +13,7 @@ import { getCodeValue } from 'utils/getCodeValue';
 import { formattedCalendarDate, dateFormat } from 'utils/formatDateTime';
 import { validateRequiredSelectField } from 'utils/validation';
 import { preparePlaceholderText, preparePlaceholderSelect } from 'utils/preparePlaceholder';
+import { VEHICLE_RECEIPT_STATUS } from 'constants/VehicleReceiptStatus';
 
 import styles from 'components/common/Common.module.css';
 
@@ -181,7 +182,7 @@ const AddEditFormMain = (props) => {
                                         <Form.Item initialValue={item?.vehicleStatus} label="Vehicle Status" name={[index, 'vehicleStatus']} rules={[validateRequiredSelectField('Vehicle Status')]}>
                                             <Select maxLength={50} placeholder={preparePlaceholderSelect('Select')} {...selectProps}>
                                                 {vehicleStatusType
-                                                    ?.filter((i) => i?.key !== 'TRN')
+                                                    ?.filter((i) => i?.key !== VEHICLE_RECEIPT_STATUS.IN_TRANSIT.key)
                                                     ?.map((item) => (
                                                         <Option key={'vs' + item.key} value={item.key}>
                                                             {item.value}
