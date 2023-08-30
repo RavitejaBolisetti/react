8307004cab615d1de53c652e0ed02295013832c6
vivek <vivek.kumar176@wipro.com>
2023-08-30 15:14:03 +0530

    vehicle-customer-master

--- src/__tests__/components/sales/vehicle-detail/customer-details/customer-details-master.test.jsx
+++ src/__tests__/components/sales/vehicle-detail/customer-details/customer-details-master.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, fireEvent, act } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import { CustomerDetailsMaster } from '@components/Sales/VehicleDetail/CustomerDetails/CustomerDetailsMaster';
 import customRender from '@utils/test-utils';
 import { Form } from 'antd';
@@ -10,11 +10,16 @@ const FormWrapper = (props) => {
         ...form,
         setFieldsValue: jest.fn(),
         resetFields: jest.fn(),
-        resetData: jest.fn()
+        resetData: jest.fn(),
+        getFieldsValue: jest.fn()
     }
     return <CustomerDetailsMaster form={myFormMock}  {...props} />;
 };
 
+
+function onChange(newActivekeys, item) {
+    newActivekeys.push(item);
+}
 
 describe('Customer Details Master', () => {
     it('should render customer details master components', () => {
@@ -29,7 +34,8 @@ describe('Customer Details Master', () => {
         const props = {
             selectedRecordId: 'testId',
             data: data,
-            filterString: jest.fn()
+            filterString: jest.fn(),
+            handleFormValueChange: jest.fn(),
         }
 
         customRender(<FormWrapper {...props} formActionType={formActionType} onChange={jest.fn()} newActivekeys={[{ id: 1, value: 'test' }]} />)
@@ -68,14 +74,12 @@ describe('Customer Details Master', () => {
         const props = {
             selectedRecordId: 'testId',
             data: data,
-            titleOverride: "test",
-            closable: false,
-            onCloseAction: jest.fn()
         }
 
         customRender(<FormWrapper {...props}
             formActionType={formActionType}
             isVisible={true}
+            handleFormValueChange={jest.fn()}
         />);
 
         const button = screen.getAllByRole('button');
@@ -101,42 +105,117 @@ describe('Customer Details Master', () => {
 
         const owerBtn = screen.getByRole('button', { name: 'Edit Owner Details' });
         fireEvent.click(owerBtn);
+    })
+
+    it('should render vehicle detals master addEdit form user type button', () => {
+        const formActionType = { viewMode: false }
+        const mockStore = {
+            data: {
+                CustomerDetails: { isLoaded: false, isLoading: false, data: { key: 1, value: 'test', id: 2 } },
+                Vehicle: {
+                    ViewVehicleDetail: { isLoaded: true, isLoading: true, data: { key: 1, value: 'test', id: 2 } },
+                },
+                OTF: {
+                    Referrals: { isLoaded: false, isReferralLoading: false, data: { key: 1, value: 'test', id: 2 } }
+                },
+            },
+        }
+
+        const formData = {
+            key: 12,
+            value: "test"
+        }
+
+        const defaultBtnVisiblity = {
+            editBtn: true,
+            saveBtn: true,
+            cancelBtn: true,
+            saveAndNewBtn: true,
+            saveAndNewBtnClicked: true,
+            closeBtn: true,
+            formBtnActive: true,
+        };
+
+        const data = { key: 1, value: 'test', id: 2 }
+
+        customRender(<FormWrapper
+            formActionType={formActionType}
+            selectedRecordId="test"
+            fetchList={jest.fn()}
+            fetchCustomerDetailData={jest.fn()}
+            saveButtonName={'Save & Next'}
+            onSuccessAction={jest.fn()}
+            showGlobalNotification={jest.fn()}
+            onError={jest.fn()}
+            buttonData={defaultBtnVisiblity}
+            onCloseAction={jest.fn()}
+            onSuccess={jest.fn()}
+            handleFormValueChange={jest.fn()}
+            handleFieldsChange={jest.fn()}
+            onFinish={jest.fn()}
+            onFinishFailed={jest.fn()}
+            formData={formData}
+            setButtonData={jest.fn()}
+            onErrorAction={jest.fn()}
+            fnSetData={jest.fn()}
+            data={data}
+            handleButtonClick={jest.fn()}
+        />, {
+            initialState: mockStore,
+        })
+
+        const saveBtn = screen.getByRole('button', { name: 'Save & Next' });
+        fireEvent.click(saveBtn);
+
+        const closeBtn = screen.getByRole('button', { name: 'Close' });
+        fireEvent.click(closeBtn);
+
+        const cancelBtn = screen.getByRole('button', { name: 'Cancel' });
+        fireEvent.click(cancelBtn);
+
+        const editBtn = screen.getByRole('button', { name: 'Edit' });
+        fireEvent.click(editBtn);
+    })
 
 
+    it('should render customer details master show', () => {
+
+        const props = {
+            selectedRecordId: 'test',
+            data: { id: 1, value: 'test', key: 1 },
+            isVisible: true
+        }
+        customRender(
+            <FormWrapper
+                {...props}
+                formType={"billingCustomers"}
+                onCloseAction={jest.fn()}
+                onSuccess={jest.fn()}
+                handleFormValueChange={jest.fn()}
+                handleFieldsChange={jest.fn()}
+                onFinish={jest.fn()}
+                onFinishFailed={jest.fn()}
+                setButtonData={jest.fn()}
+                sameAsBookingCustomer={jest.fn()}
+                canUpdate={true}
+                onSearch={jest.fn()}
+                setFormData={jest.fn()}
+            />)
+
+        const customerBtn = screen.getByRole('button', { name: 'Edit Billing Customer Details' });
+        fireEvent.click(customerBtn);
 
 
-        screen.debug()
-        screen.getByRole('data-test')
+        const sameAsOwer = screen.getByRole('checkbox', { name: 'Same As Owner' });
+        fireEvent.click(sameAsOwer);
 
+        const searchDrop = screen.getByRole('combobox', { name: "" });
+        fireEvent.change(searchDrop);
 
-        // const plusMinus = screen.getAllByRole('button');
-        // act(async () => {
-        //     fireEvent.click(plusMinus);
-        //     // expect(screen.getByText('Edit')).toBeInTheDocument()
-        // });
+        const search = screen.getByRole('img', { name: 'search' });
+        fireEvent.click(search);
 
-        // const button = screen.getAllByRole('button', { name: 'Owner Details' });
-        // fireEvent.click(button[0]);
-        // const owberCustomer = screen.getByText('Owner Customer');
-        // fireEvent.click(owberCustomer);
-
-        // fireEvent.click(button[1]);
-        // const billingDetails = screen.getByText('Billing Customer Details');
-        // fireEvent.click(billingDetails);
-
-        // fireEvent.click(button[2]);
-        // const keyAccount = screen.getByText('Key Account Details');
-        // fireEvent.click(keyAccount);
-
-        // fireEvent.click(button[3]);
-        // const loyalty = screen.getByText('Loyalty Details');
-        // fireEvent.click(loyalty);
-
-        // fireEvent.click(button[4]);
-        // const changeRequest = screen.getByText('Ownership Change Request');
-        // fireEvent.click(changeRequest);
-
-
-
+        const searchBtn = screen.getByRole('button', { name: 'search' });
+        fireEvent.click(searchBtn);
     })
 })
