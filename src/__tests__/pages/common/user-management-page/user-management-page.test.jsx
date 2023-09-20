3a282eb8996e254d21d6329cb8e463e73d0d10ee
jatin kumar <jatin.kumar13@wipro.com>
2023-09-20 09:49:46 +0530

    updated user-management-page

--- src/__tests__/pages/common/user-management-page/user-management-page.test.jsx
+++ src/__tests__/pages/common/user-management-page/user-management-page.test.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import customRender from '@utils/test-utils';
-import { screen, fireEvent } from "@testing-library/react";
 import { UserManagementPage } from 'pages/common';
 
 afterEach(() => {
     jest.restoreAllMocks();
 });
 
-describe('Left Side Bar Component', () => {
+describe('User Management Page Component', () => {
 
-    it('should render left sidebar component UI', () => {
+    it('should render user management page component', () => {
         customRender(<UserManagementPage />);
     });
 
