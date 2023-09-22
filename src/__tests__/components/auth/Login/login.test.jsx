71a64d54cabe1f428183e919dfd6d8fe89b7900a
jatin kumar <jatin.kumar13@wipro.com>
2023-09-22 10:00:41 +0530

    auth/login and pages/auth/login unit test done

--- src/__tests__/components/auth/Login/login.test.jsx
+++ src/__tests__/components/auth/Login/login.test.jsx
@@ -1,8 +1,9 @@
 import { screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import customRender from '@utils/test-utils';
-import { Logins } from '@components/Auth/Login/Login';
+import { Logins } from 'components/Auth/Login/Login';
 import ReactRecaptcha3 from 'react-google-recaptcha3';
+import { doLogin } from 'store/actions/auth';
 
 jest.mock('react-google-recaptcha3', () => {
     return {
@@ -11,6 +12,16 @@ jest.mock('react-google-recaptcha3', () => {
         destroy: jest.fn()
     }
 });
+
+jest.mock('store/actions/auth', () => ({
+    doLogin: jest.fn(),
+    doCloseLoginError: jest.fn().mockImplementation(() => {
+        return {type: 'YOUR_ACTION_TYPE'};
+    }),
+    authPreLogin: jest.fn().mockImplementation(() => {
+        return {type: 'YOUR_ACTION_TYPE'};
+    }),
+}));
 
 afterEach(() => {
     jest.restoreAllMocks();
@@ -24,9 +35,18 @@ describe('Login Form Component', () => {
 
     it('login form should work on correct details', async () => {
 
-        ReactRecaptcha3.getToken.mockImplementation(() => Promise.resolve('Kai'));
+        doLogin.mockImplementation((values, loginPageIsLoading, onSuccess, onError) => {
+            const data={ userRegisteredDevice: { match: false }, passwordStatus: true }
+            const mockResponse = { title: 'Mock Title', message: 'Mock Message' };
+            onSuccess(data);
+            onError(mockResponse);
+            return { type: 'MOCK_ACTION' };
+        });
 
-        customRender(<Logins doCloseLoginError={jest.fn()} onSuccess={jest.fn()} />);
+        customRender(<Logins doCloseLoginError={jest.fn()} authPostLogin={jest.fn()} authPreLogin={jest.fn()} /> );
+
+        const loginBtn=screen.getByRole('button', { name: 'Login' });
+        fireEvent.click(loginBtn);
 
         const userIdLabel=screen.getByText('User ID (MILE ID.Parent ID)');
         fireEvent.click(userIdLabel);
@@ -40,11 +60,12 @@ describe('Login Form Component', () => {
         const password=screen.getByTestId('inputPassword');
         fireEvent.change(password, { target: { value: 'Kai@1234' } });
 
-        const loginBtn=screen.getByRole('button', { name: 'Login' });
         fireEvent.click(loginBtn);
 
-        await waitFor(() => expect(ReactRecaptcha3.getToken).toHaveBeenCalled());
+        await waitFor(() => expect(doLogin).toHaveBeenCalled());
 
+        const continueBtn=screen.getByRole('button', { name: 'Continue' });
+        fireEvent.click(continueBtn);
     });
 
     it('login form should work on captcha failed', async () => {
