fa17a4c714da78eb340e70c9a360694f7cffb6b3
vivek <vivek.kumar176@wipro.com>
2023-09-23 17:09:17 +0530

    loyalty scheme master

--- src/__tests__/components/sales/otf/loyalty-scheme/loyalty-scheme-master.test.jsx
+++ src/__tests__/components/sales/otf/loyalty-scheme/loyalty-scheme-master.test.jsx
@@ -57,40 +57,130 @@ const buttonData = {
 describe('Loyalty scheme master render', () => {
     it('should render loyalty scheme add edit from details', () => {
 
+        const CustomerDetailData = [
+            {
+                id: 'test',
+                value: 'test1',
+            },
+            {
+                id: 'test1',
+                value: 'test1',
+            },
+        ];
+        const mockStore = createMockStore({
+            auth: { userId: '123456' },
+            data: {
+                OTF: {
+                    LoyaltyScheme: { isLoaded: true, data: CustomerDetailData },
+                },
+            },
+        });
+
+        const fetchList = jest.fn();
+
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper
+                    StatusBar={"test"}
+                    FormActionButton={FormActionButton}
+                    fetchList={fetchList}
+                    buttonData={buttonData}
+                    setButtonData={jest.fn()}
+                />
+            </Provider>
+        )
+
+        const combobox = screen.getByRole('combobox', { name: '', exact: false });
+        fireEvent.change(combobox, { target: { value: 'testing' } });
+
+        const make = screen.getByRole('combobox', { name: 'Make', exact: false });
+        fireEvent.change(make, { target: { value: 'make' } });
+
+        const modelG = screen.getByRole('combobox', { name: 'Model Group', exact: false });
+        fireEvent.change(modelG, { target: { value: 'test model' } });
+
+        const variant = screen.getByRole('combobox', { name: 'Variant', exact: false });
+        fireEvent.change(variant, { target: { value: 'variant' } });
+        
+        const usage = screen.getByRole('combobox', { name: 'Usage', exact: false });
+        fireEvent.change(usage, { target: { value: 'variant' } });
+
+        const yearOfReg = screen.getByRole('combobox', { name: 'Year of Registration', exact: false });
+        fireEvent.change(yearOfReg, { target: { value: 'variant' } });
+
+        const monthOfReg = screen.getByRole('combobox', { name: 'Month of Registration', exact: false });
+        fireEvent.change(monthOfReg, { target: { value: 'variant' } });
+
+        const relationship = screen.getByRole('combobox', { name: 'Relationship', exact: false });
+        fireEvent.change(relationship, { target: { value: '999999' } });
+
+        const schemeName = screen.getByRole('combobox', { name: 'Scheme Name', exact: false });
+        fireEvent.change(schemeName, { target: { value: 'variant' } });
+
+        const oldRegNumber = screen.getByRole('textbox', { name: 'Old Reg. Number', exact: false });
+        fireEvent.change(oldRegNumber, { target: { value: 'variant' } });
+
+        const oldChassisNumber = screen.getByRole('textbox', { name: 'Old Chassis Number', exact: false });
+        fireEvent.change(oldChassisNumber, { target: { value: 'variant' } });
+
+        const remark = screen.getByRole('textbox', { name: 'Remarks', exact: false });
+        fireEvent.change(remark, { target: { value: 'variant' } });
+
+        const customerName = screen.getByRole('textbox', { name: 'Customer Name', exact: false });
+        fireEvent.change(customerName, { target: { value: 'variant' } });
+
+        const schemeAmount = screen.getByRole('textbox', { name: 'Scheme Amount', exact: false });
+        fireEvent.change(schemeAmount, { target: { value: 'testName' } });
+
+        const submitBtn = screen.getByRole('button', { name: 'Save', exact: false })
+        fireEvent.click(submitBtn)
+
+    })
+
+    it('should render loyalty scheme search details', () => {
+
+        const CustomerDetailData = [
+            {
+                id: 'test',
+                value: 'test1',
+            },
+            {
+                id: 'test1',
+                value: 'test1',
+            },
+        ];
+        const mockStore = createMockStore({
+            auth: { userId: '123456' },
+            data: {
+                OTF: {
+                    LoyaltyScheme: { isLoaded: true, data: CustomerDetailData },
+                },
+            },
+        });
+
         const fetchList = jest.fn();
         const fetchSchemeLovList = jest.fn();
 
+        
+
         customRender(
-            <FormWrapper
-                StatusBar={"test"}
-                FormActionButton={OTFFormButton}
-                fetchList={fetchList}
-                fetchSchemeLovList={fetchSchemeLovList}
-                data={CustomerDetailData}
-                buttonData={buttonData}
-                selectedOrderId="test"
-                modelData={true}
-                variantData={true}
-            />
+            <Provider store={mockStore}>
+                <FormWrapper
+                    StatusBar={"test"}
+                    FormActionButton={FormActionButton}
+                    fetchList={fetchList}    
+                    buttonData={buttonData}
+                    setButtonData={jest.fn()}
+                />
+            </Provider>
         )
 
-        const insuranceCompany = screen.getByRole('combobox', { name: "Insurance Company" })
-        fireEvent.change(insuranceCompany)
+        const searchInput = screen.getByPlaceholderText('Search');
+        fireEvent.change(searchInput, { target: { value: 'testing' } });
 
-        const insuranceCoverNote = screen.getByRole('textbox', { name: "Insurance Cover Note" })
-        fireEvent.change(insuranceCoverNote)
+        const searchBtn = screen.getByRole('button', { name: 'search', exact: false })
+        fireEvent.click(searchBtn)
 
-        const insuranceAmount = screen.getByRole('textbox', { name: "Insurance Amount" })
-        fireEvent.change(insuranceAmount)
-
-        const insuranceCoverNoteDate = screen.getByRole('textbox', { name: "Insurance Cover Note Date" })
-        fireEvent.change(insuranceCoverNoteDate)
-
-        const registrationNumber = screen.getByRole('textbox', { name: "Registration Number" })
-        fireEvent.change(registrationNumber)
-
-        const calendarImg = screen.getByRole('img', { name: 'calendar' })
-        fireEvent.click(calendarImg)
     })
 })
 
