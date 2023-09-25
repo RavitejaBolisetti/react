9019c047d4426aea880781525aaf75a04743810d
Sujeet Kumar <sujeet.singh10@wipro.com>
2023-09-25 17:13:57 +0530

    #Add hover on reject and approve button
    US-6768-Invoice-Generation-Observation

--- src/components/Sales/VehicleInvoiceGeneration/VehicleInvoiceFormButton/VehicleInvoiceFormButton.js
+++ src/components/Sales/VehicleInvoiceGeneration/VehicleInvoiceFormButton/VehicleInvoiceFormButton.js
@@ -4,12 +4,13 @@
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
 import React from 'react';
-import { Button, Row, Col } from 'antd';
+import { Button, Row, Col, Popover } from 'antd';
 
 import { FROM_ACTION_TYPE } from 'constants/formActionType';
 import styles from 'assets/sass/app.module.scss';
 
 export const VehicleInvoiceFormButton = ({ formActionType, record, onCloseAction, onCancelInvoice, onApproveCancel, buttonData, setButtonData, saveButtonName = 'Save & Next', handleButtonClick, isLoadingOnSave, isLastSection, onPrintInvoice, onPrintForm21 }) => {
+    const content = <div>Coming Soon</div>;
     //console.log('formActionType', formActionType);
     return (
         <div className={styles.formFooter}>
@@ -47,15 +48,19 @@ export const VehicleInvoiceFormButton = ({ formActionType, record, onCloseAction
                     )}
 
                     {buttonData?.rejectCancelBtn && (
-                        <Button onClick={onApproveCancel} type="primary">
-                            Reject
-                        </Button>
+                        <Popover content={content} trigger="hover">
+                            <Button onClick={onApproveCancel} type="primary">
+                                Reject
+                            </Button>
+                        </Popover>
                     )}
 
                     {buttonData?.approveCancelBtn && (
-                        <Button onClick={onApproveCancel} type="primary">
-                            Approve
-                        </Button>
+                        <Popover content={content} trigger="hover">
+                            <Button onClick={onApproveCancel} type="primary">
+                                Approve
+                            </Button>
+                        </Popover>
                     )}
                     {/* {buttonData?.editBtn && (
             <Col xs={24} sm={16} md={18} lg={20} xl={20} className={styles.footerBtnRight}>
