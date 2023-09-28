0488d0e8d84411ba757878b61175e7983c5be821
kuldeep prajapati <ku1442186t@wipro.com>
2023-09-28 17:41:50 +0530

    worked on coverage improved

--- src/__tests__/components/sales/vehicle-purchase-order/vehicle-purchase-order-detail/add-edit-formz.test.jsx
+++ src/__tests__/components/sales/vehicle-purchase-order/vehicle-purchase-order-detail/add-edit-formz.test.jsx
@@ -17,11 +17,6 @@ const FormWrapper = (props) => {
     const [form] = Form.useForm();
     return <AddEditForm form={form} {...props} />;
 };
-
-const formActionType = {
-    editMode: true,
-};
-
 const buttonData = { closeBtn: true, cancelBtn: true, editBtn: true, allotBtn: true, unAllotBtn: true, invoiceBtn: true, deliveryNoteBtn: true, transferOTFBtn: true, cancelOTFBtn: true, changeHistory: true, nextBtn: true, saveBtn: true, formBtnActive: true, cancelVPOBtn: true, cancelOtfBtn: true, nextBtn: true, saveBtn: true };
 
 const saveButtonName = 'Save';
@@ -29,78 +24,61 @@ const isLoadingOnSave = false;
 
 describe('add edit form Components', () => {
     it('should render add edit form components', () => {
-        customRender(<FormWrapper onErrorAction={jest.fn()} handleButtonClick={jest.fn()} isVisible={true} isLoadingOnSave={false} saveButtonName={saveButtonName} formActionType={formActionType} typeData={['PO_TYPE']} />);
+        const formActionType = { editMode: true };
+        customRender(<FormWrapper onErrorAction={jest.fn()} setButtonData={jest.fn()} handleButtonClick={jest.fn()} isVisible={true} isLoadingOnSave={false} saveButtonName={saveButtonName} formActionType={formActionType} typeData={['PO_TYPE']} />);
     });
 
     it('vehicle purchase form input should work', () => {
+        const formActionType = { editMode: true };
         customRender(<FormWrapper onErrorAction={jest.fn()} handleButtonClick={jest.fn()} isVisible={true} setButtonData={jest.fn()} buttonData={buttonData} formActionType={formActionType} typeData={['PO_TYPE']} />);
 
         const purchaseOrderNumber = screen.getByRole('textbox', { name: 'Purchase Order Number' });
         fireEvent.change(purchaseOrderNumber, { target: { value: 'DmspurchaseOrder' } });
-        expect(purchaseOrderNumber).toBeTruthy();
-
         const PurchaseOrderDate = screen.getByRole('textbox', { name: 'Purchase Order Date' });
         fireEvent.change(PurchaseOrderDate, { target: { value: 'DmspurchaseDate' } });
-        expect(PurchaseOrderDate).toBeTruthy();
-
         const purchaseOrderStatus = screen.getByRole('textbox', { name: 'Purchase Order Status' });
         fireEvent.change(purchaseOrderStatus, { target: { value: 'Dmspurchasestatus' } });
-        expect(purchaseOrderStatus).toBeTruthy();
-
         const productDetails = screen.getByRole('heading', { name: 'Product Details' });
         fireEvent.click(productDetails);
         expect(productDetails).toBeTruthy();
     });
 
-    it('should check all button click events', async () => {
-        customRender(<FormWrapper onErrorAction={jest.fn()} handleButtonClick={jest.fn()} buttonData={buttonData} setButtonData={jest.fn()} isLoadingOnSave={false} saveButtonName={saveButtonName} isVisible={true} formActionType={formActionType} typeData={['PO_TYPE']} />);
-
-        const closeBtn = screen.getAllByRole('button', { name: 'Close', exact: false });
-        fireEvent.click(closeBtn[0]);
-        fireEvent.click(closeBtn[1]);
-        const increaseBtn = screen.getByRole('button', { name: 'Increase Value', exact: false });
-        fireEvent.click(increaseBtn);
-        const decreaseBtn = screen.getByRole('button', { name: 'Decrease Value', exact: false });
-        fireEvent.click(decreaseBtn);
-        const cancelBtn = screen.getByRole('button', { name: 'Cancel', exact: false });
-        fireEvent.click(cancelBtn);
-        const cancelPOBtn = screen.getByRole('button', { name: 'Cancel PO', exact: false });
-        fireEvent.click(cancelPOBtn);
-        const cancelVPOBtn = screen.getByRole('button', { name: 'Cancel VPO', exact: false });
-        fireEvent.click(cancelVPOBtn);
-        const cancelOTFBtn = screen.getAllByRole('button', { name: 'Cancel Booking', exact: false });
-        fireEvent.click(cancelOTFBtn[0]);
-        fireEvent.click(cancelOTFBtn[1]);
-        const nextBtn = screen.getByRole('button', { name: 'Next', exact: false });
-        fireEvent.click(nextBtn);
-        const saveBtn = screen.getByRole('button', { name: 'Save', exact: false });
-        fireEvent.click(saveBtn);
-
-
-        
-        const closedBtn = screen.getByRole('img', { name: 'close', exact: false });
-        fireEvent.click(closedBtn);
-        const calendarBtn = screen.getByRole('img', { name: 'calendar', exact: false });
-        fireEvent.click(calendarBtn);
-        const upBtn = screen.getByRole('img', { name: 'up', exact: false });
-        fireEvent.click(upBtn);
-        const downBtn = screen.getByRole('img', { name: 'down', exact: false });
-        fireEvent.click(downBtn);
-
-        const orderTypeBtn = screen.getByRole('combobox', { name: 'Order Type', exact: false });
+    it('should render combobox components', () => {
+        const formActionType = { editMode: true };
+        customRender(<FormWrapper onErrorAction={jest.fn()} setButtonData={jest.fn()} handleButtonClick={jest.fn()} isVisible={true} isLoadingOnSave={false} saveButtonName={saveButtonName} formActionType={formActionType} typeData={['PO_TYPE']} />);
+        const orderTypeBtn = screen.getByRole('combobox', { name: 'Order Type' });
+        fireEvent.change(orderTypeBtn, { target: { value: 'deliver' } });
         fireEvent.click(orderTypeBtn);
-        const modelBtn = screen.getByRole('combobox', { name: 'Model', exact: false });
-        fireEvent.click(modelBtn);
 
-        const quantity = screen.getByRole('spinbutton', { name: 'Quantity' });
-        fireEvent.click(quantity);
+        const modelBtn = screen.getByRole('combobox', { name: 'Model Description' });
+        fireEvent.change(modelBtn, { target: { value: 'deliver' } });
     });
 
-    it('should render text', () => {
-        customRender(<FormWrapper onErrorAction={jest.fn()} buttonData={buttonData} handleButtonClick={jest.fn()} setButtonData={jest.fn()} isLoadingOnSave={false} saveButtonName={saveButtonName} isVisible={true} formActionType={formActionType} typeData={['PO_TYPE']} />);
-        const orderType = screen.getByText('Order Type');
-        expect(orderType).toBeTruthy();
-        const defaultTitle = screen.getByText('default title');
-        expect(defaultTitle).toBeTruthy();
+    it('should render columnheader text', () => {
+        const formActionType = { viewMode: true };
+        customRender(<FormWrapper onErrorAction={jest.fn()} setButtonData={jest.fn()} buttonData={buttonData} handleButtonClick={jest.fn()} isLoadingOnSave={false} saveButtonName={saveButtonName} isVisible={true} formActionType={formActionType} typeData={['PO_TYPE']} />);
+        const orderType = screen.getByRole('columnheader', { name: 'Order Type' });
+        fireEvent.click(orderType);
+        const purchaseOrderNumber = screen.getByRole('columnheader', { name: 'Purchase Order Number' });
+        fireEvent.click(purchaseOrderNumber);
+        const dealerCode = screen.getByRole('columnheader', { name: 'Dealer Code' });
+        fireEvent.click(dealerCode);
+        const dealerLocation = screen.getByRole('columnheader', { name: 'Dealer Location' });
+        fireEvent.click(dealerLocation);
+        const purchaseCancelDate = screen.getByRole('columnheader', { name: 'Purchase Cancel Date' });
+        fireEvent.click(purchaseCancelDate);
+        const soStatus = screen.getByRole('columnheader', { name: 'SO Status' });
+        fireEvent.click(soStatus);
+        const model = screen.getByRole('columnheader', { name: 'Model' });
+        fireEvent.click(model);
+    });
+
+    it('should render add edit form components', () => {
+        const formActionType = { viewMode: false };
+        const dealerFlag = true;
+        customRender(<FormWrapper dealerFlag={dealerFlag} setButtonData={jest.fn()} onErrorAction={jest.fn()} handleButtonClick={jest.fn()} isVisible={true} isLoadingOnSave={false} saveButtonName={saveButtonName} formActionType={formActionType} typeData={['PO_TYPE']} />);
+        const orderTypeBtn = screen.getByRole('combobox', { name: 'Order Type' });
+        fireEvent.change(orderTypeBtn, { target: { value: 'deliver' } });
+        fireEvent.click(orderTypeBtn);
     });
 });
--- src/__tests__/components/sales/vehicle-purchase-order/vehicle-purchase-order-master.test.jsx
+++ src/__tests__/components/sales/vehicle-purchase-order/vehicle-purchase-order-master.test.jsx
@@ -5,7 +5,7 @@
  */
 import '@testing-library/jest-dom/extend-expect';
 import customRender from '@utils/test-utils';
-import { screen, fireEvent, getByRole, getByPlaceholderText } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import { VehiclePurchaseOrderMaster } from '@components/Sales/VehiclePurchaseOrder/VehiclePurchaseOrderMaster';
 import { Form } from 'antd';
 import { Provider } from 'react-redux';
@@ -29,8 +29,6 @@ const FormWrapper = (props) => {
     const [form] = Form.useForm();
     return <VehiclePurchaseOrderMaster form={form} {...props} />;
 };
-
-const buttonData = { closeBtn: true, cancelBtn: true, editBtn: true, allotBtn: true, unAllotBtn: true, invoiceBtn: true, deliveryNoteBtn: true, transferOTFBtn: true, changeHistory: true, nextBtn: true, saveBtn: true, formBtnActive: true, cancelOtfBtn: true };
 
 describe('Vehicle Purchase Order Master component render', () => {
     it('should render Vehicle Purchase Order component', () => {
@@ -58,6 +56,78 @@ describe('Vehicle Purchase Order Master component render', () => {
         fireEvent.click(rightBtn);
     });
 
+    it('should click when user click on img button', () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+        });
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper setFilterString={jest.fn()} resetFields={jest.fn()} handleButtonClick={jest.fn()} setFieldsValue={jest.fn()} onSuccessAction={jest.fn()} setIsLoading={jest.fn()} />
+            </Provider>
+        );
+        const searchBtn = screen.getByRole('img', { name: 'search', exact: false });
+        fireEvent.click(searchBtn);
+        const plusBtn = screen.getByRole('img', { name: 'plus', exact: false });
+        fireEvent.click(plusBtn);
+        const leftBtn = screen.getByRole('img', { name: 'left', exact: false });
+        fireEvent.click(leftBtn);
+        const rightBtn = screen.getByRole('img', { name: 'right', exact: false });
+        fireEvent.click(rightBtn);
+    });
+
+    it('should click when user click on listitem button', () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+        });
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper setFilterString={jest.fn()} resetFields={jest.fn()} handleButtonClick={jest.fn()} setFieldsValue={jest.fn()} onSuccessAction={jest.fn()} setIsLoading={jest.fn()} />
+            </Provider>
+        );
+        const previousPageBtn = screen.getByRole('listitem', { name: 'Previous Page' });
+        fireEvent.click(previousPageBtn);
+        const oneBtn = screen.getByRole('listitem', { name: '1' });
+        fireEvent.click(oneBtn);
+        const nextPageBtn = screen.getByRole('listitem', { name: 'Next Page' });
+        fireEvent.click(nextPageBtn);
+    });
+
+    it('should click on available coulmnheader', () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+        });
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper setFilterString={jest.fn()} resetFields={jest.fn()} handleButtonClick={jest.fn()} setFieldsValue={jest.fn()} onSuccessAction={jest.fn()} setIsLoading={jest.fn()} />
+            </Provider>
+        );
+        const srl = screen.getByRole('columnheader', { name: 'Srl.' });
+        fireEvent.click(srl);
+        const purchaseOrder = screen.getByRole('columnheader', { name: 'Purchase Order Number' });
+        fireEvent.click(purchaseOrder);
+        const purchaseOrderDate = screen.getByRole('columnheader', { name: 'Purchase Order Date' });
+        fireEvent.click(purchaseOrderDate);
+        const orderType = screen.getByRole('columnheader', { name: 'Order Type' });
+        fireEvent.click(orderType);
+        const status = screen.getByRole('columnheader', { name: 'Status' });
+        fireEvent.click(status);
+        const action = screen.getByRole('columnheader', { name: 'Action' });
+        fireEvent.click(action);
+    });
+
+    it('should click when user click on row button', () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+        });
+        customRender(
+            <Provider store={mockStore}>
+                <FormWrapper setFilterString={jest.fn()} resetFields={jest.fn()} handleButtonClick={jest.fn()} setFieldsValue={jest.fn()} onSuccessAction={jest.fn()} setIsLoading={jest.fn()} />
+            </Provider>
+        );
+        const SrlBtn = screen.getByRole('row', { name: 'Srl. Purchase Order Number Purchase Order Date Order Type Status Action' });
+        fireEvent.click(SrlBtn);
+    });
+
     it('reset button should work', () => {
         customRender(<VehiclePurchaseOrderMaster />);
         const advanceFilter = screen.getByRole('button', { name: /Advanced Filters/i });
@@ -72,154 +142,5 @@ describe('Vehicle Purchase Order Master component render', () => {
         fireEvent.click(advanceFilter);
         const closeBtn = screen.getByRole('button', { name: /Close/i });
         fireEvent.click(closeBtn);
-    });
-
-    it('should render advanced filters search clear', () => {
-        const mockStore = createMockStore({
-            auth: { userId: 106 },
-            data: {
-                Vehicle: {
-                    VehiclePurchaseOrderDetail: {
-                        filter: { advanceFilter: 'Test', fromDate: '06/06/2022' },
-                    },
-                },
-            },
-        });
-
-        customRender(
-            <Provider store={mockStore}>
-                <VehiclePurchaseOrderMaster />
-            </Provider>
-        );
-
-        const advanceFilter = screen.getByPlaceholderText(/Search By Vehicle Purchase Order/i);
-        fireEvent.change(advanceFilter, { target: { value: 'Test' } });
-
-        const clearBtn = screen.getByRole('button', { name: 'Clear' });
-        fireEvent.click(clearBtn);
-    });
-
-    it('should render advanced filters search', () => {
-        const mockStore = createMockStore({
-            auth: { userId: 106 },
-            data: {
-                Vehicle: {
-                    VehiclePurchaseOrderDetail: {
-                        filter: { advanceFilter: 'Test', fromDate: '06/06/2022' },
-                    },
-                },
-            },
-        });
-
-        customRender(
-            <Provider store={mockStore}>
-                <VehiclePurchaseOrderMaster fetchList={jest.fn()} buttonData={buttonData} setButtonData={jest.fn()} />
-            </Provider>
-        );
-        const advanceFilter = screen.getByPlaceholderText(/Search By Vehicle Purchase Order/i);
-        fireEvent.change(advanceFilter, { target: { value: 'Test' } });
-
-        const removeFilter = screen.getByTestId('removeFilter');
-        fireEvent.click(removeFilter);
-    });
-
-    it('close button should work', () => {
-        const mockStore = createMockStore({
-            auth: { userId: 106 },
-            data: {
-                Vehicle: {
-                    VehiclePurchaseOrderDetail: {
-                        filter: { advanceFilter: 'Test', fromDate: '06/06/2022' },
-                    },
-                },
-            },
-        });
-        const saveData = jest.fn();
-
-        customRender(
-            <Provider store={mockStore}>
-                <VehiclePurchaseOrderMaster saveData={saveData} setIsFormVisible={jest.fn()} handleButtonClick={jest.fn()} resetData={jest.fn()} fetchList={jest.fn()} buttonData={buttonData} setButtonData={jest.fn()} />
-            </Provider>
-        );
-        const plusAdd = screen.getByRole('button', { name: 'plus Add' });
-        fireEvent.click(plusAdd);
-
-        const saveBtn = screen.getByRole('button', { name: 'Close' });
-        fireEvent.click(saveBtn);
-    });
-
-    it('save button should work', () => {
-        const mockStore = createMockStore({
-            auth: { userId: 106 },
-            data: {
-                Vehicle: {
-                    VehicleAllotPriorDetail: {
-                        data: [{ cancelRemarks: 'test01', cancelRemarksCode: '00test', dealerLocation: 'noid112', dealerLocationId: 'test00', dealerParentCode: '440', dealerParentName: 'test12', id: '235', modelCode: 456, modelDescription: 'test789', orderType: null, orderTypeCode: null, purchaseOrderCancelDate: null, purchaseOrderDate: null, purchaseOrderNumber: 'PO1236', purchaseOrderStatus: null, purchaseOrderStatusCode: null, quantity: null, soDate: null, soNumber: null }],
-                        filter: [{ advanceFilter: true, fromDate: '06/06/2022', orderType: 'ASTK', purchaseOrderNumber: '78', purchaseOrderStatusCode: '80', status: undefined, toDate: '06/10/2022' }],
-                    },
-                },
-            },
-        });
-        const saveData = jest.fn();
-
-        customRender(
-            <Provider store={mockStore}>
-                <VehiclePurchaseOrderMaster saveData={saveData} setIsFormVisible={jest.fn()} handleButtonClick={jest.fn()} fetchList={jest.fn()} resetData={jest.fn()} buttonData={buttonData} setButtonData={jest.fn()} />
-            </Provider>
-        );
-        const plusAdd = screen.getByRole('button', { name: 'plus Add' });
-        fireEvent.click(plusAdd);
-        const orderType = screen.getByRole('combobox', { name: 'Order Type' });
-        fireEvent.change(orderType, { target: { value: 'purchase' } });
-
-        const newModel = screen.getByRole('combobox', { name: 'Model Description' });
-        fireEvent.change(newModel, { target: { value: 'test' } });
-
-        const spinbuttonBtn = screen.getByRole('spinbutton', { name: 'Quantity' });
-        fireEvent.change(spinbuttonBtn, { target: { value: 'upDown' } });
-
-        const submitBtn = screen.getByRole('button', { name: 'Submit' });
-        fireEvent.click(submitBtn);
-    });
-
-    it('should be able to search value', () => {
-        const mockStore = createMockStore({
-            auth: { userId: 106 },
-            data: {
-                Vehicle: {
-                    VehiclePurchaseOrderDetail: {
-                        filter: { advanceFilter: 'Test', fromDate: '06/06/2022' },
-                    },
-                },
-            },
-        });
-        customRender(
-            <Provider store={mockStore}>
-                <VehiclePurchaseOrderMaster fetchList={jest.fn()} showAddButton={true} buttonData={buttonData} />
-            </Provider>
-        );
-        const search = screen.getByPlaceholderText(/Search By Vehicle Purchase Order/i);
-        fireEvent.change(search, { target: { value: 'test' } });
-
-        const searchBtn = screen.getByRole('img', { name: 'search' });
-        fireEvent.click(searchBtn);
-
-        const closeCircle = screen.getByRole('img', { name: 'close-circle' });
-        fireEvent.click(closeCircle);
-    });
-
-    it('search button should work', () => {
-        customRender(<VehiclePurchaseOrderMaster />);
-        const advanceFilter = screen.getByRole('button', { name: /Advanced Filters/i });
-        fireEvent.click(advanceFilter);
-
-        const orderType = screen.getByRole('combobox', { name: 'Order Type' });
-        fireEvent.change(orderType, { target: { value: 'deliver' } });
-
-        const purchaseOrder = screen.getByPlaceholderText('Enter purchase order number');
-        fireEvent.change(purchaseOrder, { target: { value: '231' } });
-
-        const searchBtn = screen.getAllByRole('button', { name: /Search/i });
-        fireEvent.click(searchBtn[0]);
     });
 });
