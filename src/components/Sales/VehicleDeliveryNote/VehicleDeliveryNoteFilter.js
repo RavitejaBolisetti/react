0da170628911463edd29d9bb0eba20d1ff16edf0
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-10-01 20:20:17 +0530

    Fixed link issue

--- src/components/Sales/VehicleDeliveryNote/VehicleDeliveryNoteFilter.js
+++ src/components/Sales/VehicleDeliveryNote/VehicleDeliveryNoteFilter.js
@@ -14,7 +14,6 @@ import { PARAM_MASTER } from 'constants/paramMaster';
 import styles from 'assets/sass/app.module.scss';
 import { DELIVERY_TYPE } from 'constants/modules/vehicleDetailsNotes.js/deliveryType';
 
-
 export default function VehicleDeliveryNoteFilter(props) {
     const { deliveryType, extraParams, removeFilter, searchForm, typeData, filterString, deliveryStatusList, onDeliveryTabChange, setFilterString, handleResetFilter, advanceFilter = false, handleDeliveryNoteTypeChange, setAdvanceSearchVisible, handleButtonClick, deliveryStatus } = props;
 
@@ -39,10 +38,12 @@ export default function VehicleDeliveryNoteFilter(props) {
                         }))}
                     />
                 </Col>
-                <Col xs={24} sm={20} md={20} lg={20} xl={20}>
+            </Row>
+            <Row gutter={20}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                     <Form autoComplete="off" colon={false} className={styles.masterListSearchForm}>
                         <Row gutter={20}>
-                            <Col xs={24} sm={24} md={24} lg={24} xl={24} className={styles.verticallyCentered}>
+                            {/* <Col xs={24} sm={24} md={24} lg={24} xl={24} className={styles.verticallyCentered}>
                                 <Tabs
                                     defaultActiveKey={DELIVERY_TYPE.NOTE.key}
                                     activeKey={deliveryType}
@@ -52,7 +53,7 @@ export default function VehicleDeliveryNoteFilter(props) {
                                         label: <>{item?.title}</>,
                                     }))}
                                 />
-                            </Col>
+                            </Col> */}
                             <Col xs={24} sm={24} md={18} lg={18} xl={18} className={styles.verticallyCentered}>
                                 <QueryButtons currentItem={deliveryStatus} items={deliveryStatusList} onClick={handleDeliveryNoteTypeChange} />
                                 <div className={styles.fullWidth}>
@@ -74,11 +75,6 @@ export default function VehicleDeliveryNoteFilter(props) {
                         </Row>
                     </Form>
                 </Col>
-                {/* <Col xs={24} sm={4} md={4} lg={4} xl={4} className={styles.buttonsGroupRight}>
-                    <Button icon={<PlusOutlined />} className={styles.actionbtn} type="primary" onClick={() => handleButtonClick({ buttonAction: FROM_ACTION_TYPE?.ADD })}>
-                        Add
-                    </Button>
-                </Col> */}
             </Row>
             {advanceFilter && filterString?.advanceFilter && extraParams.find((i) => i.name) && (
                 <Row gutter={20}>
