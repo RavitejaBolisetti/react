e174cb353c02a558645de9cba7128ee4f299aedf
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-10-03 14:24:02 +0000

    Squashed commit of the following
    commit 1533d488 
    Author: jatin kumar <jatin.kumar13@wipro.com> 
    Date: Tue Oct 03 2023 12:33:31 GMT+0530 (India Standard Time) 
    
        change password unit test done

--- src/__tests__/components/common/change-password/change-password-form.test.jsx
+++ src/__tests__/components/common/change-password/change-password-form.test.jsx
@@ -3,15 +3,35 @@
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
-import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { act } from 'react-dom/test-utils';
-import { ChangePasswordForm } from '@components/common/ChangePassword/ChangePasswordForm';
+import { ChangePasswordForm } from 'components/common/ChangePassword/ChangePasswordForm';
 import customRender from '@utils/test-utils';
+import { Form } from 'antd';
 
 afterEach(() => {
     jest.restoreAllMocks();
 });
+
+jest.mock('store/actions/data/changePassword', () => ({
+    changePasswordActions: {}
+}));
+
+jest.mock('store/actions/auth', () => ({
+    doLogoutAPI: {}
+}));
+
+const FormWrapper = (props) => {
+    const [form] = Form.useForm();
+
+    const myFormMock = {
+        ...form,
+        setFieldsValue: jest.fn(),
+        resetFields: jest.fn()
+    };
+    return <ChangePasswordForm form={myFormMock} {...props} />;
+};
 
 const setFieldData = () => {};
 const setPassword = () => {};
@@ -21,10 +41,12 @@ describe('ChangePasswordForm Components', () => {
     it('should render ChangePasswordForm components', () => {
         customRender(<ChangePasswordForm />);
     });
+
     it('should change old password field in the screen', () => {
         customRender(<ChangePasswordForm />);
         expect(screen.getByText(/Old password/i)).toBeInTheDocument();
     });
+
     it('should change password field event', async () => {
         customRender(<ChangePasswordForm />);
         await act(async () => {
@@ -34,8 +56,14 @@ describe('ChangePasswordForm Components', () => {
             fireEvent.click(changePasswordButton);
         });
     });
+
     it('should render change password form input field oldPassword, newPassword and confirmPassword', async () => {
-        customRender(<ChangePasswordForm setFieldData={setFieldData} setPassword={setPassword} callback={handler} />);
+
+        const saveData=jest.fn();
+        const doLogout=jest.fn();
+        const res={ data: { name: 'Kai' } };
+
+        customRender(<FormWrapper setFieldData={setFieldData} setPassword={setPassword} callback={handler} saveData={saveData} doLogout={doLogout} />);
 
         const oldPasswordInput = screen.getByTestId('oldPasswordInput');
         fireEvent.change(oldPasswordInput, {
@@ -51,6 +79,9 @@ describe('ChangePasswordForm Components', () => {
 
         const confirmNewPasswordInput = screen.getByTestId('confirmNewPasswordInput');
         fireEvent.change(confirmNewPasswordInput, {
+            target: { value: 'Dma@test123' },
+        });
+        fireEvent.change(confirmNewPasswordInput, {
             target: { value: 'Dma@test1234' },
         });
         expect(confirmNewPasswordInput.value.includes('Dma@test1234'));
@@ -58,5 +89,18 @@ describe('ChangePasswordForm Components', () => {
         act(async () => {
             fireEvent.click(screen.getByTestId('changePassword'));
         });
+
+        const showPassword=screen.getAllByTestId('eyeInvisible');
+        fireEvent.mouseDown(showPassword[0]);
+        fireEvent.mouseUp(showPassword[0]);
+        fireEvent.mouseLeave(showPassword[0]);
+
+        await waitFor(() => { expect(saveData).toHaveBeenCalled() });
+        saveData.mock.calls[0][0].onSuccess();
+        saveData.mock.calls[0][0].onError();
+
+        await waitFor(() => { expect(doLogout).toHaveBeenCalled() });
+        doLogout.mock.calls[0][0].onSuccess(res);
     });
+
 });
