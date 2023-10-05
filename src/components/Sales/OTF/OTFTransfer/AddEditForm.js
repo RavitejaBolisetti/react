df3cd8021684a372fc68cac056c00b9e9ceaaa77
Simran <Simran.sehgal@wipro.com>
2023-10-06 00:02:33 +0530

    changes

--- src/components/Sales/OTF/OTFTransfer/AddEditForm.js
+++ src/components/Sales/OTF/OTFTransfer/AddEditForm.js
@@ -39,7 +39,6 @@ const AddEditFormMain = (props) => {
         layout: 'vertical',
         column: { xs: 1, sm: 2, lg: 2, xl: 2, xxl: 2 },
     };
-    console.log(defaultDealerLocationCode);
 
     return (
         <>
