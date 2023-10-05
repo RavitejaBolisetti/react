7ecbd61dc2e12464d08e558a4d0f81673d8f93f0
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-10-05 05:57:58 +0000

    Squashed commit of the following
    commit 7e1fb210 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 17:40:28 GMT+0530 (India Standard Time) 
    
        change in manu
    
    
    commit 4c8906e9 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 16:17:00 GMT+0530 (India Standard Time) 
    
        common otf button
    
    
    commit 54c8b4cc 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 16:14:09 GMT+0530 (India Standard Time) 
    
        otf button chnage
    
    
    commit c4a20e37 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 16:04:11 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into OTF-exchange-vehicle-unit-testcase
    
    
    commit c0ecf3e9 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 13:26:36 GMT+0530 (India Standard Time) 
    
        dealer company 85%
    
    
    commit fce925ce 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 11:44:39 GMT+0530 (India Standard Time) 
    
        echange vehicles master
    
    
    commit 12060860 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 11:12:18 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into OTF-exchange-vehicle-unit-testcase
    
    
    commit 1a04b8c9 
    Author: vivek <vivek.kumar176@wipro.com> 
    Date: Wed Oct 04 2023 11:10:47 GMT+0530 (India Standard Time) 
    
        push latest

--- src/__tests__/components/sales/common/otf-form-button/otf-form-button.test.jsx
+++ src/__tests__/components/sales/common/otf-form-button/otf-form-button.test.jsx
@@ -1,10 +1,15 @@
-import '@testing-library/jest-dom/extend-expect';
-import customRender from '@utils/test-utils';
+import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
-import { OTFFormButton } from 'components/Sales/OTF/OTFFormButton';
+import { Provider } from 'react-redux';
+import createMockStore from '__mocks__/store';
+import { OTFFormButton } from '@components/Sales/Common/OTFFormButton/OTFFormButton';
+import customRender from '@utils/test-utils';
+import { Form, Button } from 'antd';
+
 afterEach(() => {
     jest.restoreAllMocks();
 });
+
 const buttonData = {
     closeBtn: true,
     cancelBtn: true,
@@ -20,48 +25,51 @@ const buttonData = {
     saveBtn: true,
     formBtnActive: true,
     otfSoMappingHistoryBtn: true,
-    pendingCancellationOTFBtn: true
 };
 
-describe('Booking Form Button Component', () => {
-    it('should render Booking form button component', () => {
-        customRender(<OTFFormButton />);
-    });
+describe('OTF form button render', () => {
 
-    it('should render Booking form all button', async () => {
-        const workFlowDetails = {
-            allowedActions: [{
-                allowedActions: { actionName: "test" }
-            }]
-        }
-        customRender(<OTFFormButton workFlowDetails={workFlowDetails} setButtonData={jest.fn()} buttonData={buttonData} handleButtonClick={jest.fn()} />);
+    it("Should render OTF form button components", () => {
+        customRender(<OTFFormButton />)
+    })
+
+    it('Should render OTF All button', async () => {
+        customRender(<OTFFormButton setButtonData={jest.fn()} buttonData={buttonData} handleButtonClick={jest.fn()} saveButtonName={"Save & Next"}/>);
         const closeBtn = screen.getByRole('button', { name: 'Close' });
         fireEvent.click(closeBtn);
         const cancelBtn = screen.getByRole('button', { name: 'Cancel' });
         fireEvent.click(cancelBtn);
+
         const editBtn = screen.getByRole('button', { name: 'Edit' });
         fireEvent.click(editBtn);
+
+        const bookingMapHistoryBtn = screen.getByRole('button', { name: 'Booking Mapping History' });
+        fireEvent.click(bookingMapHistoryBtn);
+
         const allotBtn = screen.getByRole('button', { name: 'Allot' });
         fireEvent.click(allotBtn);
+
         const unAllotBtn = screen.getByRole('button', { name: 'Un-Allot' });
         fireEvent.click(unAllotBtn);
+
         const invoiceBtn = screen.getByRole('button', { name: 'Invoice' });
         fireEvent.click(invoiceBtn);
         const deliveryNoteBtn = screen.getByRole('button', { name: 'Delivery Note' });
         fireEvent.click(deliveryNoteBtn);
+
         const transferOTFBtn = screen.getByRole('button', { name: 'Transfer Booking' });
         fireEvent.click(transferOTFBtn);
+
         const cancelOTFBtn = screen.getByRole('button', { name: 'Cancel Booking' });
         fireEvent.click(cancelOTFBtn);
+
         const changeHistory = screen.getByRole('button', { name: 'Change History' });
         fireEvent.click(changeHistory);
+
         const nextBtn = screen.getByRole('button', { name: 'Next' });
         fireEvent.click(nextBtn);
-        const saveBtn = screen.getByRole('button', { name: 'Save' });
+        const saveBtn = screen.getByRole('button', { name: 'Save & Next' });
         fireEvent.click(saveBtn);
-        const bookingMapHistoryBtn = screen.getByRole('button', { name: 'Booking Mapping History' });
-        fireEvent.click(bookingMapHistoryBtn);
-        const allowedActions = screen.getByRole('button', { name: '' });
-        fireEvent.click(allowedActions);
     });
+
 });
