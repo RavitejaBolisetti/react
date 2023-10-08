fff564627c12980ca3bc25de9ce2bb8a980b4fe3
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-10-08 11:10:53 +0000

    Squashed commit of the following
    commit 75407864 
    Author: Vivek <vivek.vimal@wipro.com> 
    Date: Sat Oct 07 2023 13:17:55 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into US-Family-Details-Work
    
    
    commit ae4e62ec 
    Author: Vivek <vivek.vimal@wipro.com> 
    Date: Sat Oct 07 2023 13:02:15 GMT+0530 (India Standard Time) 
    
        working on family
    
    
    commit 4ef26cc9 
    Author: Vivek <vivek.vimal@wipro.com> 
    Date: Sat Oct 07 2023 01:58:21 GMT+0530 (India Standard Time) 
    
        working
    
    
    commit 35c5dc8f 
    Author: Vivek <vivek.vimal@wipro.com> 
    Date: Sat Oct 07 2023 01:53:36 GMT+0530 (India Standard Time) 
    
        working on FD
    
    
    commit a9bd63ef 
    Author: Vivek <vivek.vimal@wipro.com> 
    Date: Sat Oct 07 2023 01:34:34 GMT+0530 (India Standard Time) 
    
        working

--- src/components/common/CustomerMaster/IndividualCustomer/FamilyDetail/ViewDetail.js
+++ src/components/common/CustomerMaster/IndividualCustomer/FamilyDetail/ViewDetail.js
@@ -7,6 +7,7 @@ import React from 'react';
 import { Descriptions } from 'antd';
 import { DATA_TYPE } from 'constants/dataType';
 import { checkAndSetDefaultValue } from 'utils/checkAndSetDefaultValue';
+import { YES_NO_FLAG } from 'constants/yesNoFlag';
 
 import styles from 'assets/sass/app.module.scss';
 
@@ -20,16 +21,18 @@ const ViewDetailBase = (props) => {
         column: { xxl: 3, xl: 3, lg: 3, md: 2, sm: 1, xs: 1 },
     };
 
+    const yesNoCustomerType = props?.mnmCustomer === YES_NO_FLAG?.YES?.key ? YES_NO_FLAG?.YES?.title : props?.mnmCustomer === YES_NO_FLAG?.NO?.key ? YES_NO_FLAG?.NO?.title : null;
+
     return (
         <div className={styles.viewDrawerContainer}>
             <Descriptions {...viewProps} className={styles.descriptionBox}>
-                <Descriptions.Item label="M&M Customer">{checkAndSetDefaultValue(props?.mnmCustomer, isLoading)}</Descriptions.Item>
-                {props?.mnmCustomer === 'Yes' ? <Descriptions.Item label="Customer ID">{checkAndSetDefaultValue(props?.relationCustomerId, isLoading)}</Descriptions.Item> : null}
+                <Descriptions.Item label="M&M Customer">{checkAndSetDefaultValue(yesNoCustomerType, isLoading)}</Descriptions.Item>
+                {props?.mnmCustomer === YES_NO_FLAG?.YES?.key ? <Descriptions.Item label="Customer ID">{checkAndSetDefaultValue(props?.relationCustomerId, isLoading)}</Descriptions.Item> : null}
                 <Descriptions.Item label="Customer Name">{checkAndSetDefaultValue(props?.customerName, isLoading)}</Descriptions.Item>
                 <Descriptions.Item label="Relationship">{checkAndSetDefaultValue(props?.relationship, isLoading)}</Descriptions.Item>
                 <Descriptions.Item label="Date of Birth">{checkAndSetDefaultValue(props?.dateOfBirth, isLoading, DATA_TYPE?.DATE?.key)}</Descriptions.Item>
                 <Descriptions.Item label="Age">{checkAndSetDefaultValue(props?.relationAge, isLoading)}</Descriptions.Item>
-                {props?.mnmCustomer === 'No' ? <Descriptions.Item label="" /> : null}
+                {props?.mnmCustomer === YES_NO_FLAG?.NO?.key ? <Descriptions.Item label="" /> : null}
                 <Descriptions.Item label="Remark">{checkAndSetDefaultValue(props?.remarks, isLoading)}</Descriptions.Item>
             </Descriptions>
         </div>
