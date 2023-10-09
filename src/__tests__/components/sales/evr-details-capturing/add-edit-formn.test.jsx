e5d9f945946a030fdc668b8c300d9b428edbd0e9
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-10-09 17:30:05 +0000

    Squashed commit of the following
    commit 797bd5b5 
    Author: kuldeep prajapati <ku1442186t@wipro.com> 
    Date: Mon Oct 09 2023 18:13:19 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' into SALES-EvrDetailsCapturing-unit-testcase
    
    
    commit 645a8972 
    Author: kuldeep prajapati <ku1442186t@wipro.com> 
    Date: Mon Oct 09 2023 17:40:57 GMT+0530 (India Standard Time) 
    
        worked on coverage
    
    
    commit cd94c4e8 
    Author: kuldeep prajapati <ku1442186t@wipro.com> 
    Date: Mon Oct 09 2023 10:39:28 GMT+0530 (India Standard Time) 
    
        Merge branch 'feature' of https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/dms-ui-application into SALES-EvrDetailsCapturing-unit-testcase
    
    
    commit 0b687fda 
    Author: kuldeep prajapati <ku1442186t@wipro.com> 
    Date: Sat Oct 07 2023 17:31:52 GMT+0530 (India Standard Time) 
    
        improved coverage

---
+++ src/__tests__/components/sales/evr-details-capturing/add-edit-formn.test.jsx
@@ -0,0 +1,80 @@
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { screen, fireEvent, getByRole } from '@testing-library/react';
+import customRender from '@utils/test-utils';
+import { AddEditForm } from '@components/Sales/EvrDetailsCapturing/AddEditForm';
+import { Tag } from 'antd';
+
+const formData = { ageInDays: '241', chargeIndicator: false, chargingDueDate: '2023-08-18T11:48:51.820+00:00', chargingStatus: 'DUE FOR CHARGING', grnDate: '2023-02-07T00:00:00.000+00:00', grnId: 'GRN23D001298', grnStatus: 'RTRN', id: '61b15a20-a715-4336-8538-9bd0ca80803a', lastChargeDate: '2023-08-03T11:48:51.819+00:00', modelCode: 'ALPLM026117156889', modelDescription: 'MAHINDRA ALFA LC PLUS CNG BSVI WARM RED', modelGroupCode: 'ECOM', remarks: 'CHARGED Vehicle', vehicleStatus: null, vin: 'MA1LV2NT9P5E80476' };
+
+describe('edit form component', () => {
+    it('should render add edit form component', () => {
+        const formActionType = { editMode: true };
+        customRender(<AddEditForm formActionType={formActionType} setButtonData={jest.fn()} isVisible={true} />);
+
+        const vin = screen.getAllByRole('textbox', { value: 'VIN' });
+        fireEvent.change(vin[0], { target: { value: 'testvin' } });
+
+        const modelGroup = screen.getAllByRole('textbox', { value: 'Model Group' });
+        fireEvent.change(modelGroup[0], { target: { value: 'testgroup' } });
+
+        const modelCode = screen.getAllByRole('textbox', { value: 'Model Code' });
+        fireEvent.change(modelCode[0], { target: { value: 'test123' } });
+
+        const modelDescription = screen.getAllByRole('textbox', { value: 'Model Description' });
+        fireEvent.change(modelDescription[0], { target: { value: 'testmodel' } });
+
+        const grnID = screen.getAllByRole('textbox', { value: 'GRN ID' });
+        fireEvent.change(grnID[0], { target: { value: 'grn123' } });
+
+        const grnDate = screen.getAllByRole('textbox', { value: 'GRN Date' });
+        fireEvent.change(grnDate[0], { target: { value: '16-09-2015' } });
+
+        const grnStatus = screen.getAllByRole('textbox', { value: 'GRN Status' });
+        fireEvent.change(grnStatus[0], { target: { value: 'grnok' } });
+
+        const vehicleSt = screen.getAllByRole('textbox', { value: 'vehicleStatus' });
+        fireEvent.change(vehicleSt[0], { target: { value: 'vehicleok' } });
+
+        const ageInDays = screen.getAllByRole('textbox', { value: 'Age In Days' });
+        fireEvent.change(ageInDays[0], { target: { value: '25' } });
+
+        const dateofLastCharge = screen.getAllByRole('textbox', { value: 'Date of Last Charge' });
+        fireEvent.change(dateofLastCharge[0], { target: { value: '30' } });
+
+        const chargingDueDate = screen.getAllByRole('textbox', { value: 'Charging Due Date' });
+        fireEvent.change(chargingDueDate[0], { target: { value: '15' } });
+
+        const chargingStatus = screen.getAllByRole('textbox', { value: 'Charging Status' });
+        fireEvent.change(chargingStatus[0], { target: { value: '25' } });
+
+        const remarks = screen.getAllByRole('textbox', { value: 'Remarks' });
+        fireEvent.change(remarks[0], { target: { value: 'testRemark' } });
+    });
+
+    it('Switch component should handle onChange correctly', () => {
+        const formActionType = { editMode: true };
+        const formData = { chargeIndicator: false };
+
+        customRender(<AddEditForm formActionType={formActionType} setButtonData={jest.fn()} isVisible={true} formData={formData} />);
+        const chargedUncharged = screen.getByRole('switch', { value: 'Charged UnCharged' });
+        fireEvent.click(chargedUncharged);
+    });
+
+    it('should render add edit form button', () => {
+        const formActionType = { editMode: true };
+        customRender(<AddEditForm formActionType={formActionType} setButtonData={jest.fn()} isVisible={true} />);
+        const cal = screen.getAllByRole('img', { value: 'calendar' });
+        fireEvent.click(cal[0]);
+        fireEvent.click(cal[1]);
+    });
+
+    it('should render view details component', () => {
+        const formActionType = { viewMode: true };
+        customRender(<AddEditForm formActionType={formActionType} setButtonData={jest.fn()} isVisible={true} formData={formData} />);
+    });
+});
--- src/__tests__/components/sales/evr-details-capturing/evr-details-capturing-master.test.jsx
+++ src/__tests__/components/sales/evr-details-capturing/evr-details-capturing-master.test.jsx
@@ -1,15 +1,174 @@
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { EvrDetailsCapturingMaster } from '@components/Sales/EvrDetailsCapturing/EvrDetailsCapturingMaster';
 import customRender from '@utils/test-utils';
-import { Form, Button } from 'antd';
+import { Form } from 'antd';
 import createMockStore from '__mocks__/store';
+import { Provider } from 'react-redux';
 
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+jest.mock('@components/Sales/EvrDetailsCapturing/AddEditForm', () => {
+    const AddEditForm = ({ onFinish }) => (
+        <div>
+            <button onClick={onFinish}>Save</button>
+        </div>
+    );
+    return {
+        __esModule: true,
+        AddEditForm,
+    };
+});
+
+jest.mock('store/actions/data/evrDetailsCapturing/evrDetailsCapturing', () => ({
+    evrDetailsCapturingDataActions: {},
+}));
+
+jest.mock('store/actions/data/productHierarchy', () => ({
+    productHierarchyDataActions: {},
+}));
+
+const FormWrapper = (props) => {
+    const [advanceFilterForm] = Form.useForm();
+    return <EvrDetailsCapturingMaster advanceFilterForm={advanceFilterForm} {...props} />;
+};
+
+const buttonData = { closeBtn: true, cancelBtn: true, editBtn: true, allotBtn: true, unAllotBtn: true, invoiceBtn: true, deliveryNoteBtn: true, transferOTFBtn: true, changeHistory: true, nextBtn: true, saveBtn: true, formBtnActive: true, cancelOtfBtn: true };
 
 describe('Evr details capturing master render', () => {
+    it('Should render Evr details capturing master components', () => {
+        customRender(<EvrDetailsCapturingMaster fetchProductList={jest.fn()} />);
+    });
 
-    it("Should render Evr details capturing master components", () => {
-        customRender(<EvrDetailsCapturingMaster />)
-    })
-})
+    it('reset button should work', () => {
+        customRender(<FormWrapper setFilterString={jest.fn()} fetchProductList={jest.fn()} />);
+        const advanceFilter = screen.getByRole('button', { name: /Advanced Filters/i });
+        fireEvent.click(advanceFilter);
+        const resetBtn = screen.getByRole('button', { name: /Reset/i });
+        fireEvent.click(resetBtn);
+    });
+
+    it('test for closing the advance filter', () => {
+        customRender(<FormWrapper fetchProductList={jest.fn()} />);
+        const advanceFilter = screen.getByRole('button', { name: /Advanced Filters/i });
+        fireEvent.click(advanceFilter);
+        const closeBtn = screen.getByRole('button', { name: /Close/i });
+        fireEvent.click(closeBtn);
+    });
+
+    it('test for onfinish failed', async () => {
+        customRender(<FormWrapper isVisible={true} fetchProductList={jest.fn()} />);
+        const advanceFilter = screen.getByRole('button', { name: /Advanced Filters/i });
+        fireEvent.click(advanceFilter);
+        const searchBtn = screen.getByRole('button', { name: /Apply/i });
+        fireEvent.click(searchBtn);
+    });
+
+    it('advance filter Clear button should work', () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+            data: {
+                ProductHierarchy: [{ active: null, attributeKey: null, attributeType: 'PD', disabled: true, id: 'IG', mfgOrgSk: null, parntProdctCode: null, parntProdctId: null, prodctCode: 'IG', prodctLongName: 'Xylo', prodctShrtName: 'Xylo' }],
+                EvrDetailsCapturing: {
+                    EvrDetailsCapturingSearchList: [
+                        {
+                            data: [{ chargingStatus: 'DUE FOR CHARGING', id: '81b5f880-7a07-4a5e-aa34-629c9eeedeb8', modelCode: 'ALFLMM8585890582', modelDescription: 'MAHINDRA ALFA LC PLUS DSL BSVI WARM RED', modelGroupCode: 'LOGN' }],
+                            evrDetailData: [{ ageInDays: '279', chargeIndicator: false, chargingDueDate: '2023-07-18T10:04:31.076+00:00', chargingStatus: 'DUE FOR CHARGING', grnDate: '2022-12-31T00:00:00.000+00:00', grnId: 'GRN23A000425', grnStatus: 'RCV', id: '81b5f880-7a07-4a5e-aa34-629c9eeedeb8', lastChargeDate: '2023-07-03T10:04:31.076+00:00', modelCode: 'ALFLMM8585890582', modelDescription: 'MAHINDRA ALFA LC PLUS DSL BSVI WARM RED', modelGroupCode: 'LOGN', remarks: 'charged vehicle', vehicleStatus: null, vin: 'MA1LV2NR9N3A15434' }],
+                            filter: [{ advanceFilter: true, dueFromDate: '2023-10-01', dueToDate: '2023-10-05', model: 'LOGN' }],
+                        },
+                    ],
+                },
+            },
+        });
+        const saveData = jest.fn();
+        const fetchList = jest.fn();
+
+        customRender(
+            <Provider store={mockStore}>
+                <EvrDetailsCapturingMaster setFilterString={jest.fn()} fetchProductList={jest.fn()} saveData={saveData} setIsFormVisible={jest.fn()} handleButtonClick={jest.fn()} fetchList={fetchList} resetData={jest.fn()} buttonData={buttonData} setButtonData={jest.fn()} />
+            </Provider>
+        );
+        const advanceFilter = screen.getByPlaceholderText(/Search Model Description/i);
+        fireEvent.change(advanceFilter, { target: { value: 'Test' } });
+
+        const clearBtn = screen.getByRole('button', { name: 'close-circle' });
+        fireEvent.click(clearBtn);
+    });
+
+    it('onFinish should work', async () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+            data: {
+                ProductHierarchy: [{ active: null, attributeKey: null, attributeType: 'PD', disabled: true, id: 'IG', mfgOrgSk: null, parntProdctCode: null, parntProdctId: null, prodctCode: 'IG', prodctLongName: 'Xylo', prodctShrtName: 'Xylo' }],
+                EvrDetailsCapturing: {
+                    EvrDetailsCapturingSearchList: [
+                        {
+                            data: [{ chargingStatus: 'DUE FOR CHARGING', id: '81b5f880-7a07-4a5e-aa34-629c9eeedeb8', modelCode: 'ALFLMM8585890582', modelDescription: 'MAHINDRA ALFA LC PLUS DSL BSVI WARM RED', modelGroupCode: 'LOGN' }],
+                            evrDetailData: [{ ageInDays: '279', chargeIndicator: false, chargingDueDate: '2023-07-18T10:04:31.076+00:00', chargingStatus: 'DUE FOR CHARGING', grnDate: '2022-12-31T00:00:00.000+00:00', grnId: 'GRN23A000425', grnStatus: 'RCV', id: '81b5f880-7a07-4a5e-aa34-629c9eeedeb8', lastChargeDate: '2023-07-03T10:04:31.076+00:00', modelCode: 'ALFLMM8585890582', modelDescription: 'MAHINDRA ALFA LC PLUS DSL BSVI WARM RED', modelGroupCode: 'LOGN', remarks: 'charged vehicle', vehicleStatus: null, vin: 'MA1LV2NR9N3A15434' }],
+                            filter: [{ advanceFilter: true, dueFromDate: '2023-10-01', dueToDate: '2023-10-05', model: 'LOGN' }],
+                        },
+                    ],
+                },
+            },
+        });
+
+        const res = {
+            data: [{ chargingStatus: 'DUE FOR CHARGING', id: '81b5f880-7a07-4a5e-aa34-629c9eeedeb8', modelCode: 'ALFLMM8585890582', modelDescription: 'MAHINDRA ALFA LC PLUS DSL BSVI WARM RED', modelGroupCode: 'LOGN' }],
+        };
+
+        const fetchList = jest.fn();
+        const fetchDetailList = jest.fn();
+        const saveData = jest.fn();
+
+        customRender(
+            <Provider store={mockStore}>
+                <EvrDetailsCapturingMaster fetchProductList={jest.fn()} fetchDetailList={fetchDetailList} saveData={saveData} setIsFormVisible={jest.fn()} handleButtonClick={jest.fn()} fetchList={fetchList} buttonData={buttonData} setButtonData={jest.fn()} />
+            </Provider>
+        );
+
+        const save = screen.getByRole('button', { name: 'Save' });
+        fireEvent.click(save);
+
+        await waitFor(() => {
+            expect(saveData).toHaveBeenCalled();
+        });
+
+        saveData.mock.calls[0][0].onSuccess(res);
+        saveData.mock.calls[0][0].onError();
+    });
+
+    it('save button should work', () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+            data: {
+                EvrDetailsCapturing: {
+                    EvrDetailsCapturingSearchList: { isLoaded: true, filter: { advanceFilter: true, modelDescription: 'modeltest', model: 'testing', dueFromDate: '01/01/2000', dueToDate: '01/01/2002' }, key: 'searchParam' },
+                },
+            },
+        });
+        const fetchList = jest.fn();
+        const fetchProductList = jest.fn();
+
+        customRender(
+            <Provider store={mockStore}>
+                <EvrDetailsCapturingMaster fetchList={fetchList} fetchProductList={fetchProductList} setFilterString={jest.fn()} />
+            </Provider>
+        );
+
+        const advanceFilter = screen.getByPlaceholderText(/Search Model Description/i);
+        fireEvent.change(advanceFilter, { target: { value: 'Test' } });
+
+        const removeFilter = screen.getAllByTestId('removeFilter');
+        fireEvent.click(removeFilter[0]);
+        fetchList.mock.calls[0][0].onSuccessAction();
+        fetchList.mock.calls[0][0].onErrorAction();
+        fetchProductList.mock.calls[0][0].onCloseAction();
+    });
+});
