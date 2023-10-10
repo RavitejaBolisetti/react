0a433eb0f349b0a6af7aef39ad2ebec19058e1f4
kuldeep prajapati <ku1442186t@wipro.com>
2023-10-10 16:14:02 +0530

    RsmAsmApproval worked on coverage

---
+++ src/__tests__/components/sales/rsm-asm-approval/rsm-asm-approval-buttons.test.jsx
@@ -0,0 +1,31 @@
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { RsmAsmApprovalButtons } from '@components/Sales/RsmAsmApproval/RsmAsmApprovalButtons';
+import customRender from '@utils/test-utils';
+import { screen, fireEvent } from '@testing-library/react';
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+describe('RSM ASM form-button components', () => {
+    it('should render components', () => {
+        const buttonData = {
+            closeBtn: true,
+            rejectBtn: true,
+            approveBtn: true,
+        };
+        customRender(<RsmAsmApprovalButtons setButtonData={jest.fn()} buttonData={buttonData} handleButtonClick={jest.fn()} handleRequest={jest.fn()} />);
+
+        const closeBtn = screen.getByRole('button', { name: 'Close' });
+        fireEvent.click(closeBtn);
+        const rejectBtn = screen.getByRole('button', { name: 'Reject' });
+        fireEvent.click(rejectBtn);
+        const approveBtn = screen.getByRole('button', { name: 'Approve' });
+        fireEvent.click(approveBtn);
+    });
+});
---
+++ src/__tests__/components/sales/rsm-asm-approval/rsm-asm-approval-master.test.jsx
@@ -0,0 +1,64 @@
+/*
+ *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
+ *   All rights reserved.
+ *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
+ */
+import React from 'react';
+import RsmAsmApprovalMaster from 'components/Sales/RsmAsmApproval/RsmAsmApprovalMaster';
+import customRender from '@utils/test-utils';
+import { screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import createMockStore from '__mocks__/store';
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+jest.mock('store/actions/data/rsmAsmApproval/rsmAsmApprovalSearch', () => ({
+    rsmAsmApprovalSearchDataAction: {},
+}));
+
+describe('Rsm Asm Approval Master Component', () => {
+    it('should render RSM ASM pproval master component UI', () => {
+        customRender(<RsmAsmApprovalMaster setFilterString={jest.fn()} />);
+    });
+
+    it('reset button should work', () => {
+        customRender(<RsmAsmApprovalMaster setFilterString={jest.fn()} />);
+        const advanceFilter = screen.getByRole('button', { name: /Advanced Filters/i });
+        fireEvent.click(advanceFilter);
+        const resetBtn = screen.getByRole('button', { name: /Reset/i });
+        fireEvent.click(resetBtn);
+    });
+
+    it('test for closing the advance filter', () => {
+        customRender(<RsmAsmApprovalMaster setFilterString={jest.fn()} />);
+        const advanceFilter = screen.getByRole('button', { name: /Advanced Filters/i });
+        fireEvent.click(advanceFilter);
+        const closeBtn = screen.getByRole('button', { name: /Close/i });
+        fireEvent.click(closeBtn);
+    });
+
+    it('remove filter button should work', () => {
+        const mockStore = createMockStore({
+            auth: { userId: 106 },
+            data: {
+                RsmAsmApproval: {
+                    RsmAsmApprovalSearch: { filter: { advanceFilter: 'Test', fromDate: '06/06/2022', toDate: '06/06/2022', searchParam: 'datatest' }, key: 'searchParam' },
+                },
+            },
+        });
+        const fetchList = jest.fn();
+        customRender(
+            <Provider store={mockStore}>
+                <RsmAsmApprovalMaster fetchList={fetchList} setFilterString={jest.fn()} />
+            </Provider>
+        );
+        const advanceFilter = screen.getByPlaceholderText(/Search by Dealer Name/i);
+        fireEvent.change(advanceFilter, { target: { value: 'Test' } });
+        const removeFilter = screen.getAllByTestId('removeFilter');
+        fireEvent.click(removeFilter[0]);
+        fetchList.mock.calls[0][0].onSuccessAction();
+        fetchList.mock.calls[0][0].onErrorAction();
+    });
+});
