9a64ca24ec45592001c8571d18ae90e6985d332e
jatin kumar <jatin.kumar13@wipro.com>
2023-10-23 17:10:57 +0530

    delivery note unit test improved

---
+++ src/__tests__/components/sales/delivery-noteInvoice-cancellation/advance-filter.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import AdvanceFilter from 'components/Sales/DeliveryNoteInvoiceCancellation/AdvanceFilter';
+import customRender from '@utils/test-utils';
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+describe('Advance Filter Component', () => {
+
+    it('should render advance filter component UI', () => {
+        const filterString={ advanceFilter: true };
+        const extraParams=[{ name: 'Kai', value: 'Kai', filter: 'Kai', canRemove: true }]; 
+
+        customRender(<AdvanceFilter filterString={filterString} extraParams={extraParams} handleResetFilter={jest.fn()} removeFilter={jest.fn()} />);
+
+        const removeFilter=screen.getByTestId('removeFilter');
+        fireEvent.click(removeFilter);
+
+        const resetBtn=screen.getByRole('button', { name: 'Clear' });
+        fireEvent.click(resetBtn);
+    });
+
+});
