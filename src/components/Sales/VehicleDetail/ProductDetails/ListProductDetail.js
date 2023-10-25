2251e71f5f93358759ed070c81b8468f4e18989f
Sushil Kumar <sushil.kumar74@wipro.com>
2023-10-25 16:50:46 +0530

    code clean

--- src/components/Sales/VehicleDetail/ProductDetails/ListProductDetail.js
+++ src/components/Sales/VehicleDetail/ProductDetails/ListProductDetail.js
@@ -323,7 +323,7 @@ const ProductDetailMasterMain = (props) => {
     };
 
     return (
-        <Form layout="vertical" autoComplete="off" form={form} onValuesChange={handleFormValueChange} onFieldsChange={handleFormValueChange} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+        <Form layout="vertical" autoComplete="off" form={form} onValuesChange={handleFormValueChange} onFieldsChange={handleFormValueChange} onFinish={onFinish}>
             <Row gutter={20} className={styles.drawerBodyRight}>
                 <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                     <Row>
