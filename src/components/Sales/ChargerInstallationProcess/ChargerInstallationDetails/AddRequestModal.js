3bca3b3dcfc05d65ff54c4174418a010d210f54b
Simran <Simran.sehgal@wipro.com>
2023-10-25 17:19:53 +0530

    changes

--- src/components/Sales/ChargerInstallationProcess/ChargerInstallationDetails/AddRequestModal.js
+++ src/components/Sales/ChargerInstallationProcess/ChargerInstallationDetails/AddRequestModal.js
@@ -30,20 +30,20 @@ export const AddRequestModalForm = (props) => {
         return;
     };
 
-    const CheckDateEffectiveTo = (value, effectiveFrom) => {
-        if (!value) return Promise.resolve();
-        const bool = dayjs(value).format('YYYY-MM-DD') >= dayjs(effectiveFrom).format('YYYY-MM-DD');
-        if (bool) {
-            return Promise.resolve();
-        }
-        return Promise.reject(new Error('Date cant be less than Effective from date'));
-    };
+    // const CheckDateEffectiveTo = (value, effectiveFrom) => {
+    //     if (!value) return Promise.resolve();
+    //     const bool = dayjs(value).format('YYYY-MM-DD') >= dayjs(effectiveFrom).format('YYYY-MM-DD');
+    //     if (bool) {
+    //         return Promise.resolve();
+    //     }
+    //     return Promise.reject(new Error('Date cant be less than Effective from date'));
+    // };
 
     return (
         <Form autoComplete="off" layout="vertical" form={addRequestForm} onFinish={onModalFinish} onFinishFailed={onFinishFailed}>
             <Row gutter={16}>
                 <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
-                    <Form.Item label="Stage" name="requestStage">
+                    <Form.Item label="Stage" name="requestStage" rules={[validateRequiredInputField('Stage')]}>
                         {customSelectBox({ data: typeData?.CHRGR_INST_STG_TYPE, placeholder: preparePlaceholderText('Request Stage') })}
                     </Form.Item>
                 </Col>
