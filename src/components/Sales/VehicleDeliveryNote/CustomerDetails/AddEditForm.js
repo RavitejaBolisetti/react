85e7ae41b1c136d7481fb9303cd02b9aa3c282a3
Varsha <ase.varsha.ext@wipro.com>
2023-11-06 15:53:04 +0530

    customer details

--- src/components/Sales/VehicleDeliveryNote/CustomerDetails/AddEditForm.js
+++ src/components/Sales/VehicleDeliveryNote/CustomerDetails/AddEditForm.js
@@ -29,7 +29,7 @@ const AddEditFormMain = (props) => {
                             <Card style={{ backgroundColor: '#F2F2F2' }}>
                                 <Row gutter={20}>
                                     <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                        <Form.Item initialValue={formData?.customerId} label="Customer ID" name="customerId">
+                                        <Form.Item initialValue={formData?.customerId} label={translateContent('vehicleDeliveryNote.customerDetails.label.customerId')} name="customerId">
                                             {soldByDealer ? (
                                                 <>
                                                     <Input placeholder={preparePlaceholderText('Customer ID')} disabled={true} />
@@ -42,50 +42,50 @@ const AddEditFormMain = (props) => {
                                     {showFields && (
                                         <>
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item label="Customer Type" name="customerType">
+                                                <Form.Item label={translateContent('vehicleDeliveryNote.customerDetails.label.customerType')} name="customerType">
                                                     <Input placeholder={preparePlaceholderText('Customer Type')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.customerName} label="Customer Name" name="customerName">
+                                                <Form.Item initialValue={formData?.customerName} label={translateContent('vehicleDeliveryNote.customerDetails.label.customerName')} name="customerName">
                                                     <Input placeholder={preparePlaceholderText('Customer Name')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
 
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.mobile} label="Mobile" name="customerPhoneNumber">
+                                                <Form.Item initialValue={formData?.mobile} label={translateContent('vehicleDeliveryNote.customerDetails.label.customerPhoneNumber')} name="customerPhoneNumber">
                                                     <Input placeholder={preparePlaceholderText('Mobile')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item initialValue={formData?.address} label="Address" name="customerAddress">
+                                                <Form.Item initialValue={formData?.address} label={translateContent('vehicleDeliveryNote.customerDetails.label.customerAddress')} name="customerAddress">
                                                     <Input placeholder={preparePlaceholderText('Address')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item label="City" name="customerCity">
+                                                <Form.Item label={translateContent('vehicleDeliveryNote.customerDetails.label.customerCity')} name="customerCity">
                                                     <Input placeholder={preparePlaceholderText('City')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
 
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item label="District" name="district">
+                                                <Form.Item label={translateContent('vehicleDeliveryNote.customerDetails.label.district')} name="district">
                                                     <Input placeholder={preparePlaceholderText('District')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item label="State" name="state">
+                                                <Form.Item label={translateContent('vehicleDeliveryNote.customerDetails.label.state')} name="state">
                                                     <Input placeholder={preparePlaceholderText('State')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item label="Pincode" name="pinCode">
+                                                <Form.Item label={translateContent('vehicleDeliveryNote.customerDetails.label.customerId')} name="pinCode">
                                                     <Input placeholder={preparePlaceholderText('pincode')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
 
                                             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                                                <Form.Item label="Email ID" name="email">
+                                                <Form.Item label={translateContent('vehicleDeliveryNote.customerDetails.label.email')} name="email">
                                                     <Input placeholder={preparePlaceholderText('Email ID')} disabled={true} />
                                                 </Form.Item>
                                             </Col>
