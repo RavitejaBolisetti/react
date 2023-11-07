e6488d5acf1174b9eac269977d00794f20014cf2
Nishant Malik <nishant.malik.ext@wipro.com>
2023-11-07 12:19:30 +0530

    error handled

--- src/components/Sales/OTF/OTFFormButton/OTFFormButton.js
+++ src/components/Sales/OTF/OTFFormButton/OTFFormButton.js
@@ -7,7 +7,6 @@ import React from 'react';
 import { Button, Row, Col, Popover } from 'antd';
 
 import { FROM_ACTION_TYPE } from 'constants/formActionType';
-import { translateContent } from 'utils/translateContent';
 
 import { translateContent } from 'utils/translateContent';
 import styles from 'assets/sass/app.module.scss';
