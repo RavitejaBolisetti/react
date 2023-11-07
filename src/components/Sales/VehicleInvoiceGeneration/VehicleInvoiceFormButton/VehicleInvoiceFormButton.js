b61c07d728eb25e8401d44c6b37fd5e7415bfec7
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2023-11-07 11:31:37 +0530

    Changes in button name

--- src/components/Sales/VehicleInvoiceGeneration/VehicleInvoiceFormButton/VehicleInvoiceFormButton.js
+++ src/components/Sales/VehicleInvoiceGeneration/VehicleInvoiceFormButton/VehicleInvoiceFormButton.js
@@ -8,21 +8,22 @@ import { Button, Row, Col } from 'antd';
 
 import { FROM_ACTION_TYPE } from 'constants/formActionType';
 import styles from 'assets/sass/app.module.scss';
+import { translateContent } from 'utils/translateContent';
 
-export const VehicleInvoiceFormButton = ({ record, onCloseAction, onCancelInvoice, buttonData, setButtonData, saveButtonName = 'Save & Next', handleButtonClick, isLoadingOnSave, isLastSection, onPrintInvoice, onPrintForm21 }) => {
+export const VehicleInvoiceFormButton = ({ record, onCloseAction, onCancelInvoice, buttonData, setButtonData, saveButtonName = translateContent('global.buttons.saveAndNext'), handleButtonClick, isLoadingOnSave, isLastSection, onPrintInvoice, onPrintForm21 }) => {
     return (
         <div className={styles.formFooter}>
             <Row gutter={20}>
                 <Col xs={24} sm={8} md={6} lg={4} xl={4} className={styles.buttonsGroupLeft}>
                     {buttonData?.closeBtn && (
                         <Button danger onClick={onCloseAction}>
-                            Close
+                            {translateContent('global.buttons.close')}
                         </Button>
                     )}
 
                     {buttonData?.cancelBtn && (
                         <Button danger onClick={onCloseAction}>
-                            Cancel
+                            {translateContent('global.buttons.cancel')}
                         </Button>
                     )}
                 </Col>
@@ -30,24 +31,24 @@ export const VehicleInvoiceFormButton = ({ record, onCloseAction, onCancelInvoic
                 <Col xs={24} sm={16} md={18} lg={20} xl={20} className={styles.buttonsGroupRight}>
                     {buttonData?.printForm21Btn && (
                         <Button onClick={() => onPrintForm21(record)} danger>
-                            Print Form 21
+                            {translateContent('global.buttons.printForm21')}
                         </Button>
                     )}
                     {buttonData?.printInvoiceBtn && (
                         <Button onClick={() => onPrintInvoice(record)} danger>
-                            Print Invoice
+                            {translateContent('global.buttons.printInvoice')}
                         </Button>
                     )}
 
                     {buttonData?.cancelInvoiceBtn && (
                         <Button onClick={onCancelInvoice} danger>
-                            Cancel Invoice
+                            {translateContent('global.buttons.cancelInvoice')}
                         </Button>
                     )}
 
                     {buttonData?.nextBtn && !isLastSection && (
                         <Button onClick={() => handleButtonClick({ buttonAction: FROM_ACTION_TYPE.NEXT, record })} type="primary">
-                            Next
+                            {translateContent('global.buttons.next')}
                         </Button>
                     )}
                     {buttonData?.saveBtn && (
