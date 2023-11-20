acce930fa76ccbdc72038e787b8493bff1d0cf1e
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-11-20 12:23:18 +0000

    Squashed commit of the following
    commit 1dbdd055 
    Author: jatin kumar <jatin.kumar13@wipro.com> 
    Date: Mon Nov 20 2023 12:32:34 GMT+0530 (India Standard Time) 
    
        changes done
    
    
    commit 6e97d276 
    Author: jatin kumar <jatin.kumar13@wipro.com> 
    Date: Mon Nov 20 2023 12:31:21 GMT+0530 (India Standard Time) 
    
        auth actions unit test done

--- src/__tests__/store/action/auth/auth-index.test.jsx
+++ src/__tests__/store/action/auth/auth-index.test.jsx
@@ -1,93 +1,59 @@
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import moment from 'moment';
-import { readFromStorageAndValidateAuth } from '@store/actions/auth';
+import { readFromStorageAndValidateAuth, authPreLoginSuccess, authLoggingError, authUserLogout, authUserTokenExpired, unAuthenticateUser, doCloseLoginError, doCloseUnAuthenticatedError, authPostLogin, authPreLogin, doLogin, doRefreshToken, doLogoutAPI } from 'store/actions/auth';
+// eslint-disable-next-line jest/no-mocks-import
+import createMockStore from '__mocks__/store';
+import { waitFor } from '@testing-library/react';
 
-// Create a mock store to use during testing
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
-describe('readFromStorageAndValidateAuth', () => {
-  it('should dispatch the correct actions when there is a valid idToken that has not expired', () => {
-    // Mock the data for the localStorage
-    const idToken = 'some-valid-id-token';
-    const accessToken = 'some-access-token';
-    const refreshToken = 'some-refresh-token';
-    const userId = 'user123';
-    const passwordStatus = { isPasswordChanged: true };
+jest.mock('jwt-decode', () => jest.fn(() => ({ exp: Math.floor(Date.now() / 1000) + (60 * 60) })));
 
-    // Create a mock localStorage
-    const localStorageMock = {
-      getItem: jest.fn((key) => {
-        if (key === 'idToken') return idToken;
-        if (key === 'accessToken') return accessToken;
-        if (key === 'refreshToken') return refreshToken;
-        if (key === 'userId') return userId;
-        if (key === 'passwordStatus') return JSON.stringify(passwordStatus);
-        return null;
-      }),
-      removeItem: jest.fn(),
-    };
+describe('Auth Actions Component', () => {
+    it('should dispatch the actions', async () => {
+        const store = createMockStore({});
+        const actions = [];
 
-    // Set up the initial state of the Redux store
-    const initialState = {};
-    const store = mockStore(initialState);
+        store.subscribe(() => {
+            actions.push(store.getState().lastAction);
+        });
 
-    // Mock the current time to be just before the idToken's expiration time
-    const expirationTime = moment().add(1, 'hour').unix();
-    jest.spyOn(moment, 'now').mockImplementation(() => expirationTime * 1000 - 1);
+        store.dispatch(readFromStorageAndValidateAuth());
+        store.dispatch(authUserLogout());
+        store.dispatch(authUserTokenExpired());
+        store.dispatch(unAuthenticateUser());
+        store.dispatch(doCloseLoginError());
+        store.dispatch(doCloseUnAuthenticatedError());
+        store.dispatch(authPostLogin());
+        store.dispatch(authPreLogin());
 
-    // Invoke the action creator
-    store.dispatch(readFromStorageAndValidateAuth());
+        const requestData={ userId: 'test', password: 'password' };
+        const onError=jest.fn();
+        const onLogin=jest.fn();
+        store.dispatch(doLogin(requestData, false, onLogin, onError));
+        const params={
+          onSuccess: jest.fn(),
+          onError: jest.fn(),
+          data: { name: 'Kai' }
+        }
+        store.dispatch(doRefreshToken(params));
+        store.dispatch(doLogoutAPI(params));
 
-    // Check if the correct actions were dispatched
-    expect(store.getActions()).toEqual([{"type": "AUTH_LOGOUT"}]);
+    });
 
-    // Check if the localStorage was not cleared
-    expect(localStorageMock.removeItem).not.toHaveBeenCalled();
-  });
+    it('should dispatch the correct actions when there is a valid idToken that has not expired', async () => {
+        localStorage.setItem('idToken', 'hello');
+        localStorage.setItem('accessToken', 'accessToken');
+        localStorage.setItem('refreshToken', 'refreshToken');
+        localStorage.setItem('userId', 'userId');
+        localStorage.setItem('passwordStatus', JSON.stringify('passwordStatus'));
 
-  it('should dispatch the correct actions when there is no idToken', () => {
-    // Create a mock localStorage without idToken
-    const localStorageMock = {
-      getItem: jest.fn(() => null),
-      removeItem: jest.fn(),
-    };
+        const store = createMockStore({});
+        store.dispatch(readFromStorageAndValidateAuth());
 
-    // Set up the initial state of the Redux store
-    const initialState = {};
-    const store = mockStore(initialState);
+        const authPostLoginActions=jest.fn();
 
-    // Invoke the action creator
-    store.dispatch(readFromStorageAndValidateAuth());
+        await waitFor(() => { authPostLoginActions() });
+    });
 
-    // Check if the correct actions were dispatched
-    expect(store.getActions()).toEqual([{ type: 'AUTH_LOGOUT' }]);
-
-    // Check if the localStorage was not cleared
-    expect(localStorageMock.removeItem).not.toHaveBeenCalled();
-  });
-
-  it('should dispatch the correct actions when there is an error reading from localStorage', () => {
-    // Create a mock localStorage that throws an error when reading
-    const localStorageMock = {
-      getItem: jest.fn(() => {
-        throw new Error('localStorage error');
-      }),
-      removeItem: jest.fn(),
-    };
-
-    // Set up the initial state of the Redux store
-    const initialState = {};
-    const store = mockStore(initialState);
-
-    // Invoke the action creator
-    store.dispatch(readFromStorageAndValidateAuth());
-
-    // Check if the correct actions were dispatched
-    expect(store.getActions()).toEqual([{ type: 'AUTH_LOGOUT' }]);
-
-    // Check if the localStorage was not cleared due to the error
-    expect(localStorageMock.removeItem).not.toHaveBeenCalled();
-  });
+    it('functions should work', async () => {
+        authPreLoginSuccess();
+        authLoggingError();
+    });
 });
-
