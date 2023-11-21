6f0e9f55838b16582bd4bcee1a42b3cf958edbe5
K Aman Rao <aman.rao1@wipro.com-at-803584912514>
2023-11-22 01:56:59 +0530

    Extra Copyright line removed

--- src/components/Sales/VehicleSalesSchemeMaster/productTableColumn.js
+++ src/components/Sales/VehicleSalesSchemeMaster/productTableColumn.js
@@ -3,11 +3,6 @@
  *   All rights reserved.
  *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
  */
-/*
- *   Copyright (c) 2023 Mahindra & Mahindra Ltd.
- *   All rights reserved.
- *   Redistribution and use of any source or binary or in any form, without written approval and permission is prohibited. Please read the Terms of Use, Disclaimer & Privacy Policy on https://www.mahindra.com/
- */
 import { Tag } from 'antd';
 import { tblActionColumn, tblPrepareColumns } from 'utils/tableColumn';
 import { translateContent } from 'utils/translateContent';
