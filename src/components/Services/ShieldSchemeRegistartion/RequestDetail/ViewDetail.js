1dcf0df9537a13b2f38a9a217877f4fbc9526004
Kuldeep Singh <kuldeep.singh1@wipro.com>
2023-11-21 10:02:30 +0000

    Squashed commit of the following
    commit 42d71e52 
    Author: Sujeet Kumar <sujeet.singh10@wipro.com> 
    Date: Tue Nov 21 2023 14:31:39 GMT+0530 (India Standard Time) 
    
        #Fixed Cancel Observation
    
    US-4415-Scheme-Registration-Bug-Fix
    
    
    commit a87172d2 
    Author: Sujeet Kumar <sujeet.singh10@wipro.com> 
    Date: Tue Nov 21 2023 13:34:34 GMT+0530 (India Standard Time) 
    
        #Fixed Observation
    
    US-4415-Scheme-Registration-Bug-Fix
    
    
    commit 845bbb23 
    Author: Sujeet Kumar <sujeet.singh10@wipro.com> 
    Date: Tue Nov 21 2023 12:35:47 GMT+0530 (India Standard Time) 
    
        #Fixed Observation
    
    US-4415-Scheme-Registration-Bug-Fix

--- src/components/Services/ShieldSchemeRegistartion/RequestDetail/ViewDetail.js
+++ src/components/Services/ShieldSchemeRegistartion/RequestDetail/ViewDetail.js
@@ -54,7 +54,7 @@ const ViewDetail = (props) => {
                         {/* <Tag style={{ float: 'right' }}>{getCodeValue(typeData?.AMC_REG_APRVL_STAT, selectedOrder?.status)}</Tag> */}
                     </Col>
                 </Row>
-                <Row type="flex" align="middle" className={selectedOrder?.status === AMC_CONSTANTS?.PENDING_FOR_CANCELLATION?.title ? '' : styles.marB20}>
+                <Row type="flex" align="middle" className={selectedOrder?.status === QUERY_BUTTONS_MNM_USER?.PENDING_FOR_CANCELLATION?.key ? '' : styles.marB20}>
                     <Col xs={24} sm={24} md={24} lg={24}>
                         <div className={styles.tableTextColor85}>
                             {translateContent('shieldSchemeRegistration.label.requestedOn')}: {convertDateMonthYear(formData?.shieldRegistrationDate)}
@@ -63,11 +63,11 @@ const ViewDetail = (props) => {
                 </Row>
                 {selectedOrder?.status === QUERY_BUTTONS_MNM_USER?.PENDING_FOR_CANCELLATION?.key && <Divider className={styles.marT20} />}
                 {userType === AMC_CONSTANTS?.MNM?.key ? (
-                    selectedOrder?.status === QUERY_BUTTONS_MNM_USER?.PENDING_FOR_APPROVAL?.key || AMC_CONSTANTS?.PENDING_FOR_CANCELLATION?.title ? (
+                    selectedOrder?.status === QUERY_BUTTONS_MNM_USER?.PENDING_FOR_APPROVAL?.key || QUERY_BUTTONS_MNM_USER?.PENDING_FOR_CANCELLATION?.key ? (
                         <>
                             {selectedOrder?.status === QUERY_BUTTONS_MNM_USER?.PENDING_FOR_CANCELLATION?.key && (
                                 <>
-                                    <Divider className={styles.marT20} />
+                                    {/* <Divider className={styles.marT20} /> */}
                                     {/* <div className={styles.marB20}> */}
                                     <Descriptions {...viewProps}>
                                         <Descriptions.Item label={translateContent('shieldSchemeRegistration.label.reasonForCancellationRequest')}>{checkAndSetDefaultValue(formData?.shieldCancelRemarks)}</Descriptions.Item>
@@ -80,7 +80,7 @@ const ViewDetail = (props) => {
                             )}
 
                             <Row gutter={20} className={styles.marB20}>
-                                {/* {selectedOrder?.workFlowDetails?.allowedActions?.map((element, i) => {
+                                {/* {selectedOrder?.workflowMasterDetails?.allowedActions?.map((element, i) => {
                                     return (
                                         <Button onClick={() => handleRequest({ buttonAction: element?.actionCode })} type="primary" key={i}>
                                             {element?.actionName}
