b2fb72ed844ed26e413ca387eccdfcf52712878c
Shakambhar Pujari <shakambhar.pujari.ext@wipro.com>
2024-02-26 12:36:53 +0530

    data changes

--- src/components/common/PartyMaster/ViewDetail.js
+++ src/components/common/PartyMaster/ViewDetail.js
@@ -6,38 +6,40 @@
 import React from 'react';
 import { Descriptions } from 'antd';
 import { translateContent } from 'utils/translateContent';
+import { checkAndSetDefaultValue } from 'utils/checkAndSetDefaultValue';
 
-const ViewDetailBase = ({ formData, styles, parameterType }) => {
+const ViewDetailBase = ({ formData, styles, isLoading }) => {
     const viewOneColProps = {
         bordered: false,
         colon: false,
         layout: 'vertical',
         column: { xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 },
     };
+    const CheckSetWrapper = (value) => checkAndSetDefaultValue(value, isLoading);
+
     return (
         <>
             <div className={`${styles?.viewContainer} ${styles?.viewOneColProps}`}>
                 <Descriptions {...viewOneColProps}>
-                    <Descriptions.Item label={translateContent('partyMaster.label.partyCategory')}>{formData?.partyCategory}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.partyCode')}>{formData?.partyCode}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.partyName')}>{formData?.partyName}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.contactPersonName')}>{formData?.contactPersonName}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.designation')}>{formData?.designation}</Descriptions.Item>
-                    {/* <Descriptions.Item label={translateContent('partyMaster.label.mobileNumber')}"Mobile Number">{formData?.mobileNumber}</Descriptions.Item> */}
-                    <Descriptions.Item label={translateContent('partyMaster.label.address')}>{formData?.address}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.pinCode')}>{formData?.pinCode}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.city')}>{formData?.city}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.tehsil')}>{formData?.tehsil}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.district')}>{formData?.district}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.state')}>{formData?.state}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.mobileNumber')}>{formData?.mobileNumber}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.alternateMobileNumber')}>{formData?.alternateMobileNumber}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.gstinNumber')}>{formData?.gstInNumber}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.pan')}>{formData?.panNumber}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.partsDiscount')+ "(%)"}>{formData?.partsDiscount}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.creditDays')}>{formData?.creditDays}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.creditLimit')}>{formData?.creditLimit}</Descriptions.Item>
-                    <Descriptions.Item label={translateContent('partyMaster.label.remarks')}>{formData?.remarks}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.partyCategory')}>{CheckSetWrapper(formData?.partyCategory)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.partyCode')}>{CheckSetWrapper(formData?.partyCode)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.partyName')}>{CheckSetWrapper(formData?.partyName)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.contactPersonName')}>{CheckSetWrapper(formData?.contactPersonName)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.designation')}>{CheckSetWrapper(formData?.designation)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.address')}>{CheckSetWrapper(formData?.address)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.pinCode')}>{CheckSetWrapper(formData?.pinCode)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.city')}>{CheckSetWrapper(formData?.city)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.tehsil')}>{CheckSetWrapper(formData?.tehsil)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.district')}>{CheckSetWrapper(formData?.district)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.state')}>{CheckSetWrapper(formData?.state)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.mobileNumber')}>{CheckSetWrapper(formData?.mobileNumber)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.alternateMobileNumber')}>{CheckSetWrapper(formData?.alternateMobileNumber)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.gstinNumber')}>{CheckSetWrapper(formData?.gstInNumber)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.pan')}>{CheckSetWrapper(formData?.panNumber)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.partsDiscount') + '(%)'}>{CheckSetWrapper(formData?.partsDiscount)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.creditDays')}>{CheckSetWrapper(formData?.creditDays)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.creditLimit')}>{CheckSetWrapper(formData?.creditLimit)}</Descriptions.Item>
+                    <Descriptions.Item label={translateContent('partyMaster.label.remarks')}>{CheckSetWrapper(formData?.remarks)}</Descriptions.Item>
                 </Descriptions>
             </div>
         </>
